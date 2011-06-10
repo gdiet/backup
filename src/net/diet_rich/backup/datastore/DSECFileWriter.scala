d6eeaa3e93fb82672cbec9f9df5d0c5b57df0004
Georg Dietrich <Georg Dietrich@localhost>
2011-06-11 00:50:36 +0200

    added MIT license

--- src/net/diet_rich/backup/datastore/DSECFileWriter.scala
+++ src/net/diet_rich/backup/datastore/DSECFileWriter.scala
@@ -1,3 +1,5 @@
+// Copyright (c) 2011 Georg Dietrich
+// Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php
 package net.diet_rich.backup.datastore
 
 private[datastore]
