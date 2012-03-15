e118037cf299ed234d470248cf818638c5ae94db
ugdietrich <ugdietrich@localhost>
2012-03-15 17:05:17 +0100

    cont'd: DataStore implementation and tests

--- src/net/diet_rich/dfs/ds/BasicDataStore.scala
+++ src/net/diet_rich/dfs/ds/BasicDataStore.scala
@@ -3,21 +3,50 @@ package net.diet_rich.dfs.ds
 import java.io.File
 import net.diet_rich.util.ASSUME
 import net.diet_rich.util.data.Bytes
+import scala.collection.mutable.LinkedHashMap
 
 class BasicDataStore(datadir: File) extends DataStore {
 
   ASSUME(datadir.isDirectory, "data directory <" + datadir + "> is not a directory.")
   
-  protected def dataLength = 1000 // TODO store in directory config file
-  // TODO introduce sensible files-per-dir (in 2 or more levels)
+  protected val dataLength = 1000 // TODO store in directory configuration file
+  protected val openFiles = 10 // TODO make a backup system configuration property
+
+  protected def fileOffset(offset: Long) = offset - offset % dataLength
   
-  def write(offset: Long, data: Bytes) = checkClosed { throw new AssertionError }
+  protected def fileName(offset: Long) = "%015d" format fileOffset(offset)
+  
+  // TODO introduce sensible files-per-dir (in 2 or more levels)
+  protected def dataFileFor(offset: Long) = new File(datadir, fileName(offset))
+  
+  protected val openDataFiles = LinkedHashMap[Long, CachedDataFile]()
+
+  protected def dataFile(offset: Long) = {
+    val entryOffset = fileOffset(offset)
+    openDataFiles get entryOffset getOrElse {
+      if (openDataFiles.size >= openFiles) {
+        val entryToRemove = openDataFiles.keys head
+        val removed = (openDataFiles remove entryToRemove get)
+        removed writeData
+      }
+      val result = new CachedDataFile(entryOffset, dataLength, dataFileFor(offset))
+      openDataFiles += ((entryOffset, result))
+      result
+    }
+  }
+  
+  def write(offset: Long, data: Bytes) = checkClosed {
+    val df = dataFile(offset)
+    df.write(offset, data)
+    // write rest if any to next file
+    throw new AssertionError
+  }
   
   def read(offset: Long, size: Long) : Bytes = checkClosed { throw new AssertionError }
   
   protected var closed = false
   
-  def close() : Unit = {
+  def close : Unit = {
     closed = true
     // TODO add close operations
   }
--- test/net/diet_rich/dfs/ds/SpecDataStoreTests.scala
+++ test/net/diet_rich/dfs/ds/SpecDataStoreTests.scala
@@ -1,9 +1,12 @@
 package net.diet_rich.dfs.ds
 
+import org.fest.assertions.Assertions.assertThat
 import org.testng.annotations.{BeforeMethod,Test}
 import java.io.File
 import org.apache.commons.io.FileUtils
 import SpecDataStoreTests._
+import net.diet_rich.util.data.Bytes
+import net.diet_rich.util.io.using
 
 class SpecDataStoreTests {
 
@@ -13,28 +16,40 @@ class SpecDataStoreTests {
   }
   
   @Test
-  def writeAndRead = {
-    val store = new BasicDataStore(testDir)
-    throw new AssertionError
+  def writeAndRead = using (new BasicDataStore(testDir)) { store =>
+    val data = Bytes forLong 987654321
+    store write (5000, data)
+    assertThat( store read (5000, data length) bytes ) isEqualTo data.bytes
   }
   
   @Test
-  def writeCloseRead = throw new AssertionError
+  def writeCloseRead = {
+    val store1 = new BasicDataStore(testDir)
+    val data = Bytes forLong 987654321
+    store1 write (5000, data)
+    store1.close
+    using (new BasicDataStore(testDir)) { store2 =>
+      assertThat( store2 read (5000, data length) bytes ) isEqualTo data.bytes
+    }
+  }
   
   @Test
-  def writeOverwriteRead = throw new AssertionError
-
-  @Test
-  def writeOverFileLimitRead = throw new AssertionError
+  def moreTests = throw new AssertionError
   
-  @Test
-  def writeOverFileLimitCloseRead = throw new AssertionError
-
-  @Test
-  def writeAferCloseMustFail = throw new AssertionError
-
-  @Test
-  def readAferCloseMustFail = throw new AssertionError
+//  @Test
+//  def writeOverwriteRead = throw new AssertionError
+//
+//  @Test
+//  def writeOverFileLimitRead = throw new AssertionError
+//  
+//  @Test
+//  def writeOverFileLimitCloseRead = throw new AssertionError
+//
+//  @Test
+//  def writeAferCloseMustFail = throw new AssertionError
+//
+//  @Test
+//  def readAferCloseMustFail = throw new AssertionError
   
 }
 
