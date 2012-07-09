3b9c07aa1c72c78277ec97ee3ec316ad919c98b6
ugdietrich <ugdietrich@localhost>
2012-07-09 09:53:37 +0200

    (1629/7435) cont'd backup implementation

--- src/net/diet_rich/util/io/_.scala
+++ src/net/diet_rich/util/io/_.scala
@@ -8,5 +8,17 @@ package object io {
   /** closes the resource after the operation */
   def using[Closeable <: {def close() : Unit}, ReturnType] (resource: Closeable)(operation: Closeable => ReturnType) : ReturnType =
     try { operation(resource) } finally { resource.close }
+
+  type Reader = { def read(bytes: Array[Byte], offset: Int, length: Int): Int }
+    
+  def readFully(input: Reader, bytes: Array[Byte], offset: Int, length: Int) : Int = {
+    def readRecurse(offset: Int): Int = {
+      input.read(bytes, offset, length - offset) match {
+        case -1 => offset
+        case  n => readRecurse(offset)
+      }
+    }
+    readRecurse(offset) - offset
+  }
     
 }
