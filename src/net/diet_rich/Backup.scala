4ea46507d42b2942f3bcc74246e1f041e7fd5aeb
ugdietrich <ugdietrich@localhost>
2012-07-14 18:11:57 +0200

    (1648/7519) cont'd backup implementation

--- src/net/diet_rich/Backup.scala
+++ src/net/diet_rich/Backup.scala
@@ -3,6 +3,7 @@ package net.diet_rich
 import java.io.File
 import net.diet_rich.fdfs._
 import net.diet_rich.util.closureToRunnable
+import net.diet_rich.util.io.readFully
 
 
 object Backup extends App {
@@ -33,6 +34,9 @@ object Backup extends App {
 class Backup(source: File, repository: File, target: String) {
   import Backup._
   
+  // FIXME read from repository settings
+  val hashAlgorithm = "MD5"
+  
   if (!target.startsWith("/")) throw new IllegalArgumentException("Target path must start with '/'.")
   if (!source.canRead) throw new IllegalArgumentException("Can't read source.")
   
@@ -57,8 +61,8 @@ class Backup(source: File, repository: File, target: String) {
     if (source.isFile) {
       // get print, keep bytes read
       val bytes = new Array[Byte](STOREARRAYSIZE)
-      val input = new java.io.RandomAccessFile(source, "r")
-      val sizeOfInput = input.length
+      val sizeOfInput = source.length
+      val input = new HashCalculator(new java.io.RandomAccessFile(source, "r"), "MD5")
       val (bytesReadForPrint, print) = PrintCalculator.print(input, bytes)
       // if print matches
       if (datadb.hasMatchingPrint(sizeOfInput, print)) {
@@ -71,6 +75,14 @@ class Backup(source: File, repository: File, target: String) {
         // if fits into memory
         if (fitsToMemory) {
           // read into memory
+          def readRecurse(startAt: Int, arrays: List[Array[Byte]]): Long = {
+            val array = arrays.head;
+            val read: Long = readFully(input, bytes, startAt, array.length - startAt)
+            read + (if (read == 0) 0 else readRecurse(0, arrays.tail))
+          }
+          readRecurse(bytesReadForPrint, storeArrays)
+          // FIXME continue
+          
           // check hash and store if necessary
         } else {
           // does not fit into memory
--- src/net/diet_rich/fdfs/PrintCalculator.scala
+++ src/net/diet_rich/fdfs/PrintCalculator.scala
@@ -1,13 +1,12 @@
 package net.diet_rich.fdfs
 
-import java.security.MessageDigest
 import net.diet_rich.util.io._
 
 object PrintCalculator {
   val PRINTAREA = 2048
   
   val zeroBytePrint: Long = print (new java.io.ByteArrayInputStream(new Array(0)), new Array(PRINTAREA)) ._2
-  
+
   def print(input: Reader, bytes: Array[Byte]) : (Int, Long) = {
     val read = readFully(input, bytes, 0, PRINTAREA)
     val crc = new java.util.zip.CRC32
@@ -16,4 +15,18 @@ object PrintCalculator {
     adler.update(bytes, 0, read)
     (read, adler.getValue << 32 | crc.getValue)
   }
+}
+
+class HashCalculator(input: Reader, algorithm: String) {
+  protected val digester = java.security.MessageDigest.getInstance(algorithm)
+  def hash = digester.digest
+  def read(bytes: Array[Byte], offset: Int, length: Int): Int = {
+    val result = input.read(bytes, offset, length)
+    if (result > 0) digester.update(bytes, offset, result)
+    result
+  }
+}
+
+object HashCalculator {
+  def zeroByteHash(algorithm: String): Array[Byte] = java.security.MessageDigest.getInstance(algorithm).digest
 }
