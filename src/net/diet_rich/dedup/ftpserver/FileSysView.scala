60ec53703939c6128a0662f50194d334ebb20fe7
die <die@localhost>
2013-01-19 18:24:10 +0100

    Added FTP path resolver

--- src/net/diet_rich/dedup/ftpserver/FileSysView.scala
+++ src/net/diet_rich/dedup/ftpserver/FileSysView.scala
@@ -19,42 +19,44 @@ object Helpers {
 }
 
 class FileSysView(repository: Repository) extends FileSystemView {
+  
   var workingDirectory = getHomeDirectory
+
+  def resolvePath(path: String): Option[RepoFile] = {
+    def cdTo(currentDir: RepoFile, path: List[String]): Option[RepoFile] = {
+      path match {
+        case Nil => Some(currentDir)
+        case ""  :: tail => cdTo(currentDir, tail)
+        case "." :: tail => cdTo(currentDir, tail)
+        case "..":: tail => currentDir.getParent.flatMap(cdTo(_, tail))
+        case e   :: tail => currentDir.listFilesScala.find(_.getName == e).flatMap(cdTo(_, tail))
+      }
+    }
+    val (startDir, relativeDir) =
+      if (path.startsWith("/")) (getHomeDirectory, path.substring(1)) else (workingDirectory, path)
+    cdTo(startDir, relativeDir.split('/').toList)
+  }
+  
   def changeWorkingDirectory(dir: String): Boolean = logAnd(f"cd $dir") {
-    if (!dir.contains("/") && !dir.contains(".")) {
-      workingDirectory.listFilesScala.find(_.getName == dir) match {
-        case Some(newDir) =>
-          workingDirectory = newDir
-          true
-        case None =>
-          false
-      }
-    } else if (dir == "..") {
-      workingDirectory.getParent match {
-        case Some(newDir) =>
-          workingDirectory = newDir
-          true
-        case None =>
-          false
-      }
-    } else if (dir == "/") {
-      workingDirectory = getHomeDirectory
-      true
-    } else {
-      System.err.println(f"cd $dir");
-      Thread.sleep(500)
-      ???
+    resolvePath(dir) match {
+      case None => false
+      case Some(repoFile) => workingDirectory = repoFile; true
     }
   }
+  
   def dispose(): Unit = logAnd("dispose")(System.exit(0))
+  
   def getFile(name: String): FtpFile = logAnd("getting: " + name) {
-    if (name == "./")
-      workingDirectory
-    else
-      ???
+    resolvePath(name) match {
+      case Some(repoFile) => repoFile
+      case None => throw new IllegalArgumentException("could not resolve path $name")
+    }
   }
+  
   def getHomeDirectory(): RepoFile = logAnd("getHomeDirectory")(new RepoFile(repository, TreeDB.ROOTID))
+
   def getWorkingDirectory(): RepoFile = logAnd("getWorkingDirectory")(workingDirectory)
+
   def isRandomAccessible(): Boolean = ???
 }
 
@@ -66,66 +68,66 @@ class RepoFile(repository: Repository, id: TreeEntryID) extends FtpFile {
   def getParent: Option[RepoFile] =
     repository.fs.entry(id).flatMap(_.parentOption.map(new RepoFile(repository, _)))
   
-  override def getAbsolutePath(): String = logAnd("getAbsolutePath for " + id) {
+  def getAbsolutePath(): String = logAnd("getAbsolutePath for " + id) {
     repository.fs.path(id) match {
       case Some(path) => path.value
       case _ => logAnd("WARN: getAbsolutePath - node $id does not exist")("")
     }
   }
 
-  override def isFile(): Boolean = logAnd("isFile for " + id) {
+  def isFile(): Boolean = logAnd("isFile for " + id) {
     repository.fs.fullDataInformation(id).isDefined
   }
 
-  override def isDirectory(): Boolean = logAnd("isDirectory for " + id) {
+  def isDirectory(): Boolean = logAnd("isDirectory for " + id) {
     repository.fs.entry(id).isDefined && (!isFile)
   }
 
   def listFilesScala =
     repository.fs.children(id).map(e => new RepoFile(repository, e.id))
   
-  override def listFiles(): java.util.List[FtpFile] = logAnd("listFiles for " + id) {
+  def listFiles(): java.util.List[FtpFile] = logAnd("listFiles for " + id) {
     JavaConversions.seqAsJavaList(listFilesScala.toSeq)
   }
 
-  override def isHidden(): Boolean = logAnd("isHidden for " + id) { false }
+  def isHidden(): Boolean = logAnd("isHidden for " + id) { false }
   
-  override def getSize(): Long = logAnd("getSize for " + id) {
+  def getSize(): Long = logAnd("getSize for " + id) {
     repository.fs.fullDataInformation(id).map(_.size.value).getOrElse(0L)
   }
 
-  override def getLastModified(): Long = logAnd("getLastModified for " + id) {
+  def getLastModified(): Long = logAnd("getLastModified for " + id) {
     repository.fs.entry(id).map(_.time.value).getOrElse(0L)
   }
   
-  override def isReadable(): Boolean = logAnd("isReadable for " + id) {
+  def isReadable(): Boolean = logAnd("isReadable for " + id) {
     repository.fs.entry(id).isDefined
   }
 
-  override def doesExist(): Boolean = logAnd("doesExist for " + id) {
+  def doesExist(): Boolean = logAnd("doesExist for " + id) {
     repository.fs.entry(id).isDefined
   }
   
-  override def isWritable(): Boolean = logAnd("isWritable for " + id) { false } // FIXME write
+  def isWritable(): Boolean = logAnd("isWritable for " + id) { false } // FIXME write
 
-  override def getName(): String = logAnd("getLastModified for " + id) {
+  def getName(): String = logAnd("getLastModified for " + id) {
     repository.fs.entry(id).map(_.name).getOrElse("")
   }
 
-  override def getLinkCount(): Int = logAnd("getLinkCount for " + id) {
+  def getLinkCount(): Int = logAnd("getLinkCount for " + id) {
     if (doesExist) 1 else 0
   }
 
-  override def getOwnerName(): String = logAnd(f"getOwnerName for $id") { "backup" }
+  def getOwnerName(): String = logAnd(f"getOwnerName for $id") { "backup" }
 
-  override def getGroupName(): String = logAnd(f"getGroupName for $id") { "dedup" }
+  def getGroupName(): String = logAnd(f"getGroupName for $id") { "dedup" }
   
-  override def createInputStream(x$1: Long): java.io.InputStream = ???
-  override def createOutputStream(x$1: Long): java.io.OutputStream = ???
-  override def delete(): Boolean = ???
-  override def isRemovable(): Boolean = ???
-  override def mkdir(): Boolean = ???
-  override def move(x$1: org.apache.ftpserver.ftplet.FtpFile): Boolean = ???
-  override def setLastModified(x$1: Long): Boolean = ???
+  def createInputStream(x$1: Long): java.io.InputStream = ???
+  def createOutputStream(x$1: Long): java.io.OutputStream = ???
+  def delete(): Boolean = ???
+  def isRemovable(): Boolean = ???
+  def mkdir(): Boolean = ???
+  def move(x$1: org.apache.ftpserver.ftplet.FtpFile): Boolean = ???
+  def setLastModified(x$1: Long): Boolean = ???
   
 }
