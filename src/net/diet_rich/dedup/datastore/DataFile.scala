384b4dcf9189d2aceadf48ad13cfa7d5195e7bfb
die <die@localhost>
2013-01-19 19:00:18 +0100

    Added read method to DataFile for planned FTP read access

--- src/net/diet_rich/dedup/datastore/DataFile.scala
+++ src/net/diet_rich/dedup/datastore/DataFile.scala
@@ -5,12 +5,14 @@ package net.diet_rich.dedup.datastore
 
 import java.io._
 import net.diet_rich.util._
+import net.diet_rich.util.io.fillFrom
 import net.diet_rich.util.vals._
 
 class DataFile(position: Position, val path: File) {
 
   private var usageCount = 0
   private var file: Option[RandomAccessFile] = None
+  private var readonly = true
 
   def acquire = synchronized {
     assume(usageCount >= 0)
@@ -22,8 +24,9 @@ class DataFile(position: Position, val path: File) {
     usageCount = usageCount - 1
   }
 
-  private def initializeFile = {
-    assume(file.isEmpty)
+  private def initializeFileReadWrite = {
+    assume(readonly || file.isEmpty)
+    file.foreach(_.close) // if open readonly
     path.getParentFile.mkdirs
     val alreadyExisted = path.isFile
     val result = new RandomAccessFile(path, "rw")
@@ -32,14 +35,29 @@ class DataFile(position: Position, val path: File) {
     file = Some(result)
     result
   }
+
+  private def initializeFileReadOnly = {
+    assume(file.isEmpty)
+    file = Some(new RandomAccessFile(path, "r"))
+    file.get
+  }
   
   def write(position: Long, bytes: Array[Byte], offset: Position, size: Size) = synchronized {
     assume(usageCount >= 1)
     assume(offset.value <= Int.MaxValue)
     assume(size.value <= Int.MaxValue)
-    val out = file.getOrElse(initializeFile)
+    val out = file.getOrElse(initializeFileReadWrite)
     out.seek(position)
     out.write(bytes, offset.value toInt, size.value toInt)
+  }
+
+  def read(position: Long, bytes: Array[Byte], offset: Position, size: Size): Int = synchronized {
+    assume(usageCount >= 1)
+    assume(offset.value <= Int.MaxValue)
+    assume(size.value <= Int.MaxValue)
+    val in = file.getOrElse(initializeFileReadOnly)
+    in.seek(position)
+    fillFrom(in, bytes, offset.value toInt, size.value toInt)
   }
 
   def closeIfUnused = synchronized {
