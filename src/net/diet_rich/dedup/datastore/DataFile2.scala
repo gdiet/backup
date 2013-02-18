d4155c7841a3ddcfdbd6e81f6c39cacc8ec9a5dd
die <die@localhost>
2013-02-18 13:29:03 +0100

    (1922/7824) added check print and erase data to DataFile2

--- src/net/diet_rich/dedup/datastore/DataFile2.scala
+++ src/net/diet_rich/dedup/datastore/DataFile2.scala
@@ -9,29 +9,46 @@ import net.diet_rich.util.vals._
 
 class DataFile2(val position: Position, val file: java.io.File) extends Closeable {
 
-  protected final def maybeFileAccess = maybeFileAccessor 
-  protected final def usageCount = usageCounter
+  protected final def randomAccessFile: RandomAccessFile = maybeFileAccess.getOrElse(initializeFile)
+  protected final def maybeFileAccess: Option[RandomAccessFile] = maybeFileAccessor 
+  protected final def usageCount: Int = usageCounter
+  protected final def dataPrint: Long = dataPrintVar
   
   private final var maybeFileAccessor: Option[RandomAccessFile] = None
-  private var usageCounter = 0
+  private var usageCounter: Int = 0
+  private var dataPrintVar: Long = 0
 
   // is overridden in DataFileWrite
-  protected def internalInitializeFile = (new RandomAccessFile(file, "r"), true)
+  protected def internalInitializeFile = new RandomAccessFile(file, "r")
   
   protected final def initializeFile = {
     assume(maybeFileAccess.isEmpty)
     assume(usageCount == 1)
-    val (fileAccessor, mayCheckHeader) = internalInitializeFile
-    if (mayCheckHeader && fileAccessor.readLong() != position.value) {
-      // TODO real warning logging
-      fileAccessor.seek(0)
-      println(s"WARN: read position ${fileAccessor.readLong()} from data file but expected $position. Fixing now...")
-      fileAccessor.seek(0)
-      fileAccessor.writeLong(position.value)
-    }
-    maybeFileAccessor = Some(fileAccessor)
-    fileAccessor
+    val randomAccessFile = internalInitializeFile
+    randomAccessFile.seek(0)
+    dataPrintVar = randomAccessFile.readLong()
+    assume (randomAccessFile.readLong() == position.value)
+    maybeFileAccessor = Some(randomAccessFile)
+    randomAccessFile
   }
+
+  final def checkDataPrint: Boolean = synchronized {
+    assume(usageCount >= 1)
+    val bytes = new Array[Byte](randomAccessFile.length().toInt - DataFile2.headerBytes)
+    randomAccessFile.seek(DataFile2.headerBytes)
+    fillFrom(randomAccessFile, bytes, 0, bytes.length)
+    calcDataPrint(dataPrint, bytes, 0, bytes.length) == 0
+  }
+  
+  protected final def calcDataPrint(seed: Long, bytes: Array[Byte], offset: Int, size: Int): Long = {
+    var print = seed
+    for (n <- offset until offset + size)
+      print = print ^ (n + 1) * 5870203405204807807L * bytes(n)
+    print
+  }
+  
+  protected final def updateDataPrint(bytes: Array[Byte], offset: Int, size: Int) =
+    dataPrintVar = calcDataPrint(dataPrint, bytes, offset, size)
   
   final def acquire = synchronized {
     assume(usageCount >= 0)
@@ -71,7 +88,6 @@ trait DataFileRead {
     assume(usageCount >= 1)
     assume(offset.value <= Int.MaxValue)
     assume(size.value <= Int.MaxValue)
-    val randomAccessFile = maybeFileAccess.getOrElse(initializeFile)
     randomAccessFile.seek(position + DataFile2.headerBytes)
     fillFrom(randomAccessFile, bytes, offset.value toInt, size.value toInt)
   }
@@ -80,11 +96,9 @@ trait DataFileRead {
 trait DataFileWrite extends Closeable {
   self: DataFile2 =>
   
-  private var dataPrint: Long = 0
-
   abstract final override def close = synchronized {
     maybeFileAccess.foreach { fileAccessor =>
-      fileAccessor.seek(8)
+      fileAccessor.seek(0)
       fileAccessor.writeLong(dataPrint)
     }
     super.close
@@ -94,19 +108,30 @@ trait DataFileWrite extends Closeable {
     val alreadyExisted = file.isFile
     if (!alreadyExisted) file.getParentFile.mkdirs
     val fileAccessor = new RandomAccessFile(file, "rw")
+    fileAccessor.seek(8)
     if (!alreadyExisted) fileAccessor.writeLong(position.value)
     (fileAccessor, alreadyExisted)
   }
 
-  final def write(position: Long, bytes: Array[Byte], offset: Position, size: Size): Unit = synchronized {
+  final def writeNewData(position: Position, bytes: Array[Byte], offset: Position, size: Size): Unit = synchronized {
     assume(usageCount >= 1)
     assume(offset.value <= Int.MaxValue)
     assume(size.value <= Int.MaxValue)
-    val randomAccessFile = maybeFileAccess.getOrElse(initializeFile)
-    randomAccessFile.seek(position + DataFile2.headerBytes)
+    randomAccessFile.seek(position.value + DataFile2.headerBytes)
     randomAccessFile.write(bytes, offset.value toInt, size.value toInt)
-    // FIXME assumes the data file area was not written to before
-    for (n <- offset.value.toInt until offset.value.toInt + size.value.toInt)
-      dataPrint = dataPrint ^ (n + 1) * 0x1000100010001L * bytes(n)
+    updateDataPrint(bytes, offset.value.toInt, size.value.toInt)
+  }
+  
+  // FIXME introduce IntSize and IntOffset?
+  final def eraseData(position: Position, size: Size): Unit = synchronized {
+    assume(usageCount >= 1)
+    assume(size.value <= Int.MaxValue)
+    val bytes = new Array[Byte](size.value.toInt)
+    randomAccessFile.seek(position.value + DataFile2.headerBytes)
+    val read = fillFrom(randomAccessFile, bytes, 0, bytes.length)
+    updateDataPrint(bytes, 0, read)
+    randomAccessFile.seek(position.value + DataFile2.headerBytes)
+    randomAccessFile.write(new Array[Byte](size.value.toInt))
+    assume(read == size.value)
   }  
 }
