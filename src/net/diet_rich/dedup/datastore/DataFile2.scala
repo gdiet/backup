e8fab2c7473c5c1b19dd4ab1549a1e694b4cf7f4
die <die@localhost>
2013-02-22 17:53:53 +0100

    (1939/7915) Simpler single-threaded approach for DataFile2

--- src/net/diet_rich/dedup/datastore/DataFile2.scala
+++ src/net/diet_rich/dedup/datastore/DataFile2.scala
@@ -3,133 +3,71 @@
 // http://www.opensource.org/licenses/mit-license.php
 package net.diet_rich.dedup.datastore
 
-import java.io._
+import java.io.{File, RandomAccessFile}
 import net.diet_rich.util.io.fillFrom
-import net.diet_rich.util.vals._
-import DataFile2.headerBytes
+import DataFile2._
 
-// FIXME rename
 object DataFile2 {
   val headerBytes = 16
-}
-
-class DataFile2(protected val position: Position, protected val file: File) {
-
-  protected final def randomAccessFile: RandomAccessFile = maybeFileAccess.getOrElse(initializeFile)
-  protected final def maybeFileAccess: Option[RandomAccessFile] = maybeFileAccessor 
-  protected final def usageCount: Int = usageCounter
-  protected final def dataPrint: Long = dataPrintVar
   
-  private var maybeFileAccessor: Option[RandomAccessFile] = None
-  private var usageCounter: Int = 0
-  private var dataPrintVar: Long = 0
-
-  // is overridden in DataFileWrite
-  protected def internalInitializeFile = new RandomAccessFile(file, "r")
-  
-  protected final def initializeFile = {
-    assume(maybeFileAccess.isEmpty)
-    assume(usageCount == 1)
-    val randomAccessFile = internalInitializeFile
-    randomAccessFile.seek(0)
-    dataPrintVar = randomAccessFile.readLong()
-    assume (randomAccessFile.readLong() == position.value)
-    maybeFileAccessor = Some(randomAccessFile)
-    randomAccessFile
-  }
-
-  final def isDataPrintOK: Boolean = synchronized ({
-    assume(usageCount >= 1)
-    val bytes = new Array[Byte](randomAccessFile.length().toInt - headerBytes)
-    randomAccessFile.seek(headerBytes)
-    fillFrom(randomAccessFile, bytes, 0, bytes.length)
-    calcDataPrint(dataPrint, bytes, 0, bytes.length) == 0
-  })
-  
-  protected final def calcDataPrint(seed: Long, bytes: Array[Byte], offset: Int, size: Int): Long = {
-    var print = seed
-    for (n <- offset until offset + size)
-      print = print ^ (n + 1) * 5870203405204807807L * bytes(n)
+  def calcDataPrint(offsetInFileData: Int, bytes: Array[Byte], offsetInArray: Int, size: Int): Long = {
+    var print: Long = 0L
+    for (n <- 0 until size)
+      print = print ^ (n + offsetInFileData + 1) * 5870203405204807807L * bytes(n + offsetInArray)
     print
   }
-  
-  protected final def updateDataPrint(bytes: Array[Byte], offset: Int, size: Int) =
-    dataPrintVar = calcDataPrint(dataPrint, bytes, offset, size)
-  
-  final def acquire = synchronized {
-    assume(usageCount >= 0)
-    usageCounter = usageCounter + 1
-  }
-
-  final def release = synchronized {
-    assume(usageCount >= 1)
-    usageCounter = usageCounter - 1
-  }
-  
-  // is overridden in DataFileWrite
-  def close = synchronized {
-    assume(usageCount == 0)
-    maybeFileAccess.foreach(_.close)
-    maybeFileAccessor = None
-  }
-
-  final def closeIfUnused = synchronized {
-    assume(usageCount >= 0)
-    if (usageCount == 0) close
-  }
-  
-  final def isClosedAndUnused = synchronized {
-    assume(usageCount >= 0)
-    maybeFileAccess.isEmpty && usageCount == 0
-  }
 }
 
-trait DataFileRead extends DataFile2 {
-  final def read(position: Long, bytes: Array[Byte], offset: Position, size: Size): Int = synchronized {
-    assume(usageCount >= 1)
-    assume(offset.value <= Int.MaxValue)
-    assume(size.value <= Int.MaxValue)
-    randomAccessFile.seek(position + headerBytes)
-    fillFrom(randomAccessFile, bytes, offset.value toInt, size.value toInt)
-  }
-}
+class DataFile2(dataFileNumber: Long, file: File, readonly: Boolean) {
 
-trait DataFileWrite extends DataFile2 {
-  final override def close = synchronized {
-    maybeFileAccess.foreach { fileAccessor =>
-      fileAccessor.seek(0)
-      fileAccessor.writeLong(dataPrint)
+  private var print: Long = 0L
+  
+  protected val randomAccessFile: RandomAccessFile = {
+    if (readonly) {
+      val fileAccess = new RandomAccessFile(file, "r")
+      assume(dataFileNumber == fileAccess.readLong)
+      fileAccess
+    } else {
+      val isNewFile = !file.exists()
+      val fileAccess = new RandomAccessFile(file, "rw")
+      if (isNewFile) {
+        fileAccess.writeLong(dataFileNumber)
+      } else {
+        assume(dataFileNumber == fileAccess.readLong)
+        fileAccess.seek(8)
+        print = fileAccess.readLong
+      }
+      fileAccess
     }
-    super.close
   }
   
-  override final protected def internalInitializeFile = {
-    val alreadyExisted = file.isFile
-    if (!alreadyExisted) file.getParentFile.mkdirs
-    val fileAccessor = new RandomAccessFile(file, "rw")
-    fileAccessor.seek(8)
-    if (!alreadyExisted) fileAccessor.writeLong(position.value)
-    fileAccessor
+  def close = {
+    if (!readonly) {
+      randomAccessFile.seek(8)
+      randomAccessFile.writeLong(print)
+    }
+    randomAccessFile.close
   }
 
-  final def writeNewData(position: Long, bytes: Array[Byte], offset: Position, size: Size): Unit = synchronized {
-    assume(usageCount >= 1)
-    assume(offset.value <= Int.MaxValue)
-    assume(size.value <= Int.MaxValue)
-    randomAccessFile.seek(position + headerBytes)
-    randomAccessFile.write(bytes, offset.value toInt, size.value toInt)
-    updateDataPrint(bytes, offset.value.toInt, size.value.toInt)
+  def writeNewData(offsetInFileData: Int, bytes: Array[Byte], offsetInArray: Int, size: Int, printOfBytes: Long): Unit = {
+    randomAccessFile.seek(offsetInFileData + headerBytes)
+    randomAccessFile.write(bytes, offsetInArray, size)
+    print = print ^ printOfBytes
+  }
+
+  def eraseData(offsetInFileData: Int, size: Int): Unit =
+    overwriteData(offsetInFileData, new Array[Byte](size), 0, size, 0L)
+  
+  def overwriteData(offsetInFileData: Int, bytesToWrite: Array[Byte], offsetInArray: Int, size: Int, printOfBytes: Long): Unit = {
+    val bytesToRead = new Array[Byte](size)
+    val numRead = read(offsetInFileData, bytesToRead, 0, size)
+    print = print ^ calcDataPrint(offsetInFileData, bytesToRead, 0, numRead)
+    writeNewData(offsetInArray, bytesToWrite, offsetInArray, size, printOfBytes)
+  }
+
+  def read(offsetInFileData: Int, bytesToRead: Array[Byte], offsetInArray: Int, size: Int): Int = {
+    randomAccessFile.seek(offsetInFileData + headerBytes)
+    fillFrom(randomAccessFile, bytesToRead, offsetInArray, size)
   }
   
-  final def eraseData(position: Position, size: Size): Unit = synchronized {
-    assume(usageCount >= 1)
-    assume(size.value <= Int.MaxValue)
-    val bytes = new Array[Byte](size.value.toInt)
-    randomAccessFile.seek(position.value + DataFile2.headerBytes)
-    val read = fillFrom(randomAccessFile, bytes, 0, bytes.length)
-    updateDataPrint(bytes, 0, read)
-    randomAccessFile.seek(position.value + DataFile2.headerBytes)
-    randomAccessFile.write(new Array[Byte](size.value.toInt))
-    assume(read == size.value)
-  }  
 }
--- src/net/diet_rich/dedup/datastore/DataStore2.scala
+++ src/net/diet_rich/dedup/datastore/DataStore2.scala
@@ -3,90 +3,83 @@
 // http://www.opensource.org/licenses/mit-license.php
 package net.diet_rich.dedup.datastore
 
-import java.io._
-import net.diet_rich.util.io._
-import net.diet_rich.util.vals._
-
-class DataStore2[DFile <: DataFile2] private (baseDir: File, dataSize: Size, dFileFactory: (Position, File) => DFile) { import DataStore2._
-
-  private val dataDir = baseDir.child(dirName)
-  
-  private def pathInDataDir(position: Position) =
-    f"${position.value / dataSize.value}%010X".grouped(2).mkString("/")
-
-  private def dataFile(pathInDir: String) = dataDir.child(pathInDir)
-
-  private val dataFiles = collection.mutable.Map[String, DFile]()
-  private val fileQueue = collection.mutable.Queue[String]()
-
-  private def newDataFile(position: Position, pathInDir: String) = {
-    val file = dFileFactory(position, dataFile(pathInDir))
-    dataFiles.put(pathInDir, file)
-    file
-  }
-
-  private def acquireDataFile(position: Position, pathInDir: String) = {
-    val file = dataFiles.get(pathInDir).getOrElse(newDataFile(position, pathInDir))
-    file.acquire
-    file
-  }
-  
-  private def closeSurplusFiles = {
-    synchronized {
-      if (fileQueue.size > concurrentDataFiles) {
-        val path = fileQueue.dequeue
-        Some((path, dataFiles(path)))
-      } else None
-    } match {
-      case Some((path, file)) =>
-        file.closeIfUnused
-        synchronized { file.synchronized {
-          if (file.isClosedAndUnused)  {
-            dataFiles.remove(path)
-          } else {
-            fileQueue.enqueue(path)
-          }
-        } }
-      case _ => Unit
-    }
-  }
-
-  protected final def readWrite[Result](func: (DFile, Long, Array[Byte], Position, Size) => Result)
-    (position: Position, bytes: Array[Byte], offset: Position, size: Size): Result = {
-      assume((position.value % dataSize.value) + size.value <= dataSize.value, s"position: $position / data size: $dataSize / size: $size")
-      val path = pathInDataDir(position)
-      val file = synchronized(acquireDataFile(position, path))
-      try {
-        closeSurplusFiles
-        val dataOffset = position.value % dataSize.value
-        func(file, dataOffset, bytes, offset, size)
-      } finally {
-        file.release
-      }
-    }
-  
-  final def shutdown: Unit = synchronized {
-    dataFiles.values.foreach(_.close)
-    dataFiles.clear
-    fileQueue.clear
-  }
-}
-
-trait ReadOnlyDataStore extends DataStore2[DataFileRead] {
-  final def readFromSingleDataFile =
-    readWrite((file, dataOffset, bytes, offset, size) => file.read(dataOffset, bytes, offset, size)) _
-}
-
-trait WriteDataStore extends DataStore2[DataFileWrite] {
-  final def writeToSingleDataFile =
-    readWrite((file, dataOffset, bytes, offset, size) => file.writeNewData(dataOffset, bytes, offset, size)) _
-}
+import java.io.File
+import java.util.concurrent.{Callable, ExecutionException, Executors}
+import net.diet_rich.util.io.EnhancedFile
+import net.diet_rich.util.vals.Position
 
 object DataStore2 {
   val dirName = "data"
   val concurrentDataFiles = 30
-  def readOnly(baseDir: File, dataSize: Size) =
-    new DataStore2(baseDir, dataSize, (position, file) => new DataFile2(position, file) with DataFileRead) with ReadOnlyDataStore
-  def writeOnly(baseDir: File, dataSize: Size) =
-    new DataStore2(baseDir, dataSize, (position, file) => new DataFile2(position, file) with DataFileWrite) with WriteDataStore
+}
+
+class DataStore2(baseDir: File, dataSize: Int, readonly: Boolean) { import DataStore2._
+
+  private val executor = Executors.newSingleThreadExecutor()
+  private def execute[T](f: => T): T = try {
+    executor.submit(new Callable[T] { override def call() = f }).get()
+  } catch { case e: ExecutionException => throw e.getCause }
+
+  private val dataDir: File = baseDir.child(dirName)
+  private def pathInDataDir(dataFileNumber: Long) = f"$dataFileNumber%010X".grouped(2).mkString("/")
+  private def dataFile(dataFileNumber: Long): File = dataDir.child(pathInDataDir(dataFileNumber))
+  
+  private val dataFileHandlers = collection.mutable.LinkedHashMap[Long, DataFile2]()
+
+  def shutdown: Unit = {
+    executor.shutdown
+    executor.awaitTermination(Long.MaxValue, java.util.concurrent.TimeUnit.DAYS)
+    dataFileHandlers.values.foreach(_.close)
+  }
+
+  private def acquireDataFile(dataFileNumber: Long) = {
+    val dataFileHandler =
+      dataFileHandlers.remove(dataFileNumber)
+      .getOrElse(new DataFile2(dataFileNumber, dataFile(dataFileNumber), readonly))
+    dataFileHandlers.put(dataFileNumber, dataFileHandler)
+    if (dataFileHandlers.size > concurrentDataFiles)
+      dataFileHandlers.remove(dataFileHandlers.keys.head).get.close
+    dataFileHandler
+  }
+
+  private def dataFileNumberAndOffset(position: Position, size: Int) = {
+    val dataFileNumber = position.value / dataSize
+    val offsetInFileData = (position.value % dataSize).toInt
+    assume(offsetInFileData + size <= dataSize, s"offsetInFileData: $offsetInFileData / dataSize: $dataSize / size: $size")
+    (dataFileNumber, offsetInFileData)
+  }
+  
+  def eraseDataInSingleDataFile(position: Position, size: Int): Unit = {
+    val (dataFileNumber, offsetInFileData) = dataFileNumberAndOffset(position, size)
+    execute {
+      val dataFileHandler = acquireDataFile(dataFileNumber)
+      dataFileHandler.eraseData(offsetInFileData, size)
+    }
+  }
+  
+  def overwriteDataInSingleDataFile(position: Position, bytes: Array[Byte], offsetInArray: Int, size: Int): Unit = {
+    val (dataFileNumber, offsetInFileData) = dataFileNumberAndOffset(position, size)
+    val dataPrint = DataFile2.calcDataPrint(offsetInFileData, bytes, offsetInArray, size)
+    execute {
+      val dataFileHandler = acquireDataFile(dataFileNumber)
+      dataFileHandler.overwriteData(offsetInFileData, bytes, offsetInArray, size, dataPrint)
+    }
+  }
+  
+  def writeNewDataToSingleDataFile(position: Position, bytes: Array[Byte], offsetInArray: Int, size: Int): Unit = {
+    val (dataFileNumber, offsetInFileData) = dataFileNumberAndOffset(position, size)
+    val dataPrint = DataFile2.calcDataPrint(offsetInFileData, bytes, offsetInArray, size)
+    execute {
+      val dataFileHandler = acquireDataFile(dataFileNumber)
+      dataFileHandler.writeNewData(offsetInFileData, bytes, offsetInArray, size, dataPrint)
+    }
+  }
+  
+  def readFromSingleDataFile(position: Position, bytes: Array[Byte], offsetInArray: Int, size: Int): Unit = {
+    val (dataFileNumber, offsetInFileData) = dataFileNumberAndOffset(position, size)
+    execute {
+      val dataFileHandler = acquireDataFile(dataFileNumber)
+      dataFileHandler.read(offsetInFileData, bytes, offsetInArray, size)
+    }
+  }
 }
