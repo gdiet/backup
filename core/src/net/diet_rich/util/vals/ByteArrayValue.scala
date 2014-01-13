10c65812786b6ebae5b9b2cc995140f6492f58b1
die <die@localhost>
2014-01-13 07:45:46 +0100

    starting sbt build and webdav server

--- core/src/net/diet_rich/util/vals/ByteArrayValue.scala
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Georg Dietrich
-// Licensed under the MIT license:
-// http://www.opensource.org/licenses/mit-license.php
-package net.diet_rich.util.vals
-
-trait ByteArrayValue extends TypedValue[Array[Byte]]
--- core/src/net/diet_rich/util/vals/Time.scala
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Georg Dietrich
-// Licensed under the MIT license:
-// http://www.opensource.org/licenses/mit-license.php
-package net.diet_rich.util.vals
-
-case class Time(value: Long) extends OrderedLongValue[Time]
