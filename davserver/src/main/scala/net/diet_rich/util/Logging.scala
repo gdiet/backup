e30d728d8160806ce7d4f6ec0d1750e1653f0ebd
die <die@localhost>
2014-01-18 21:37:22 +0100

    use byName parameters for logging calls, evaluate only if logging level enabled

--- davserver/src/main/scala/net/diet_rich/util/Logging.scala
+++ davserver/src/main/scala/net/diet_rich/util/Logging.scala
@@ -3,8 +3,18 @@
 // http://www.opensource.org/licenses/mit-license.php
 package net.diet_rich.util
 
-import org.slf4j.LoggerFactory
+import org.slf4j._
 
 trait Logging {
-  protected val log = LoggerFactory getLogger (this getClass)
+  protected val log = new Logging.LogAdapter(LoggerFactory getLogger (this getClass))
+}
+
+object Logging {
+  class LogAdapter(private val logger: Logger) {
+    def error(message: => String) = if (logger isErrorEnabled) logger error message
+    def warn (message: => String) = if (logger isWarnEnabled ) logger warn  message
+    def info (message: => String) = if (logger isInfoEnabled ) logger info  message
+    def debug(message: => String) = if (logger isDebugEnabled) logger debug message
+    def trace(message: => String) = if (logger isTraceEnabled) logger trace message
+  }
 }
