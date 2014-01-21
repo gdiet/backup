01f96295ad4b3f9bd5b605bc8902581684d76659
die <die@localhost>
2014-01-21 07:52:58 +0100

    create-play-repo uses xitrum, too

--- create-play-repo.bat
+++ create-play-repo.bat
@@ -1,11 +1,13 @@
 cd %~dp0
 
+call e:\georg\bin\sbt\bin\sbt.bat xitrumPackage
+
 set repo=target\playRepo
 
 rd /S /Q %repo%
 md %repo%
 
-set classpath=lib_managed/jars/org.scala-lang/scala-library/*;lib_managed/jars/com.h2database/h2/*;target/scala-2.10/dedup_2.10-0.03-SNAPSHOT.jar 
+set classpath=target/xitrum/lib/*
 
 java -cp %classpath% net.diet_rich.dedup.repository.Create -r %repo% -g n
 java -cp %classpath% net.diet_rich.dedup.backup.Backup -r %repo% -g n -s src -t /src -i n
