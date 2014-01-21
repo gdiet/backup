29c6606f4cb8b77f8424d84fa87b3bf5032a12e0
die <die@localhost>
2014-01-21 09:39:30 +0100

    using JAVA_HOME in batch file

--- davserver/start-server.bat
+++ davserver/start-server.bat
@@ -9,7 +9,15 @@ if "%SBT_HOME%"=="" (
 	exit /b -1
 )
 
+if "%JAVA_HOME%"=="" (
+	echo.
+	echo SBT_HOME is not set, exiting...
+	echo.
+	pause
+	exit /b -1
+)
+
 cd ..
 call "%SBT_HOME%\bin\sbt.bat" ;xitrumPackage ;davserver/xitrumPackage
 cd davserver
-start "davserver" java -cp target/xitrum/lib/* net.diet_rich.dedup.webdav.ServerApp
+start "davserver" "%JAVA_HOME%\bin\java.exe" -cp target/xitrum/lib/* net.diet_rich.dedup.webdav.ServerApp
