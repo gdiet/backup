c6aa64a6bcc48c7f6ed3b9713a6322b893c0447e
die <die@localhost>
2014-05-24 19:01:18 +0200

    added print calculation method

--- core/src/main/scala/net/diet_rich/dedup/core/values/Print.scala
+++ core/src/main/scala/net/diet_rich/dedup/core/values/Print.scala
@@ -3,4 +3,12 @@
 // http://www.opensource.org/licenses/mit-license.php
 package net.diet_rich.dedup.core.values
 
+import net.diet_rich.dedup.util.{Bytes, init}
+
 case class Print(val value: Long) extends LongValue
+
+object Print extends (Long => Print) {
+  import java.util.zip.{Adler32, CRC32}
+  def apply(bytes: Bytes): Print =
+    Print(init(new Adler32){_ update bytes}.getValue << 32 | init(new CRC32){_ update bytes}.getValue)
+}
