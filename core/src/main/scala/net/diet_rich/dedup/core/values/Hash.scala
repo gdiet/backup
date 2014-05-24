1bc7b4767e2274e2061c577fcbd3aee4a0c0cebe
die <die@localhost>
2014-05-24 18:59:20 +0200

    added hash calculation methods

--- core/src/main/scala/net/diet_rich/dedup/core/values/Hash.scala
+++ core/src/main/scala/net/diet_rich/dedup/core/values/Hash.scala
@@ -3,9 +3,27 @@
 // http://www.opensource.org/licenses/mit-license.php
 package net.diet_rich.dedup.core.values
 
+import java.security.MessageDigest
+
+import net.diet_rich.dedup.util.Bytes
+
 case class Hash(value: Array[Byte]) {
   def !==(a: Hash) = ! ===(a)
   def ===(a: Hash) = java.util.Arrays.equals(value, a.value)
   override def equals(a: Any) = throw new UnsupportedOperationException("use === to compare hash contents")
   override def toString = s"Hash(${value map ("%02X" format _) mkString})"
+}
+
+object Hash extends (Array[Byte] => Hash) {
+  def algorithmChecked(algorithm: String): String = {
+    MessageDigest getInstance algorithm
+    algorithm
+  }
+  def digestLength(algorithm: String): Int =
+    MessageDigest getInstance algorithm getDigestLength
+  def calculate(algorithm: String)(data: Iterator[Bytes]): (Hash, Size) = {
+    val digester = MessageDigest getInstance algorithm
+    val numberOfBytes = data map { bytes => digester update bytes; bytes.length.toLong } sum;
+    (Hash(digester.digest), Size(numberOfBytes))
+  }
 }
