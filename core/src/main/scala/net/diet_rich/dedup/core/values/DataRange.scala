4be82604e78734ac4122d785853b39cf833a33c0
die <die@localhost>
2014-05-24 18:58:09 +0200

    added Position and DataRange for byte store

---
+++ core/src/main/scala/net/diet_rich/dedup/core/values/DataRange.scala
@@ -0,0 +1,6 @@
+// Copyright (c) Georg Dietrich
+// Licensed under the MIT license:
+// http://www.opensource.org/licenses/mit-license.php
+package net.diet_rich.dedup.core.values
+
+case class DataRange(start: Position, fin: Position)
