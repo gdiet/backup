94c81df9fb261fc62d0f6c429f21110902d500d9
die <die@localhost>
2014-07-15 08:41:10 +0200

    first implementation of read in DataStorePart

--- core/src/main/scala/net/diet_rich/dedup/core/data/DataBackend.scala
+++ core/src/main/scala/net/diet_rich/dedup/core/data/DataBackend.scala
@@ -34,15 +34,20 @@ trait DataStorePart extends DataBackendSlice with Lifecycle { _: DataSettingsSli
 
   object dataBackend extends DataBackend {
     override def read(entry: DataRange): Iterator[Bytes] = {
-      ???
+      val initialDataFileNumber = entry.start / dataSettings.blocksize
+      dataFileDistributionFor(initialDataFileNumber, entry.start, entry.size, Nil).iterator.map {
+        case (dataFileNumber, offsetInFile, currentSize) =>
+          execute(dataFileNumber){ _ read (offsetInFile.value, Bytes zero currentSize) }
+      }
     }
+
     override def write(data: Bytes, start: Position): Unit = {
       val initialDataFileNumber = start / dataSettings.blocksize
       dataFileDistributionFor(initialDataFileNumber, start, data.size, Nil).foldLeft (data) {
         case (remainingData, (dataFileNumber, offsetInFile, currentSize)) =>
           val currentChunk = remainingData withSize currentSize
           val printOfCurrentChunk = DataFile.calcDataPrint(offsetInFile.value, currentChunk) // calculated outside of the store thread!
-          execute(dataFileNumber){ dataFileHandler(dataFileNumber) writeData(offsetInFile.value, currentChunk, printOfCurrentChunk) }
+          execute(dataFileNumber){ _ writeData(offsetInFile.value, currentChunk, printOfCurrentChunk) }
           remainingData withOffset currentSize
       }
     }
@@ -56,13 +61,14 @@ trait DataStorePart extends DataBackendSlice with Lifecycle { _: DataSettingsSli
     private val executors = Array.fill(dataSettings storeThreads)(Executors.newSingleThreadExecutor)
     private def threadNumber(dataFileNumber: Long): Int = (dataFileNumber % dataSettings.storeThreads).toInt
     private def executor(dataFileNumber: Long) = executors(threadNumber(dataFileNumber))
-    private def execute[T](dataFileNumber: Long)(f: => T): T = resultOf(Future(f)(ExecutionContext fromExecutorService executor(dataFileNumber)))
+    private def execute[T](dataFileNumber: Long)(f: DataFile => T): T =
+      resultOf(Future(f(dataFileHandler(dataFileNumber)))(ExecutionContext fromExecutorService executor(dataFileNumber)))
 
     private def pathInDataDir(dataFileNumber: Long) = f"$dataFileNumber%010X" grouped 2 mkString "/"
     private def dataFile(dataFileNumber: Long): File = new File(dataSettings.dataDir, pathInDataDir(dataFileNumber))
 
     private val dataFiles = Array.fill(dataSettings storeThreads)(scala.collection.mutable.LinkedHashMap[Long, DataFile]())
-    private def dataFileHandler(dataFileNumber: Long) = {
+    private def dataFileHandler(dataFileNumber: Long): DataFile = {
       val dataFilesMap = dataFiles(threadNumber(dataFileNumber))
       val dataFileHandler = dataFilesMap remove dataFileNumber getOrElse new DataFile(dataFileNumber, dataFile(dataFileNumber), dataSettings readonly)
       if (dataFilesMap.size >= dataSettings.fileHandlesPerThread) dataFilesMap.remove(dataFilesMap.keys.head).get close()
