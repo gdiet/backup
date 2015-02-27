7cf75309f43c65c03226b01a803e043d1bace670
die <die@localhost>
2015-02-27 23:15:45 +0100

    continued implementation of Repository

--- src/main/scala/net/diet_rich/dedup/core/Repository.scala
+++ src/main/scala/net/diet_rich/dedup/core/Repository.scala
@@ -1,7 +1,10 @@
 package net.diet_rich.dedup.core
 
+import java.io.IOException
+
 import net.diet_rich.dedup.core.data._
-import net.diet_rich.dedup.core.meta.{RangesQueue, MetaBackend}
+import net.diet_rich.dedup.core.meta.{TreeEntry, RangesQueue, MetaBackend}
+import net.diet_rich.dedup.util.now
 
 object Repository {
   val PRINTSIZE = 8192
@@ -9,7 +12,7 @@ object Repository {
 
 class Repository(metaBackend: MetaBackend, dataBackend: DataBackend, freeRanges: RangesQueue, hashAlgorithm: String, storeMethod: Int, storeThreads: Int) {
 
-  val storeLogic: StoreLogicBackend = new StoreLogic(metaBackend, ???, freeRanges, hashAlgorithm, storeMethod, storeThreads)
+  protected val storeLogic: StoreLogicBackend = new StoreLogic(metaBackend, dataBackend.write _, freeRanges, hashAlgorithm, storeMethod, storeThreads)
 
   def close(): Unit = {
     storeLogic close()
@@ -24,4 +27,21 @@ class Repository(metaBackend: MetaBackend, dataBackend: DataBackend, freeRanges:
     metaBackend.storeEntries(dataid).iterator
       .flatMap { case (start, fin) => dataBackend read (start, fin) }
 
+  def createUnchecked(parent: Long, name: String, source: Option[Source] = None, time: Option[Long] = Some(now)): TreeEntry =
+    metaBackend.createUnchecked(parent, name, time, source map storeLogic.dataidFor)
+
+  def create(parent: Long, name: String, source: Option[Source] = None, time: Option[Long] = Some(now)): TreeEntry = metaBackend.inTransaction {
+    val created = metaBackend.create(parent, name, time)
+    try {
+      metaBackend.change(created.id, parent, name, time, source map storeLogic.dataidFor)
+      .getOrElse(throw new IOException("failed to update created file with data entry"))
+    } catch {
+      case e: IOException =>
+        if (metaBackend.markDeleted(created.id)) throw e
+        else throw new IOException("failed to delete partially created file", e)
+    }
+  }
+
+  def createOrReplace(parent: Long, name: String, source: Option[Source] = None, time: Option[Long] = Some(now)): TreeEntry =
+    metaBackend.createOrReplace(parent, name, time, source map storeLogic.dataidFor)
 }
