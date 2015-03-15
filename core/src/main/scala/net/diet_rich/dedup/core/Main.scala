156449fbc1b9c33d143ba87adfb3a7d172703cde
die <die@localhost>
2015-03-15 18:16:05 +0100

    backup and restore: removing shutdown hook when job is finished

--- core/src/main/scala/net/diet_rich/dedup/core/Main.scala
+++ core/src/main/scala/net/diet_rich/dedup/core/Main.scala
@@ -3,10 +3,10 @@ package net.diet_rich.dedup.core
 import java.io.File
 
 import net.diet_rich.dedup.core.data.StoreMethod
-import net.diet_rich.dedup.util.CommandLineUtils
+import net.diet_rich.dedup.util.{Logging, CommandLineUtils}
 import net.diet_rich.dedup.util.io._
 
-object Main extends App {
+object Main extends App with Logging {
   if (args.length < 2) println("arguments: <command> <repository> [key:value options]") else {
     val commandLineUtils = CommandLineUtils(args)
     import commandLineUtils._
@@ -26,12 +26,9 @@ object Main extends App {
         val storeMethod = optional("storeMethod") map StoreMethod.named
         val versionComment = optional("versionComment")
         using(Repository openReadWrite (repositoryDir, storeMethod, parallel, versionComment)) { repository =>
-          sys addShutdownHook {
-            println("shutdown requested - shutting down dedup system before backup is complete")
-            repository close()
-            println("shutdown of dedup system complete")
+          wrapInShutdownHook(repository) {
+            using(new BackupAlgorithm(repository, parallel)) { _ backup (source, target) }
           }
-          using(new BackupAlgorithm(repository, parallel)) { _ backup (source, target) }
         }
 
       case "restore" =>
@@ -41,15 +38,22 @@ object Main extends App {
         require(target.isDirectory, s"Target $target must be a directory")
         require(target.listFiles.isEmpty, s"Target directory $target must be empty")
         using(Repository openReadOnly repositoryDir) { repository =>
-          sys addShutdownHook {
-            println("shutdown requested - shutting down dedup system before restore is complete")
-            repository close()
-            println("shutdown of dedup system complete")
+          wrapInShutdownHook(repository) {
+            using(new RestoreAlgorithm(repository)) { _ restore(source, target) }
           }
-          using(new RestoreAlgorithm(repository)) { _ restore(source, target) }
         }
 
       case _ => println(s"unknown command $command")
     }
+  }
+
+  def wrapInShutdownHook(closable: AutoCloseable)(task: => Unit) = {
+    val hook = sys addShutdownHook {
+      log warn "shutdown requested - shutting down dedup system before job is completed"
+      closable close()
+      log info "shutdown of dedup system complete"
+    }
+    try task
+    finally hook remove()
   }
 }
