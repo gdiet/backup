a2ee14e6049c18866748344cbe4e45393d043c1d
georg <georg@localhost>
2015-11-02 13:00:50 +0100

    store output stream without large internal cache

--- DedupFS/src/main/scala/net/diet_rich/dedupfs/StoreOutputStream.scala
+++ DedupFS/src/main/scala/net/diet_rich/dedupfs/StoreOutputStream.scala
@@ -2,31 +2,42 @@ package net.diet_rich.dedupfs
 
 import java.io.{IOException, OutputStream}
 import java.util.Arrays.copyOfRange
+import java.util.concurrent.ArrayBlockingQueue
 
 import net.diet_rich.common._, io.Source
 
-class StoreOutputStream(storeLogic: StoreLogic, maxBytesToCache: Long, processDataid: Long => Unit) extends OutputStream {
+class StoreOutputStream(storeLogic: StoreLogic, processDataid: Long => Unit) extends OutputStream {
   private var closed = false
-  private var cached = 0L
-  private var cache = Vector[Bytes]()
+  private val transfer = new ArrayBlockingQueue[Bytes](8)
   override def write(byte: Int): Unit = write(Array(byte.toByte), 0, 1)
   override def write(data: Array[Byte]): Unit = write(data, 0, data.length)
   override def write(data: Array[Byte], offset: Int, length: Int): Unit = {
     if (closed) throw new IOException("Output stream is already closed")
-    if (cached + length > maxBytesToCache) throw new IOException("Output stream exceeded cache size limit") // TODO implement plan B
-    if (length > 0) cache = cache :+ Bytes(copyOfRange(data, offset, offset+length), 0, length)
+    if (length > 0) transfer put Bytes(copyOfRange(data, offset, offset+length), 0, length)
   }
   override def close(): Unit = if (!closed) {
     closed = true
+    transfer put Bytes.empty
     processDataid(storeLogic dataidFor theSource)
   }
+
+  protected val dataid = storeLogic futureDataidFor theSource
+
   private object theSource extends Source {
-    override def read(count: Int): Bytes = {
-      cache.headOption match {
-        case None => Bytes.empty
-        case Some(bytes) =>
-          if (bytes.length <= count) bytes
-          else valueOf(bytes.withLength(count)) before {cache = bytes.addOffset(count) +: cache.tail}
+    protected var pool: Array[Byte] = Array()
+    @annotation.tailrec
+    override final def read(count: Int): Bytes = {
+      if (pool.length >= count) {
+        valueOf(Bytes(pool, 0, count)) before { pool = pool drop count }
+      } else {
+        transfer.take() match {
+          case Bytes.empty =>
+            transfer put Bytes.empty // Allows calling read multiple times when no more data available
+            valueOf(Bytes(pool, 0, pool.length)) before { pool = Array() }
+          case newData =>
+            pool = pool ++ newData.asByteArray
+            read(count)
+        }
       }
     }
   }
