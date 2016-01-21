0bf6358782e86cf1bfd0821488b931ac21e043d8
die <die@localhost>
2016-01-21 22:11:31 +0100

    FIXMEs reviewed

--- DedupFS/src/main/scala/net/diet_rich/dedupfs/metadata/sql/MetadataReadSQL.scala
+++ DedupFS/src/main/scala/net/diet_rich/dedupfs/metadata/sql/MetadataReadSQL.scala
@@ -6,12 +6,11 @@ import net.diet_rich.dedupfs.metadata.{TreeEntry, MetadataReadBasic}
 
 case class TreeQueryResult(treeEntry: TreeEntry, deleted: Boolean, id: Long, timeOfEntry: Timestamp)
 
+// FIXME test
 /** Metadata read extension methods specific for this file system implementation. */
 trait MetadataReadSQL { _: MetadataReadBasic =>
   /** @return All entries, including deleted and historical entries. */
   def treeEntriesFor(key: Long): Iterator[TreeQueryResult]
   /** @return All entries that have been at one time child of the node, including deleted and historical entries. */
   def treeChildrenOf(parentKey: Long): Iterator[TreeQueryResult]
-  // FIXME methods to access entry ID and timestamp
-  // FIXME test
 }
