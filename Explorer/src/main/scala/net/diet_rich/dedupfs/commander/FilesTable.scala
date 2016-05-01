eb404127fa0ae07731c2bd59a128ec2fe5f460f5
die <die@localhost>
2016-05-01 17:36:42 +0200

    sorting by size keeps directories at top

--- Explorer/src/main/scala/net/diet_rich/dedupfs/commander/FilesTable.scala
+++ Explorer/src/main/scala/net/diet_rich/dedupfs/commander/FilesTable.scala
@@ -1,5 +1,6 @@
 package net.diet_rich.dedupfs.commander
 
+import java.lang.{Long => JLong}
 import java.util.Comparator
 import javafx.beans.property.SimpleObjectProperty
 import javafx.collections.FXCollections
@@ -21,15 +22,21 @@ class FilesTable {
   iconColumn.getStyleClass add "iconColumn"
   iconColumn setCellValueFactory callback(_.getValue.icon)
   iconColumn setCellFactory callback(new ImageTableCell[FilesTableItem](17, 17))
+  iconColumn setSortable false
 
-  private val nameColumn = new TableColumn[FilesTableItem, NameColumnEntry]("Name")
-  nameColumn setCellValueFactory callback(data => new SimpleObjectProperty(NameColumnEntry(data.getValue)))
-  nameColumn setCellFactory EditableTableCellFactory(NameColumnEntry.stringConverter)
-  nameColumn setOnEditCommit handle(event => event.getRowValue.name setValue event.getNewValue.name)
-  nameColumn setComparator NameColumnEntry.comparator(nameColumn.getSortType)
+  private val nameColumn = new TableColumn[FilesTableItem, ColumnEntry[String]]("Name")
+  // TODO extract method for cell value factory
+  nameColumn setCellValueFactory callback(data => new SimpleObjectProperty(ColumnEntry[String](data.getValue.name.getValue, Some(data.getValue.isDirectory))))
+  // TODO not generic - just needed here
+  nameColumn setCellFactory EditableTableCellFactory(ColumnEntry.stringConverter(identity))
+  nameColumn setOnEditCommit handle(event => event.getRowValue.name setValue event.getNewValue.detail)
+  nameColumn setComparator ColumnEntry.comparator(nameColumn.getSortType)
 
-  private val sizeColumn = new TableColumn[FilesTableItem, String]("Size")
-  sizeColumn setCellValueFactory callback(_.getValue.size)
+  private val sizeColumn = new TableColumn[FilesTableItem, ColumnEntry[JLong]]("Size")
+  // TODO extract method for cell value factory
+  sizeColumn setCellValueFactory callback(data => new SimpleObjectProperty(ColumnEntry[JLong](data.getValue.size.getValue, Some(data.getValue.isDirectory))))
+  sizeColumn setCellFactory sizeCellFactory
+  sizeColumn setComparator ColumnEntry.comparator(sizeColumn.getSortType)
 
   val table = new TableView[FilesTableItem](fileSorted)
   fileSorted.comparatorProperty() bind table.comparatorProperty()
@@ -38,46 +45,55 @@ class FilesTable {
   table.getSortOrder add nameColumn
 }
 
-object FilesTable {
-  private case class NameColumnEntry(name: String, maybeDirectory: Option[Boolean]) {
+private object FilesTable {
+  case class ColumnEntry[T](detail: T, maybeDirectory: Option[Boolean]) {
     def isDirectory = maybeDirectory contains true
   }
-  private object NameColumnEntry {
-    def apply(file: FilesTableItem): NameColumnEntry = NameColumnEntry(file.name.getValue, Some(file.isDirectory))
-    val stringConverter = new StringConverter[NameColumnEntry] {
-      override def fromString(string: String): NameColumnEntry = NameColumnEntry(string, None)
-      override def toString(entry: NameColumnEntry): String = entry.name
+  object ColumnEntry {
+    def stringConverter[T](convert: String => T) = new StringConverter[ColumnEntry[T]] {
+      override def fromString(string: String): ColumnEntry[T] = ColumnEntry[T](convert(string), None)
+      override def toString(entry: ColumnEntry[T]): String = entry.detail.toString
     }
-    def comparator(sortType: => SortType) = new Comparator[NameColumnEntry] {
-      override def compare(o1: NameColumnEntry, o2: NameColumnEntry): Int = {
+    def comparator[T <: Comparable[T]](sortType: => SortType) = new Comparator[ColumnEntry[T]] {
+      override def compare(o1: ColumnEntry[T], o2: ColumnEntry[T]): Int = {
         val direction = sortType match {
           case SortType.ASCENDING  =>  1
           case SortType.DESCENDING => -1
         }
+        "".compare("")
         (o1.isDirectory, o2.isDirectory) match {
           case (true,  false) => -direction
           case (false, true ) =>  direction
-          case (true,  true ) =>  direction * o1.name.compare(o2.name)
-          case (false, false) =>              o1.name.compare(o2.name)
+          case (true,  true ) =>  direction * o1.detail.compareTo(o2.detail)
+          case (false, false) =>              o1.detail.compareTo(o2.detail)
         }
       }
     }
   }
 
-  private object EditableTableCellFactory {
-    def apply[T](stringConverter: StringConverter[T]) = new Callback[TableColumn[FilesTableItem, T], TableCell[FilesTableItem, T]] {
-      override def call(param: TableColumn[FilesTableItem, T]): TableCell[FilesTableItem, T] = new TextFieldTableCell[FilesTableItem, T](stringConverter) {
-        override def updateItem(entry: T, isEmpty: Boolean): Unit = {
-          super.updateItem(entry, isEmpty)
-          setEditable(
-            if (getTableRow != null) {
-              val rowIndex = getTableRow.getIndex
-              val items = getTableView.getItems
-              rowIndex >= 0 && rowIndex < items.size() && items.get(rowIndex).isEditable
-            } else false
-          )
-        }
+  case class EditableTableCellFactory[T](stringConverter: StringConverter[T]) extends Callback[TableColumn[FilesTableItem, T], TableCell[FilesTableItem, T]] {
+    override def call(param: TableColumn[FilesTableItem, T]): TableCell[FilesTableItem, T] = new TextFieldTableCell[FilesTableItem, T](stringConverter) {
+      override def updateItem(entry: T, isEmpty: Boolean): Unit = {
+        super.updateItem(entry, isEmpty)
+        setEditable(
+          if (getTableRow != null) {
+            val rowIndex = getTableRow.getIndex
+            val items = getTableView.getItems
+            rowIndex >= 0 && rowIndex < items.size() && items.get(rowIndex).isEditable
+          } else false
+        )
       }
     }
+  }
+
+  val sizeCellFactory = new Callback[TableColumn[FilesTableItem, ColumnEntry[JLong]], TableCell[FilesTableItem, ColumnEntry[JLong]]] {
+    override def call(param: TableColumn[FilesTableItem, ColumnEntry[JLong]]): TableCell[FilesTableItem, ColumnEntry[JLong]] =
+      new TextFieldTableCell[FilesTableItem, ColumnEntry[JLong]](ColumnEntry.stringConverter[JLong](_.toLong)) {
+        override def updateItem(entry: ColumnEntry[JLong], isEmpty: Boolean): Unit = {
+          super.updateItem(entry, isEmpty)
+          // TODO number formatting
+          if (entry == null) setText("") else setText(entry.detail.toString)
+        }
+      }
   }
 }
--- Explorer/src/main/scala/net/diet_rich/dedupfs/commander/FilesTableItem.scala
+++ Explorer/src/main/scala/net/diet_rich/dedupfs/commander/FilesTableItem.scala
@@ -1,11 +1,11 @@
 package net.diet_rich.dedupfs.commander
 
-import javafx.beans.property.Property
+import javafx.beans.property.{LongProperty, Property}
 import javafx.scene.image.Image
 
 trait FilesTableItem {
   def name: Property[String]
-  def size: Property[String]
+  def size: LongProperty
   def icon: Property[Image]
   def isDirectory: Boolean
   def isEditable: Boolean
