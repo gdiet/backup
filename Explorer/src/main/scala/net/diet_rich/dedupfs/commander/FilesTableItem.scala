905a6fdfb2b1cc7c6d5ddc9afc57482b4bddf432
die <die@localhost>
2016-05-02 07:51:54 +0200

    FilesTableItem for physical file

--- Explorer/src/main/scala/net/diet_rich/dedupfs/commander/FilesTableItem.scala
+++ Explorer/src/main/scala/net/diet_rich/dedupfs/commander/FilesTableItem.scala
@@ -1,6 +1,7 @@
 package net.diet_rich.dedupfs.commander
 
-import javafx.beans.property.{LongProperty, Property}
+import java.io.File
+import javafx.beans.property._
 import javafx.scene.image.Image
 
 trait FilesTableItem {
@@ -9,4 +10,16 @@ trait FilesTableItem {
   def icon: Property[Image]
   def isDirectory: Boolean
   def isEditable: Boolean
+}
+
+case class PhysicalFileTableItem(file: File) extends FilesTableItem {
+  override val name: Property[String] = new SimpleStringProperty(file.getName)
+  override val size: LongProperty = new SimpleLongProperty(file.length)
+  override val icon: Property[Image] = new SimpleObjectProperty[Image](if (file.isDirectory) imageFolder else imageFile)
+  override def isDirectory: Boolean = file.isDirectory
+  override def isEditable: Boolean = file.canWrite
+}
+
+object FilesTableItem {
+  def apply(file: File): FilesTableItem = PhysicalFileTableItem(file)
 }
