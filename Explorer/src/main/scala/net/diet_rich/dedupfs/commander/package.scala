b74d09f51c56184d49702d81d672aa4553770e56
die <die@localhost>
2016-05-05 12:28:57 +0200

    deleted unused code

--- Explorer/src/main/scala/net/diet_rich/dedupfs/commander/package.scala
+++ Explorer/src/main/scala/net/diet_rich/dedupfs/commander/package.scala
@@ -8,11 +8,6 @@ import net.diet_rich.common.init
 package object commander {
   val conf = ConfigFactory.load()
 
-  val imageFile   = new Image(conf getString "icons.file")
-  val imageFolder = new Image(conf getString "icons.folder")
-  val imageReload = new Image(conf getString "icons.reload")
-  val imageUp     = new Image(conf getString "icons.up")
-
   def imageView(key: String): ImageView = init(new ImageView(new Image(conf getString s"$key.icon"))) { imageView =>
     imageView.setFitWidth (conf getDoubleList s"$key.size" get 0)
     imageView.setFitHeight(conf getDoubleList s"$key.size" get 1)
