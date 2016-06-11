8880c741f48f54c801ded6aae8c0ec631a3a2e6a
georg <georg@localhost>
2016-06-11 21:24:31 +0200

    added double explorer pane

---
+++ FileExplorer/src/main/scala/net/diet_rich/dedupfs/explorer/DoubleExplorerPane.scala
@@ -0,0 +1,47 @@
+package net.diet_rich.dedupfs.explorer
+
+import java.util.function.Consumer
+import javafx.scene.control.{Button, SplitPane}
+import javafx.scene.layout.{BorderPane, HBox, Priority}
+import javafx.scene.{Scene, Node, Parent}
+
+import net.diet_rich.common._
+import net.diet_rich.common.fx._
+import net.diet_rich.dedupfs.explorer.filesPane.{FileSystemRegistry, FilesPane}
+
+class DoubleExplorerPane(registry: FileSystemRegistry, initialUrlLeft: String, initialUrlRight: String) {
+  private val leftPane = new FilesPane(registry, initialUrlLeft)
+  private val rightPane = new FilesPane(registry, initialUrlRight)
+  private val splitPane = new SplitPane(leftPane.component, rightPane.component)
+
+  private var activePane = init(leftPane) { _ setActive true }
+  private def otherPane = if (activePane == leftPane) rightPane else leftPane
+  val sceneFocusChangeListener = changeListener { node: Node =>
+    if (nodesUp(node) contains otherPane.component) {
+      activePane setActive false
+      otherPane setActive true
+      activePane = otherPane
+    }
+  }
+  def registerIn(scene: Scene): Unit = scene.focusOwnerProperty addListener sceneFocusChangeListener
+
+  val component: Parent = init(new BorderPane()) { pane =>
+    pane setCenter splitPane
+    pane setBottom init(new HBox()) { bottom =>
+      bottom.getChildren add init (new Button(conf getString "button.rename.label")) { renameButton =>
+        renameButton setOnAction handle(activePane renameSingleSelection())
+      }
+      bottom.getChildren add init (new Button(conf getString "button.copy.label")) { copyButton =>
+      }
+      bottom.getChildren add init (new Button(conf getString "button.move.label")) { moveButton =>
+      }
+      bottom.getChildren forEach new Consumer[Node] {
+        override def accept(node: Node): Unit = node match { case button: Button =>
+          HBox setHgrow (button, Priority.ALWAYS)
+          button setPrefWidth 40
+          button setMaxWidth Double.MaxValue
+        }
+      }
+    }
+  }
+}
