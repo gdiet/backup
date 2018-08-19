22e3ab7a78c25cf113903b0adf28945013ed8068
georg <georg@localhost>
2018-08-19 21:04:16 +0200

    cleanup

--- FuseFS/src/main/scala/net/diet_rich/fusefs/FuseFS.scala
+++ FuseFS/src/main/scala/net/diet_rich/fusefs/FuseFS.scala
@@ -7,36 +7,31 @@ import net.diet_rich.util.ClassLogging
 import ru.serce.jnrfuse.struct.{FileStat, FuseFileInfo, Statvfs}
 import ru.serce.jnrfuse.{ErrorCodes, FuseFillDir, FuseStubFS}
 
-class FuseFS(fs: FileSystem) extends FuseStubFS with ClassLogging {
-  private val OK = 0
-  private val O777 = 511 // octal 0777
-
+class FuseFS(fs: FileSystem) extends FuseStubFS with ClassLogging { import FuseFS._
   // Note: Calling FileStat.toString DOES NOT WORK
-  override def getattr(path: String, stat: FileStat): Int = log(s"getattr($path, fileStat)"){
+  override def getattr(path: String, stat: FileStat): Int = log(s"getattr($path, fileStat)") {
+    stat.st_uid.set(getContext.uid.get)
+    stat.st_gid.set(getContext.gid.get)
     fs.getNode(path) match {
-      case None => -ErrorCodes.ENOENT
+      case None => ENOENT
       case Some(_: Dir) =>
         stat.st_mode.set(FileStat.S_IFDIR | O777)
-        stat.st_uid.set(getContext.uid.get)
-        stat.st_gid.set(getContext.gid.get)
         OK
       case Some(file: File) =>
         stat.st_mode.set(FileStat.S_IFREG | O777)
         stat.st_size.set(file.size)
-        stat.st_uid.set(getContext.uid.get)
-        stat.st_gid.set(getContext.gid.get)
         OK
     }
   }
 
-  override def mkdir(path: String, mode: Long): Int = log(s"mkdir($path, $mode)"){
+  override def mkdir(path: String, mode: Long): Int = log(s"mkdir($path, $mode)") {
     fs.getNode(path) match {
-      case Some(_) => -ErrorCodes.EEXIST
+      case Some(_) => EEXIST
       case None => FileSystem.splitParentPath(path).flatMap {
         case (parent, name) => fs.getNode(parent).collect { case dir: Dir => dir.mkDir(name) }
       } match {
         case Some(true) => 0
-        case _ => -ErrorCodes.ENOENT
+        case _ => ENOENT
       }
     }
   }
@@ -46,30 +41,30 @@ class FuseFS(fs: FileSystem) extends FuseStubFS with ClassLogging {
                        buf: Pointer,
                        filler: FuseFillDir,
                        offset: Long,
-                       fi: FuseFileInfo): Int = log(s"readdir($path, buffer, filler, $offset, fileInfo)"){
+                       fi: FuseFileInfo): Int = log(s"readdir($path, buffer, filler, $offset, fileInfo)") {
     if (offset.toInt < 0 || offset.toInt != offset) -ErrorCodes.EOVERFLOW
-    else
-      fs.getNode(path) match {
-        case None => -ErrorCodes.ENOENT
-        case Some(_: File) => -ErrorCodes.ENOTDIR
-        case Some(dir: Dir) =>
-          def entries = "." #:: ".." #:: dir.list.toStream.map(_.name)
-          entries.zipWithIndex
-            .drop(offset.toInt)
-            .exists {
-              case (entry, k) => filler.apply(buf, entry, null, k + 1) != 0
-            }
-          OK
-      }
+    else fs.getNode(path) match {
+      case None => ENOENT
+      case Some(_: File) => ENOTDIR
+      case Some(dir: Dir) =>
+        def entries = "." #:: ".." #:: dir.list.toStream.map(_.name)
+        entries.zipWithIndex
+          .drop(offset.toInt)
+          .exists {
+            case (entry, k) => filler.apply(buf, entry, null, k + 1) != 0
+          }
+        OK
+    }
   }
 
-  override def statfs(path: String, stbuf: Statvfs): Int = log(s"statfs($path, buffer)"){
+  override def statfs(path: String, stbuf: Statvfs): Int = log(s"statfs($path, buffer)") {
     if (Platform.getNativePlatform.getOS == WINDOWS) {
       // statfs needs to be implemented on Windows in order to allow for copying
       // data from other devices because winfsp calculates the volume size based
       // on the statvfs call.
       // see https://github.com/billziss-gh/winfsp/blob/14e6b402fe3360fdebcc78868de8df27622b565f/src/dll/fuse/fuse_intf.c#L654
       if ("/" == path) {
+        // TODO Eventually, we want something meaningful here.
         stbuf.f_blocks.set(1024 * 1024) // total data blocks in file system
         stbuf.f_frsize.set(1024) // fs block size
         stbuf.f_bfree.set(1024 * 1024) // free blocks in fs
@@ -80,20 +75,26 @@ class FuseFS(fs: FileSystem) extends FuseStubFS with ClassLogging {
 }
 
 object FuseFS extends ClassLogging {
+  private val O777    = 511 // octal 0777
+  private val OK      = 0
+  private val EEXIST  = -ErrorCodes.EEXIST
+  private val ENOENT  = -ErrorCodes.ENOENT
+  private val ENOTDIR = -ErrorCodes.ENOTDIR
+
   def mount(fs: FileSystem): AutoCloseable = {
     new AutoCloseable {
       val fuseFS = new FuseFS(fs)
       try {
+        // TODO Eventually, we want to have to mount point configurable.
         val mountPoint = Platform.getNativePlatform.getOS match {
-          case WINDOWS => "I:\\"
+          case WINDOWS => """I:\"""
           case _ => "/tmp/mntfs"
         }
         log.info(s"mount($mountPoint)")
         fuseFS.mount(java.nio.file.Paths.get(mountPoint), false, false)
-      } catch {
-        case e: Throwable =>
-          log.info(e.getMessage)
-          fuseFS.umount(); throw e
+      } catch { case e: Throwable =>
+        log.info(e.getMessage)
+        fuseFS.umount(); throw e
       }
       override def close(): Unit = fuseFS.umount()
     }
--- Util/src/main/scala/net/diet_rich/util/ClassLogging.scala
+++ Util/src/main/scala/net/diet_rich/util/ClassLogging.scala
@@ -5,6 +5,8 @@ import org.slf4j.{Logger, LoggerFactory}
 trait ClassLogging {
   protected val log: Logger = LoggerFactory.getLogger(getClass)
   protected def log[T](message: => String)(f: => T): T =
-    if (log.isDebugEnabled) init(f)(t => log.debug(s"$message -> $t"))
-    else f
+    if (!log.isDebugEnabled) f
+    else try init(f)(t => log.debug(s"$message -> $t")) catch {
+      case e: Throwable => log.error(s"$message -> $e", e); throw e
+    }
 }
