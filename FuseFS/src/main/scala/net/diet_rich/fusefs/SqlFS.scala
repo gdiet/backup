44e6b125345d67091204ea96f67f80e69f8b4468
die <die@localhost>
2018-10-20 20:16:14 +0200

    avoiding https://issues.scala-lang.org/browse/SI-7645

--- FuseFS/src/main/scala/net/diet_rich/fusefs/SqlFS.scala
+++ FuseFS/src/main/scala/net/diet_rich/fusefs/SqlFS.scala
@@ -20,30 +20,30 @@ class SqlFS extends FileSystem {
   println(root.list.toList)
 
   private def nodeFor(entry: meta.TreeEntry): Node = {
-    trait Common {
-      def name: String = entry.name
-      def renameTo(path: String): RenameResult = {
-        FileSystem.pathElements(path).getOrElse(Seq()).reverse match {
-          case List() => TargetExists
-          case newName :: elements =>
-            meta.entry(elements.reverse) match {
-              case None => TargetParentDoesNotExist
-              case Some(newParent) =>
-                if (newParent.data.nonEmpty) TargetParentNotADirectory
-                else meta.rename(entry.id, newName, newParent.id)
-            }
-        }
+    def rename(path: String): RenameResult = {
+      FileSystem.pathElements(path).getOrElse(Seq()).reverse match {
+        case List() => TargetExists
+        case newName :: elements =>
+          meta.entry(elements.reverse) match {
+            case None => TargetParentDoesNotExist
+            case Some(newParent) =>
+              if (newParent.data.nonEmpty) TargetParentNotADirectory
+              else meta.rename(entry.id, newName, newParent.id)
+          }
       }
     }
-    if (entry.isDir) new Dir with Common {
+    if (entry.isDir) new Dir {
+      override def name: String = entry.name
+      override def renameTo(path: String): RenameResult = rename(path)
       override def list: Seq[Node] = meta.children(entry.id).map(nodeFor)
       override def mkDir(child: String): Boolean = meta.addNewDir(entry.id, child).isDefined
       override def toString: String = s"Dir '$name': $entry"
-    } else
-      new File with Common {
-        override def size: Long = 0 // FIXME
-        override def toString: String = s"File '$name': $entry"
-      }
+    } else new File {
+      override def name: String = entry.name
+      override def renameTo(path: String): RenameResult = rename(path)
+      override def size: Long = 0 // FIXME
+      override def toString: String = s"File '$name': $entry"
+    }
   }
 
   override def getNode(path: String): Option[Node] = FileSystem.pathElements(path).flatMap(meta.entry).map(nodeFor)
