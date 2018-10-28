cca99cce6325fd36e129d8b7ecfa234949498aff
die <die@localhost>
2018-10-28 20:23:05 +0100

    forgotten transaction for mkdir

--- MetaH2/src/main/scala/net/diet_rich/dedup/metaH2/H2MetaBackend.scala
+++ MetaH2/src/main/scala/net/diet_rich/dedup/metaH2/H2MetaBackend.scala
@@ -57,9 +57,9 @@ class H2MetaBackend(implicit connectionFactory: ConnectionFactory) {
     entries(path) match {
       case Nel(Left(newName), Right(parent) :: _) =>
         if (!parent.isDir) MkdirParentNotADir else
-          MkdirOk(init(prepITreeEntry.run(parent.id, newName, None, None))(
+          MkdirOk(transaction(init(prepITreeEntry.run(parent.id, newName, None, None))(
             prepITreeJournal.run(_, parent.id, newName, None, None, false, None)
-          ))
+          )))
       case Nel(Left(_), _) => MkdirParentNotFound
       case Nel(Right(_), _) => MkdirExists
     }
