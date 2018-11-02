a1c3d90a32071c972bcff46c3928a50f28a6101e
georg <georg@localhost>
2018-11-02 17:02:42 +0100

    move complete for now

--- src/main/scala/net/diet_rich/dedupfs/FuseFS.scala
+++ src/main/scala/net/diet_rich/dedupfs/FuseFS.scala
@@ -1,13 +1,9 @@
 package net.diet_rich.dedupfs
 
+import jnr.ffi.Platform
 import jnr.ffi.Platform.OS.WINDOWS
-import jnr.ffi.{Platform, Pointer}
 import net.diet_rich.util._
-import net.diet_rich.util.fs._
-import ru.serce.jnrfuse.struct.{FileStat, FuseFileInfo, Statvfs}
-import ru.serce.jnrfuse.{ErrorCodes, FuseFillDir, FuseStubFS}
-
-// FIXME this is such a thin wrapper that we can get rid of it (merge FuseFS and SqlFS)
+import ru.serce.jnrfuse.ErrorCodes
 
 object FuseConstants {
   val O777     : Int = 511 // octal 0777
@@ -20,123 +16,9 @@ object FuseConstants {
   val ENOTEMPTY: Int = -ErrorCodes.ENOTEMPTY
 }
 
-/** See for example https://www.cs.hmc.edu/~geoff/classes/hmc.cs135.201001/homework/fuse/fuse_doc.html */
-class FuseFS(fs: SqlFS) extends FuseStubFS with ClassLogging { import fs._
-  import FuseConstants._
-
-  // FIXME FUSE provides a "file handle" in the "fuse_file_info" structure. The file handle
-  // FIXME is stored in the "fh" element of that structure, which is an unsigned 64-bit
-  // FIXME integer (uint64_t) uninterpreted by FUSE. If you choose to use it, you should set
-  // FIXME that field in your open, create, and opendir functions; other functions can then use it.
-
-  /** Return file attributes. For the given pathname, this should fill in the elements of the "stat" structure.
-    * We might want to fill in additional fields, see https://linux.die.net/man/2/stat and
-    * https://www.cs.hmc.edu/~geoff/classes/hmc.cs135.201001/homework/fuse/fuse_doc.html */
-  // Note: Calling FileStat.toString DOES NOT WORK
-  override def getattr(path: String, stat: FileStat): Int = log(s"getattr($path, fileStat)", asTrace = true) {
-    stat.st_uid.set(getContext.uid.get)
-    stat.st_gid.set(getContext.gid.get)
-    getNode(path) match {
-      case None => ENOENT
-      case Some(_: Dir) => stat.st_mode.set(FileStat.S_IFDIR | O777); OK
-      case Some(file: File) =>
-        stat.st_mode.set(FileStat.S_IFREG | O777)
-        stat.st_size.set(file.size)
-        OK
-    }
-  }
-
-  /** Attempts to create a directory named [path]. */
-  override def mkdir(path: String, mode: Long): Int = log(s"mkdir($path, $mode)")(fs.mkdir(path, mode))
-
-  /** See https://www.cs.hmc.edu/~geoff/classes/hmc.cs135.201001/homework/fuse/fuse_doc.html#readdir-details */
-  override def readdir(path: String, buf: Pointer, filler: FuseFillDir, offset: Long, fi: FuseFileInfo): Int =
-    log(s"readdir($path, buffer, filler, $offset, fileInfo)") {
-      if (offset.toInt < 0 || offset.toInt != offset) -ErrorCodes.EOVERFLOW
-      else fs.readdir(path) match {
-        // ENOENT No such directory.
-        // ENOTDIR path does not refer to a directory.
-        case ReaddirBadPath => EIO
-        case ReaddirNotFound => ENOENT
-        case ReaddirNotADirectory => ENOTDIR
-        case ReaddirOk(children) =>
-          def entries = "." #:: ".." #:: children.toStream.map(_.name)
-          entries.zipWithIndex
-            .drop(offset.toInt)
-            .exists {
-              case (entry, k) => filler.apply(buf, entry, null, k + 1) != 0
-            }
-          OK
-      }
-    }
-
-  /** Renames a file, moving it between directories if required. If newpath already exists it will be atomically
-    * replaced. oldpath can specify a directory. In this case, newpath must either not exist, or it must specify
-    * an empty directory. See https://linux.die.net/man/2/rename */
-  override def rename(oldpath: String, newpath: String): Int = log(s"rename($oldpath, $newpath)") {
-    fs.renameImpl(oldpath, newpath) match {
-        // EINVAL An attempt was made to make a directory a subdirectory of itself.
-        // EISDIR newpath is an existing directory, but oldpath is not a directory.
-        // ENOENT The link named by oldpath does not exist; or, a directory component in newpath does not exist; or, oldpath or newpath is an empty string.
-        // ENOTDIR A component used as a directory in oldpath or newpath is not, in fact, a directory. Or, oldpath is a directory, and newpath exists but is not a directory.
-        // ENOTEMPTY or EEXIST newpath is a nonempty directory, that is, contains entries other than "." and "..".
-        case RenameOk => OK
-        case RenameNotFound => ENOENT
-        case RenameTargetExists => EEXIST
-        case RenameParentDoesNotExist => ENOENT
-        case RenameParentNotADirectory => ENOTDIR
-        case RenameBadPath => EIO
-    }
-  }
-
-  /** Delete a directory. */ // TODO make sure implementation matches specification with regard to "." and ".."
-  override def rmdir(path: String): Int =
-    log(s"rmdir($path)")(fs.rmdir(path))
-
-  // FIXME check https://linux.die.net/man/2/statfs and https://www.cs.hmc.edu/~geoff/classes/hmc.cs135.201001/homework/fuse/fuse_doc.html
-  override def statfs(path: String, stbuf: Statvfs): Int = log(s"statfs($path, buffer)") {
-    if (Platform.getNativePlatform.getOS == WINDOWS) {
-      // statfs needs to be implemented on Windows in order to allow for copying
-      // data from other devices because winfsp calculates the volume size based
-      // on the statvfs call.
-      // see https://github.com/billziss-gh/winfsp/blob/14e6b402fe3360fdebcc78868de8df27622b565f/src/dll/fuse/fuse_intf.c#L654
-      if ("/" == path) {
-        // TODO Eventually, we want something meaningful here.
-        stbuf.f_blocks.set(1024 * 1024) // total data blocks in file system
-        stbuf.f_frsize.set(1024) // fs block size
-        stbuf.f_bfree.set(1024 * 1024) // free blocks in fs
-      }
-    }
-    OK
-  }
-
-  // FIXME check https://linux.die.net/man/2/unlink and https://www.cs.hmc.edu/~geoff/classes/hmc.cs135.201001/homework/fuse/fuse_doc.html
-  override def unlink(path: String): Int =
-    log(s"unlink($path)")(fs.unlink(path))
-
-  // FIXME double-check
-  override def create(path: String, mode: Long, fi: FuseFileInfo): Int =
-    log(s"create($path, buf, $mode, fileInfo)")(fs.create(path, mode, fi))
-
-  // TODO probably not needed
-  override def open(path: String, fi: FuseFileInfo): Int = log(s"open($path, fileInfo)") { OK }
-
-  // FIXME double-check
-  override def write(path: String, buf: Pointer, size: Long, offset: Long, fi: FuseFileInfo): Int =
-    log(s"write($path, buf, $size, $offset, fileInfo)")(fs.write(path, buf, size, offset, fi))
-
-  // FIXME double-check
-  override def truncate(path: String, size: Long): Int = log(s"truncate($path, $size)")(fs.truncate(path, size))
-
-  // FIXME double-check
-  override def read(path: String, buf: Pointer, size: Long, offset: Long, fi: FuseFileInfo): Int =
-    log(s"read($path, buf, $size, $offset, fileInfo)")(fs.read(path, buf, size, offset, fi))
-}
-
 object FuseFS extends ClassLogging with App {
   def mount(fs: SqlFS): AutoCloseable = {
     new AutoCloseable {
-      val fuseFS = new FuseFS(fs)
       try {
         // TODO Eventually, we want to have the mount point configurable.
         val mountPoint = Platform.getNativePlatform.getOS match {
@@ -144,12 +26,12 @@ object FuseFS extends ClassLogging with App {
           case _ => "/tmp/mntfs"
         }
         log.info(s"mount($mountPoint)")
-        fuseFS.mount(java.nio.file.Paths.get(mountPoint), false, false)
+        fs.mount(java.nio.file.Paths.get(mountPoint), false, false)
       } catch { case e: Throwable =>
         log.info(e.getMessage)
-        fuseFS.umount(); throw e
+        fs.umount(); throw e
       }
-      override def close(): Unit = fuseFS.umount()
+      override def close(): Unit = fs.umount()
     }
   }
 
--- src/main/scala/net/diet_rich/dedupfs/SqlFS.scala
+++ src/main/scala/net/diet_rich/dedupfs/SqlFS.scala
@@ -1,12 +1,13 @@
 package net.diet_rich.dedupfs
 
-import jnr.ffi.Pointer
+import jnr.ffi.{Platform, Pointer}
+import net.diet_rich.bytestore.{ByteStore, MemoryByteStore}
 import net.diet_rich.dedup.metaH2.{Database, H2, H2MetaBackend}
 import net.diet_rich.util.{ClassLogging, Head, Nel}
 import net.diet_rich.util.fs._
 import net.diet_rich.util.sql.ConnectionFactory
-import ru.serce.jnrfuse.FuseStubFS
-import ru.serce.jnrfuse.struct.FuseFileInfo
+import ru.serce.jnrfuse.{ErrorCodes, FuseFillDir, FuseStubFS}
+import ru.serce.jnrfuse.struct.{FileStat, FuseFileInfo, Statvfs}
 
 object SqlFS {
   val separator = "/"
@@ -26,24 +27,55 @@ object SqlFS {
       })
     }
 }
-// Note: This is not yet a fuse FS, still need to move things from FuseFS here...
+
+// FIXME FUSE provides a "file handle" in the "fuse_file_info" structure. The file handle
+// FIXME is stored in the "fh" element of that structure, which is an unsigned 64-bit
+// FIXME integer (uint64_t) uninterpreted by FUSE. If you choose to use it, you should set
+// FIXME that field in your open, create, and opendir functions; other functions can then use it.
+
+/** See for example https://www.cs.hmc.edu/~geoff/classes/hmc.cs135.201001/homework/fuse/fuse_doc.html */
 class SqlFS extends FuseStubFS with ClassLogging {
+
   private implicit val connectionFactory: ConnectionFactory =
     ConnectionFactory(H2.jdbcMemoryUrl, H2.defaultUser, H2.defaultPassword, H2.memoryOnShutdown)
   Database.create("MD5", Map())
   protected val meta: H2MetaBackend = new H2MetaBackend
 
   private def sync[T](t: T): T = synchronized(t)
-  protected def sync[T](message: => String)(f: => T): T = log(message)(sync(f))
+  protected def sync[T](message: => String, asTrace: Boolean = false)(f: => T): T = log(message, asTrace)(sync(f))
 
+  private val bytestore: ByteStore = new MemoryByteStore
 
   import FuseConstants._
+
+  override def flush(path: String, fi: FuseFileInfo): Int =
+    log(s"flush($path, fileInfo)")(super.flush(path, fi))
+
+  override def release(path: String, fi: FuseFileInfo): Int =
+    log(s"release($path, fileInfo)")(super.release(path, fi))
+
+  /** Return file attributes. For the given pathname, this should fill in the elements of the "stat" structure.
+    * We might want to fill in additional fields, see https://linux.die.net/man/2/stat and
+    * https://www.cs.hmc.edu/~geoff/classes/hmc.cs135.201001/homework/fuse/fuse_doc.html */
+  // Note: Calling FileStat.toString DOES NOT WORK
+  override def getattr(path: String, stat: FileStat): Int = sync(s"getattr($path, fileStat)", asTrace = true) {
+    stat.st_uid.set(getContext.uid.get)
+    stat.st_gid.set(getContext.gid.get)
+    getNode(path) match {
+      case None => ENOENT
+      case Some(_: Dir) => stat.st_mode.set(FileStat.S_IFDIR | O777); OK
+      case Some(file: File) =>
+        stat.st_mode.set(FileStat.S_IFREG | O777)
+        stat.st_size.set(file.size)
+        OK
+    }
+  }
 
   /** Attempts to create a directory named [path].
     * EEXIST  Path already exists (not necessarily as a directory).
     * ENOENT  A directory component in pathname does not exist.
     * ENOTDIR A component used as a directory in pathname is not, in fact, a directory. */
-  override def mkdir(path: String, mode: Long): Int = sync {
+  override def mkdir(path: String, mode: Long): Int = sync(s"mkdir($path, $mode)") {
     SqlFS.pathElements(path).fold[Int](EIO)(meta.mkdir(_) match {
         case MkdirOk(_) => OK
         case MkdirParentNotFound => ENOENT
@@ -52,8 +84,45 @@ class SqlFS extends FuseStubFS with ClassLogging {
     })
   }
 
+  /** Renames a file, moving it between directories if required. If newpath already exists it will be atomically
+    * replaced. oldpath can specify a directory. In this case, newpath must either not exist, or it must specify
+    * an empty directory. See https://linux.die.net/man/2/rename */
+  override def rename(oldpath: String, newpath: String): Int = sync(s"rename($oldpath, $newpath)") {
+    renameImpl(oldpath, newpath) match {
+      // EINVAL An attempt was made to make a directory a subdirectory of itself.
+      // EISDIR newpath is an existing directory, but oldpath is not a directory.
+      // ENOENT The link named by oldpath does not exist; or, a directory component in newpath does not exist; or, oldpath or newpath is an empty string.
+      // ENOTDIR A component used as a directory in oldpath or newpath is not, in fact, a directory. Or, oldpath is a directory, and newpath exists but is not a directory.
+      // ENOTEMPTY or EEXIST newpath is a nonempty directory, that is, contains entries other than "." and "..".
+      case RenameOk => OK
+      case RenameNotFound => ENOENT
+      case RenameTargetExists => EEXIST
+      case RenameParentDoesNotExist => ENOENT
+      case RenameParentNotADirectory => ENOTDIR
+      case RenameBadPath => EIO
+    }
+  }
 
-
+  /** See https://www.cs.hmc.edu/~geoff/classes/hmc.cs135.201001/homework/fuse/fuse_doc.html#readdir-details */
+  override def readdir(path: String, buf: Pointer, filler: FuseFillDir, offset: Long, fi: FuseFileInfo): Int =
+    sync(s"readdir($path, buffer, filler, $offset, fileInfo)") {
+      if (offset.toInt < 0 || offset.toInt != offset) -ErrorCodes.EOVERFLOW
+      else readdir(path) match {
+        // ENOENT No such directory.
+        // ENOTDIR path does not refer to a directory.
+        case ReaddirBadPath => EIO
+        case ReaddirNotFound => ENOENT
+        case ReaddirNotADirectory => ENOTDIR
+        case ReaddirOk(children) =>
+          def entries = "." #:: ".." #:: children.toStream.map(_.name)
+          entries.zipWithIndex
+            .drop(offset.toInt)
+            .exists {
+              case (entry, k) => filler.apply(buf, entry, null, k + 1) != 0
+            }
+          OK
+      }
+    }
 
   // FIXME temporary
   private val files: collection.mutable.Map[Long, Array[Byte]] = collection.mutable.Map()
@@ -64,7 +133,7 @@ class SqlFS extends FuseStubFS with ClassLogging {
     * The file handle in the "fh" element of FuseFileInfo is an unsigned 64-bit integer uninterpreted by FUSE.
     * If you choose to use it, you should set that field in your open, create, and opendir functions; other
     * functions can then use it. */
-  override def create(path: String, mode: Long, fi: FuseFileInfo): Int = sync {
+  override def create(path: String, mode: Long, fi: FuseFileInfo): Int = sync(s"create($path, buf, $mode, fileInfo)") {
     SqlFS.pathElements(path).map(meta.entries) match {
       case None => EIO
       case Some(Nel(Left(fileName), Right(parent) :: _)) =>
@@ -79,43 +148,45 @@ class SqlFS extends FuseStubFS with ClassLogging {
     }
   }
 
-  override def write(path: String, buf: Pointer, size: Long, offset: Long, fi: FuseFileInfo): Int = sync {
-    // FIXME handle size > MAXINT
-    SqlFS.pathElements(path).map(meta.entries) match {
-      case None => EIO
-      case Some(Nel(Left(_), _)) => ENOENT
-      case Some(Nel(Right(entry), _)) =>
-        if (entry.isDir) EISDIR
-        else {
-          val array = {
-            val a = files.getOrElse(entry.id, new Array[Byte](0))
-            if (a.length >= offset + size) a else java.util.Arrays.copyOf(a, (offset + size).toInt)
+  override def write(path: String, buf: Pointer, size: Long, offset: Long, fi: FuseFileInfo): Int =
+    sync(s"write($path, buf, $size, $offset, fileInfo)") {
+      // FIXME handle size > MAXINT
+      SqlFS.pathElements(path).map(meta.entries) match {
+        case None => EIO
+        case Some(Nel(Left(_), _)) => ENOENT
+        case Some(Nel(Right(entry), _)) =>
+          if (entry.isDir) EISDIR
+          else {
+            val array = {
+              val a = files.getOrElse(entry.id, new Array[Byte](0))
+              if (a.length >= offset + size) a else java.util.Arrays.copyOf(a, (offset + size).toInt)
+            }
+            buf.get(0, array, offset.toInt, size.toInt)
+            log.info(s"written ${entry.id} -> ${new String(array, "UTF-8")}")
+            files += entry.id -> array
+            size.toInt
           }
-          buf.get(0, array, offset.toInt, size.toInt)
-          log.info(s"written ${entry.id} -> ${new String(array, "UTF-8")}")
-          files += entry.id -> array
-          size.toInt
-        }
+      }
     }
-  }
 
-  override def read(path: String, buf: Pointer, size: Long, offset: Long, fi: FuseFileInfo): Int = sync {
-    SqlFS.pathElements(path).map(meta.entries) match {
-      case None => EIO
-      case Some(Nel(Left(_), _)) => ENOENT
-      case Some(Nel(Right(entry), _)) =>
-        if (entry.isDir) EISDIR
-        else {
-          val array = files.getOrElse(entry.id, Array())
-          val bytesToRead = math.min(array.length - offset, size).toInt
-          buf.put(0, array, offset.toInt, bytesToRead)
-          log.info(s"read ${entry.id} -> ${new String(array, "UTF-8")}")
-          bytesToRead
-        }
+  override def read(path: String, buf: Pointer, size: Long, offset: Long, fi: FuseFileInfo): Int =
+    sync(s"read($path, buf, $size, $offset, fileInfo)") {
+      SqlFS.pathElements(path).map(meta.entries) match {
+        case None => EIO
+        case Some(Nel(Left(_), _)) => ENOENT
+        case Some(Nel(Right(entry), _)) =>
+          if (entry.isDir) EISDIR
+          else {
+            val array = files.getOrElse(entry.id, Array())
+            val bytesToRead = math.min(array.length - offset, size).toInt
+            buf.put(0, array, offset.toInt, bytesToRead)
+            log.info(s"read ${entry.id} -> ${new String(array, "UTF-8")}")
+            bytesToRead
+          }
+      }
     }
-  }
 
-  override def truncate(path: String, size: Long): Int = sync {
+  override def truncate(path: String, size: Long): Int = sync(s"truncate($path, $size)") {
     SqlFS.pathElements(path).map(meta.entries) match {
       case None => EIO
       case Some(Nel(Left(_), _)) => ENOENT
@@ -129,7 +200,7 @@ class SqlFS extends FuseStubFS with ClassLogging {
     }
   }
 
-
+  // FIXME remove - not necessary for current purposes
   sealed trait Node {
     protected def entry: meta.TreeEntry
     final def name: String = sync(entry.name)
@@ -147,8 +218,25 @@ class SqlFS extends FuseStubFS with ClassLogging {
   private def nodeFor(entry: meta.TreeEntry): Node =
     if (entry.isDir) new Dir(entry) else new File(entry)
 
+  // FIXME check https://linux.die.net/man/2/statfs and https://www.cs.hmc.edu/~geoff/classes/hmc.cs135.201001/homework/fuse/fuse_doc.html
+  override def statfs(path: String, stbuf: Statvfs): Int = log(s"statfs($path, buffer)") {
+    if (Platform.getNativePlatform.getOS == jnr.ffi.Platform.OS.WINDOWS) {
+      // statfs needs to be implemented on Windows in order to allow for copying
+      // data from other devices because winfsp calculates the volume size based
+      // on the statvfs call.
+      // see https://github.com/billziss-gh/winfsp/blob/14e6b402fe3360fdebcc78868de8df27622b565f/src/dll/fuse/fuse_intf.c#L654
+      if ("/" == path) {
+        // TODO Eventually, we want something meaningful here.
+        stbuf.f_blocks.set(1024 * 1024) // total data blocks in file system
+        stbuf.f_frsize.set(1024) // fs block size
+        stbuf.f_bfree.set(1024 * 1024) // free blocks in fs
+      }
+    }
+    OK
+  }
+
   /** Delete a directory. */ // TODO make sure implementation matches specification with regard to "." and ".."
-  override def rmdir(path: String): Int = sync {
+  override def rmdir(path: String): Int = sync(s"rmdir($path)") {
       delete(path, expectDir = true) match {
         // EINVAL path has . as last component.
         // ENOENT A directory component in path does not exist.
@@ -163,7 +251,7 @@ class SqlFS extends FuseStubFS with ClassLogging {
     }
 
   // FIXME check https://linux.die.net/man/2/unlink and https://www.cs.hmc.edu/~geoff/classes/hmc.cs135.201001/homework/fuse/fuse_doc.html
-  override def unlink(path: String): Int = sync {
+  override def unlink(path: String): Int = sync(s"unlink($path)") {
     delete(path, expectDir = false) match {
       case DeleteOk => OK
       case DeleteHasChildren => EIO // should never happen
