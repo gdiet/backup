9b2bd128ad05624471b07f162342ea2e12a8c485
georg <georg@localhost>
2018-11-04 12:34:50 +0100

    cleanup

--- src/main/scala/net/diet_rich/dedupfs/FileSystem.scala
+++ src/main/scala/net/diet_rich/dedupfs/FileSystem.scala
@@ -5,35 +5,29 @@ import jnr.ffi.Platform.OS.WINDOWS
 import net.diet_rich.util._
 
 object FileSystem extends ClassLogging with App {
-  def mount(fs: SqlFS): AutoCloseable = {
-    new AutoCloseable {
-      try {
-        // TODO Eventually, we want to have the mount point configurable.
-        val mountPoint = Platform.getNativePlatform.getOS match {
-          case WINDOWS => """I:\"""
-          case _ => "/tmp/mntfs"
-        }
-        log.info(s"mount($mountPoint)")
-        fs.mount(java.nio.file.Paths.get(mountPoint), false, false)
-      } catch { case e: Throwable =>
-        log.info(e.getMessage)
-        fs.umount(); throw e
-      }
-      override def close(): Unit = fs.umount()
+  def mount(fs: SqlFS): AutoCloseable = try {
+    // TODO Eventually, we want to have the mount point configurable.
+    val mountPoint = Platform.getNativePlatform.getOS match {
+      case WINDOWS => """I:\"""
+      case _ => "/tmp/mntfs"
     }
+    log.info(s"mount($mountPoint)")
+    fs.mount(java.nio.file.Paths.get(mountPoint), false, false)
+
+    // FIXME remove
+    println(fs.mkdir("/hallo", 0))
+    println(fs.mkdir("/hallo/welt", 0))
+    println(fs.mkdir("/hello", 0))
+    println(fs.mkdir("/hello/world", 0))
+    println(fs.readdir("/").asInstanceOf[fs.ReaddirOk].children.mkString("\n"))
+
+    () => fs.umount()
+  } catch { case e: Throwable =>
+    log.info(e.getMessage)
+    fs.umount(); throw e
   }
 
-  val sqlFS = new SqlFS
-
-  // FIXME remove
-  import sqlFS._
-  println(mkdir("/hallo", 0))
-  println(mkdir("/hallo/welt", 0))
-  println(mkdir("/hello", 0))
-  println(mkdir("/hello/world", 0))
-  println(readdir("/").asInstanceOf[ReaddirOk].children.mkString("\n"))
-
-  val fsHandle = mount(sqlFS)
+  val fsHandle = mount(new SqlFS)
   try io.StdIn.readLine("[enter] to exit ...")
   finally fsHandle.close()
 }
