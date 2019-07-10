a18677be4f69fac5216ddfd69cdeaf6cfd74f717
die <die@localhost>
2019-07-10 22:19:07 +0200

    factored out common code

--- src/main/scala/dedup/Datastore.scala
+++ src/main/scala/dedup/Datastore.scala
@@ -21,36 +21,36 @@ class Datastore(baseDir: File, readOnly: Boolean) extends AutoCloseable {
     if (openFiles.size >= parallelOpenFiles)
       openFiles.head match { case (f, _) => if (f != file) openFiles.remove(f).foreach(_.close()) }
     openFiles.remove(file)
-      .getOrElse(new RandomAccessFile(file, writeFlag).tap(_ => println(s"opened $file for writing")))
+      .getOrElse {
+        if (!readOnly) new File(file).getParentFile.mkdirs()
+        new RandomAccessFile(file, writeFlag).tap(_ => println(s"opened $file for writing"))
+      }
       .tap(openFiles.addOne(file, _))
   }
 
-  def write(position: Long, data: Array[Byte]): Unit = {
-    require(!readOnly, "Datastore is read-only.")
-    val positionInFile = position % fileSize
-    val bytesToWrite = math.min(fileSize - positionInFile, data.length).toInt
+  private def pathOffsetSize(position: Long, size: Int): (String, Long, Int) = {
+    def positionInFile = position % fileSize
+    val chunkSize = math.min(fileSize - positionInFile, size).toInt
     val fileName = s"%010d".format((position / fileSize % 100) * fileSize) // 100MB per file
     val dir2 = f"${position / fileSize / 100 % 100}%02d"                   //  10GB per dir
     val dir1 = f"${position / fileSize / 100 / 100}%02d"                   //   1TB per dir
-    println(s"write ${data.length} bytes at $position in $dir1 / $dir2 / $fileName : $positionInFile")
     val filePath = s"$basePath/$dir1/$dir2/$fileName"
-    new File(filePath).getParentFile.mkdirs()
-    open(filePath).tap(_.seek(positionInFile)).write(data)
+    (filePath, positionInFile, chunkSize)
+  }
+
+  def write(position: Long, data: Array[Byte]): Unit = {
+    val (path, offset, bytesToWrite) = pathOffsetSize(position, data.length)
+    println(s"write ${data.length} bytes at position $position in offset $offset in $path")
+    open(path).tap(_.seek(offset)).write(data)
     if (data.length > bytesToWrite) write(position + bytesToWrite, data.drop(bytesToWrite))
   }
 
   def read(position: Long, size: Int): Array[Byte] = {
-    // FIXME duplicate code
-    val positionInFile = position % fileSize
-    val bytesToRead = math.min(fileSize - positionInFile, size).toInt
-    val fileName = s"%010d".format((position / fileSize % 100) * fileSize) // 100MB per file
-    val dir2 = f"${position / fileSize / 100 % 100}%02d"                   //  10GB per dir
-    val dir1 = f"${position / fileSize / 100 / 100}%02d"                   //   1TB per dir
-    println(s"read $size bytes from $position in $dir1 / $dir2 / $fileName : $positionInFile")
-    val filePath = s"$basePath/$dir1/$dir2/$fileName"
-    val bytes = new Array[Byte](size)
-    open(filePath).pipe { ra =>
-      ra.seek(positionInFile)
+    val (path, offset, bytesToRead) = pathOffsetSize(position, size)
+    println(s"read $bytesToRead bytes from $position in offset $offset in $path")
+    val bytes = new Array[Byte](bytesToRead)
+    open(path).pipe { ra =>
+      ra.seek(offset)
       ra.readFully(bytes) // TODO corrupt data files -> entry will not be read at all
     }
     if (size > bytesToRead) bytes ++ read(position + bytesToRead, size - bytesToRead)
