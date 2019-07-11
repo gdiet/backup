e4329beca9c4183c9818466d4e256470169473bd
die <die@localhost>
2019-07-11 22:17:59 +0200

    small cleanup

--- src/main/scala/util/H2.scala
+++ src/main/scala/util/H2.scala
@@ -10,12 +10,9 @@ object H2 {
   // ;TRACE_LEVEL_SYSTEM_OUT=2"
   private def jdbcUrl(directory: java.io.File) = s"jdbc:h2:$directory/dedupfs;DB_CLOSE_ON_EXIT=FALSE"
 
-  private val dbUser = "sa"
-  private val dbPassword = ""
-
   def rw(directory: java.io.File): Connection =
-    DriverManager.getConnection(jdbcUrl(directory), dbUser, dbPassword).tap(_.setAutoCommit(true))
+    DriverManager.getConnection(jdbcUrl(directory), "sa", "").tap(_.setAutoCommit(true))
 
   def ro(directory: java.io.File): Connection =
-    DriverManager.getConnection(jdbcUrl(directory) + ";ACCESS_MODE_DATA=r", dbUser, dbPassword).tap(_.setAutoCommit(true))
+    DriverManager.getConnection(jdbcUrl(directory) + ";ACCESS_MODE_DATA=r", "sa", "")
 }
