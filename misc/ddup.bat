1cd5bf0019548690d0312803bc2b0d251689dd72
die <die@localhost>
2019-07-25 17:26:48 +0200

    default target is current timestamp

--- misc/ddup.bat
+++ misc/ddup.bat
@@ -1,2 +1,4 @@
+@set timestamp=%DATE:~6,4%.%DATE:~3,2%.%DATE:~0,2%_%TIME:~0,2%.%TIME:~3,2%
+@set timestamp=%timestamp: =0%
 @set Path=%~dp0openjdk-11u-11.0.4_11-jre\bin
-@%~dp0app\dedup.bat repo=%~dp0 mount=J:\ %*
+@%~dp0app\dedup.bat repo=%~dp0 mount=J:\ target=%timestamp% %*
