aeef537ac1c251c16a8d68b2b733361c511df8b2
Georg <gdiet@users.noreply.github.com>
2019-10-03 23:55:18 +0200

    prefer immutable map over mutable sorted map

--- src/main/scala/dedup2/DataStore.scala
+++ src/main/scala/dedup2/DataStore.scala
@@ -2,42 +2,40 @@ package dedup2
 
 import java.io.File
 
-import scala.collection.mutable
-
 class DataStore(repo: File, readOnly: Boolean) extends AutoCloseable {
   private val longTermStore = new LongTermStore(repo, readOnly)
   override def close(): Unit = longTermStore.close()
   private val basePath = new File(repo, "shortterm").getAbsolutePath
 }
 object DataStore {
-  private val entries: mutable.Map[Long, mutable.SortedMap[Long, Array[Byte]]] = mutable.Map()
-  private def entry(dataId: Long) =
-    entries.getOrElse(dataId, mutable.SortedMap[Long, Array[Byte]]().tap(entries += dataId -> _))
+  private var entries = Map[Long, Map[Long, Array[Byte]]]().withDefaultValue(Map())
 
   def read(dataId: Long, ltStart: Long, ltStop: Long)(position: Long, size: Int): Array[Byte] = ???
   def size(dataId: Long, ltStart: Long, ltStop: Long): Long = ???
+
   def write(dataId: Long)(position: Long, data: Array[Byte]): Unit = {
-    val chunks = entry(dataId)
+    val chunks = entries(dataId)
     println(s"Stage 1: ${chunks.view.mapValues(_.mkString("[",",","]")).toMap}")
-    chunks.get(position) match {
-      case None => chunks += position -> data
+    val combinedChunks = chunks + (chunks.get(position) match {
+      case None => position -> data
       case Some(previousData) =>
-        if (data.length >= previousData.length) chunks += position -> data
-        else System.arraycopy(data, 0, previousData, 0, data.length)
+        if (data.length >= previousData.length) position -> data
+        else { System.arraycopy(data, 0, previousData, 0, data.length); position -> previousData }
+    })
+    println(s"Stage 2: ${combinedChunks.view.mapValues(_.mkString("[",",","]")).toMap}")
+    val (toMerge, others) = combinedChunks.partition { case (chunkPosition, chunkData) =>
+      (chunkPosition < position && chunkPosition + chunkData.length > position) ||
+        (chunkPosition >= position && chunkPosition < position + data.length)
     }
-    println(s"Stage 2: ${chunks.view.mapValues(_.mkString("[",",","]")).toMap}")
-    val endOfWrittenChunk = position + data.length
-    val updates: mutable.Map[Long, Option[(Long, Array[Byte])]] = chunks.collect {
-      case (chunkPosition, chunkData) if chunkPosition < position && chunkPosition + chunkData.length > position =>
-        chunkPosition -> Some(chunkPosition -> chunkData.take((position - chunkPosition).toInt))
-      case (chunkPosition, chunkData) if chunkPosition > position && chunkPosition < endOfWrittenChunk =>
-        val remainingChunkSize = (chunkPosition + chunkData.length - endOfWrittenChunk).toInt
-        if (remainingChunkSize < 1) chunkPosition -> None
-        else chunkPosition -> Some(endOfWrittenChunk -> chunkData.takeRight(remainingChunkSize))
+    println(s"Merge  : ${toMerge.view.mapValues(_.mkString("[",",","]")).toMap}")
+    val reduced = toMerge.toSeq.sortBy(_._1).reduceLeft[(Long, Array[Byte])] { case (posA -> dataA, posB -> dataB) =>
+      if (posA + dataA.length >= posB + dataB.length) posA -> dataA
+      else posA -> (dataA ++ dataB.drop((posA + dataA.length - posB).toInt))
     }
-    println(s"Updates: ${updates.view.mapValues(_.map(x => x._1 -> x._2.mkString("[",",","]"))).toMap}")
-    updates.foreach { case (position, maybeNew) => chunks -= position; maybeNew.foreach(chunks +=) }
-    println(s"Stage 3: ${chunks.view.mapValues(_.mkString("[",",","]")).toMap}")
+    println(s"Reduced: ${reduced._1} ${reduced._2.mkString("[",",","]")}")
+    val merged = others + reduced
+    println(s"Stage 3: ${merged.view.mapValues(_.mkString("[",",","]")).toMap}")
+    entries += dataId -> merged
   }
 }
 
