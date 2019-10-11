4d7646a55b0c1723b4c1f5aa8965dc53e765b306
Georg <gdiet@users.noreply.github.com>
2019-10-11 16:18:39 +0200

    prepare app release

--- src/main/script/server-read.bat
+++ src/main/script/server-read.bat
@@ -1,2 +1,1 @@
-@rem %~dp0openjdk-11u-11.0.4_11-jre\bin\java
-@java -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.Server
+@%~dp0openjdk-11u-11.0.4_11-jre\bin\java -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.Server
