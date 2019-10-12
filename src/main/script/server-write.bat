4110d8f5a12589e99e8698eafb53b4c2191381e8
Georg <gdiet@users.noreply.github.com>
2019-10-12 10:19:53 +0200

    added file logging

--- src/main/script/server-write.bat
+++ src/main/script/server-write.bat
@@ -1,1 +1,1 @@
-@start "DedupFS" %~dp0openjdk-11u-11.0.4_11-jre\bin\java -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.TrayApp write
+@start "DedupFS" %~dp0openjdk-11u-11.0.4_11-jre\bin\javaw -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.TrayApp write
