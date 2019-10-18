bf8527bb5135d78408ab795d97f30d1f1f175fcb
Georg <gdiet@users.noreply.github.com>
2019-10-18 08:19:07 +0200

    intermediate commit triggering scalac assertion

--- src/main/scala/dedup/DataStore.scala
+++ src/main/scala/dedup/DataStore.scala
@@ -10,10 +10,11 @@ class DataStore(dataDir: String, readOnly: Boolean) extends AutoCloseable {
   override def close(): Unit = longTermStore.close()
 
   sealed trait Entry {
-    def write(data: Array[Byte]): Unit
-    def length: Int
-    def drop(left: Int, right: Int): Entry
+    def position: Long
     def data: Array[Byte]
+    def length: Int
+    def write(data: Array[Byte]): Unit
+    def drop(left: Int, right: Int): Entry
     def ++(other: Entry): Entry
   }
   object Entry {
@@ -32,40 +33,40 @@ class DataStore(dataDir: String, readOnly: Boolean) extends AutoCloseable {
     override def write(data: Array[Byte]): Unit = ???
     override def ++(other: Entry): Entry = ???
   }
-  // Map(id/dataId -> size, Map(position, data))
-  private var entries = Map[(Long, Long), (Long, Map[Long, Entry])]()
+  // Map(id/dataId -> size, Seq(data))
+  private var entries = Map[(Long, Long), (Long, Seq[Entry])]()
   private var memoryUsage = 0L
 
   def hasTemporaryData(id: Long, dataId: Long): Boolean = entries.contains(id -> dataId)
 
   def read(id: Long, dataId: Long, ltStart: Long, ltStop: Long)(position: Long, requestedSize: Int): Array[Byte] = {
-    val (fileSize, localChunks) = entries.getOrElse(id -> dataId, ltStop - ltStart -> Map[Long, Entry]())
+    val (fileSize, localEntries) = entries.getOrElse(id -> dataId, ltStop - ltStart -> Seq[Entry]())
     val sizeToRead = math.max(math.min(requestedSize, fileSize - position), 0).toInt
-    val chunks: Map[Long, Entry] = localChunks.collect {
-      case (chunkPosition, entry)
-        if (chunkPosition <  position && chunkPosition + entry.length > position) ||
-           (chunkPosition >= position && chunkPosition                < position + sizeToRead) =>
-        val dropLeft  = math.max(0, position - chunkPosition).toInt
-        val dropRight = math.max(0, chunkPosition + entry.length - (position + sizeToRead)).toInt
-        chunkPosition + dropLeft -> entry.drop(dropLeft, dropRight)
+    val chunks: Seq[Entry] = localEntries.collect {
+      case entry
+        if (entry.position <  position && entry.position + entry.length > position) ||
+           (entry.position >= position && entry.position                < position + sizeToRead) =>
+        val dropLeft  = math.max(0, position - entry.position).toInt
+        val dropRight = math.max(0, entry.position + entry.length - (position + sizeToRead)).toInt
+        entry.drop(dropLeft, dropRight)
     }
-    val chunksToRead = chunks.foldLeft(SortedMap(position -> sizeToRead)) { case (chunksToRead, (chunkPosition, chunkData)) =>
-      val (readPosition, sizeToRead) = chunksToRead.filter(_._1 <= chunkPosition).last
+    val chunksToRead = chunks.foldLeft(SortedMap(position -> sizeToRead)) { case (chunksToRead, entry) =>
+      val (readPosition, sizeToRead) = chunksToRead.filter(_._1 <= entry.position).last
       chunksToRead - readPosition ++ Seq(
-        readPosition -> (chunkPosition - readPosition).toInt,
-        chunkPosition + chunkData.length -> (readPosition + sizeToRead - chunkPosition - chunkData.length).toInt
+        readPosition -> (entry.position - readPosition).toInt,
+        entry.position + entry.length -> (readPosition + sizeToRead - entry.position - entry.length).toInt
       ).filterNot(_._2 == 0)
     }
     val chunksRead: SortedMap[Long, Array[Byte]] =
       chunksToRead.map { case (start, length) => start -> longTermStore.read(ltStart + start, length) }
-    (chunksRead ++ chunks.view.mapValues(_.data)).map(_._2).reduce(_ ++ _)
+    (chunksRead ++ chunks.map(e => e.position -> e.data)).map(_._2).reduce(_ ++ _)
   }
 
   def size(id: Long, dataId: Long, ltStart: Long, ltStop: Long): Long =
     entries.get(id -> dataId).map(_._1).getOrElse(ltStop - ltStart)
 
   def delete(id: Long, dataId: Long, writeLog: Boolean = true): Unit = {
-    memoryUsage = memoryUsage - entries.get(id -> dataId).toSeq.flatMap(_._2).map(_._2.length.toLong).sum
+    memoryUsage = memoryUsage - entries.get(id -> dataId).toSeq.flatMap(_._2).map(_.length.toLong).sum
     if (writeLog) log.debug(s"Delete - memory usage $memoryUsage.")
     entries -= (id -> dataId)
   }
@@ -73,21 +74,21 @@ class DataStore(dataDir: String, readOnly: Boolean) extends AutoCloseable {
   def truncate(id: Long, dataId: Long, ltStart: Long, ltStop: Long): Unit = {
     delete(id, dataId, writeLog = false)
     log.debug(s"Truncate - memory usage $memoryUsage.")
-    entries += (id -> dataId) -> (0L -> Map())
+    entries += (id -> dataId) -> (0L -> Seq())
   }
 
   def write(id: Long, dataId: Long, ltStart: Long, ltStop: Long)(position: Long, data: Array[Byte]): Unit = {
-    val (fileSize, chunks) = entries.getOrElse(id -> dataId, ltStop - ltStart -> Map[Long, Entry]())
+    val (fileSize, chunks) = entries.getOrElse(id -> dataId, ltStop - ltStart -> Seq[Entry]())
     val newSize = math.max(fileSize, position + data.length)
-    val combinedChunks = chunks + (chunks.get(position) match {
-      case None => position -> Entry(id, dataId, position, data)
+    val combinedChunks = chunks :+ (chunks.find(_.position == position) match {
+      case None => Entry(id, dataId, position, data)
       case Some(previousData) =>
-        if (data.length >= previousData.length) position -> Entry(id, dataId, position, data)
-        else position -> previousData.tap(_.write(data))
+        if (data.length >= previousData.length) Entry(id, dataId, position, data)
+        else previousData.tap(_.write(data))
     })
-    val (toMerge, others) = combinedChunks.partition { case (chunkPosition, chunkData) =>
-      (chunkPosition < position && chunkPosition + chunkData.length > position) ||
-        (chunkPosition >= position && chunkPosition < position + data.length)
+    val (toMerge, others) = combinedChunks.partition { entry =>
+      (entry.position < position && entry.position + entry.length > position) ||
+        (entry.position >= position && entry.position < position + data.length)
     }
     val reduced = toMerge.toSeq.sortBy(_._1).reduceLeft[(Long, Entry)] { case (posA -> dataA, posB -> dataB) =>
       if (posA + dataA.length >= posB + dataB.length) posA -> dataA
