d1117aeb49c09240922a0d1e3816d425e2ccba54
Georg <gdiet@users.noreply.github.com>
2019-10-27 16:40:11 +0100

    windows - check for db trace file before starting write mode

--- src/main/script/console-write.bat
+++ src/main/script/console-write.bat
@@ -1,3 +1,11 @@
-@rem Options: init write repo=repositoryDirectory mount=mountPoint
-@call %~dp0dbbackup.bat
-@%~dp0jre\bin\java -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.Server write
+@echo off
+if exist fsdb\dedupfs.trace.db (
+  echo Database trace file fsdb\dedupfs.trace.db found.
+  echo Check for database problems first.
+  echo.
+  pause
+  exit /B
+)
+call %~dp0dbbackup.bat
+rem Options: init write repo=repositoryDirectory mount=mountPoint
+%~dp0jre\bin\java -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.Server write
--- src/main/script/server-write.bat
+++ src/main/script/server-write.bat
@@ -1,3 +1,11 @@
-@rem Options: init write repo=repositoryDirectory mount=mountPoint
-@call %~dp0dbbackup.bat
-@start "DedupFS" %~dp0jre\bin\javaw -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.TrayApp write
+@echo off
+if exist fsdb\dedupfs.trace.db (
+  echo Database trace file fsdb\dedupfs.trace.db found.
+  echo Check for database problems first.
+  echo.
+  pause
+  exit /B
+)
+call %~dp0dbbackup.bat
+rem Options: init write repo=repositoryDirectory mount=mountPoint
+start "DedupFS" %~dp0jre\bin\javaw -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.TrayApp write
