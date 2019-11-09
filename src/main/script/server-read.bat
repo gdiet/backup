d9ae3734aaed78b2d5c345852fd22a3ff5bf167b
Georg <gdiet@users.noreply.github.com>
2019-11-09 09:17:32 +0100

    swing ui for the dedup file server

--- src/main/script/server-read.bat
+++ src/main/script/server-read.bat
@@ -1,2 +1,6 @@
 @rem Options: init write repo=repositoryDirectory mount=mountPoint
-@start "DedupFS" %~dp0jre\bin\javaw -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.TrayApp
+@rem Possible main classes:
+@rem * dedup.Server (console, use "java")
+@rem * dedup.TrayApp (use "javaw")
+@rem * dedup.ServerGui (use "javaw")
+@start "DedupFS" %~dp0jre\bin\javaw -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.ServerGui
--- src/main/script/server-write-tray.bat
+++ src/main/script/server-write-tray.bat
@@ -8,8 +8,4 @@ if exist fsdb\dedupfs.trace.db (
 )
 call %~dp0dbbackup.bat
 rem Options: init write repo=repositoryDirectory mount=mountPoint
-@rem Possible main classes:
-@rem * dedup.Server (console, use "java")
-@rem * dedup.TrayApp (use "javaw")
-@rem * dedup.ServerGui (use "javaw")
-start "DedupFS" %~dp0jre\bin\javaw -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.ServerGui write
+start "DedupFS" %~dp0jre\bin\javaw -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.TrayApp write
