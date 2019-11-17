47bf97b48b596157a18d3562fd0087c24a31cf54
Georg <gdiet@users.noreply.github.com>
2019-11-17 10:29:40 +0100

    clearer naming

--- src/main/scala/dedup/DataStore.scala
+++ src/main/scala/dedup/DataStore.scala
@@ -24,24 +24,24 @@ class DataStore(dataDir: String, tempPath: String, readOnly: Boolean) extends Au
   def ifDataWritten(id: Long, dataId: Long)(f: => Unit): Unit =
     if (entries.getEntry(id, dataId).nonEmpty) f
 
-  def read(id: Long, dataId: Long, startStop: StartStop)(position: Long, requestedSize: Int): Array[Byte] = {
+  def read(id: Long, dataId: Long, startStop: StartStop)(offset: Long, requestedSize: Int): Array[Byte] = {
     assumeLogged(id > 0, s"id > 0 ... $id")
     assumeLogged(dataId > 0, s"dataId > 0 ... $dataId")
-    assumeLogged(position >= 0, s"position >= 0 ... $position")
+    assumeLogged(offset >= 0, s"offset >= 0 ... $offset")
     assumeLogged(requestedSize >= 0, s"requestedSize >= 0 ... $requestedSize")
     assumeLogged(requestedSize < 1000000, s"requestedSize < 1000000 ... $requestedSize")
     val longTermSize = startStop.size
     val (fileSize, localEntries) = entries.getEntry(id, dataId).getOrElse(longTermSize -> Seq[Entry]())
-    val sizeToRead = math.max(math.min(requestedSize, fileSize - position), 0).toInt
+    val sizeToRead = math.max(math.min(requestedSize, fileSize - offset), 0).toInt
     val chunks: Seq[Entry] = localEntries.collect {
       case entry
-        if (entry.position <  position && entry.position + entry.length > position) ||
-           (entry.position >= position && entry.position                < position + sizeToRead) =>
-        val dropLeft  = math.max(0, position - entry.position).toInt
-        val dropRight = math.max(0, entry.position + entry.length - (position + sizeToRead)).toInt
+        if (entry.position <  offset && entry.position + entry.length > offset) ||
+           (entry.position >= offset && entry.position                < offset + sizeToRead) =>
+        val dropLeft  = math.max(0, offset - entry.position).toInt
+        val dropRight = math.max(0, entry.position + entry.length - (offset + sizeToRead)).toInt
         entry.drop(dropLeft, dropRight)
     }
-    val chunksNotCached = chunks.foldLeft(SortedMap(position -> sizeToRead)) { case (chunksToRead, entry) =>
+    val chunksNotCached = chunks.foldLeft(SortedMap(offset -> sizeToRead)) { case (chunksToRead, entry) =>
       val (readPosition, sizeToRead) = chunksToRead.filter(_._1 <= entry.position).last
       chunksToRead - readPosition ++ Seq(
         readPosition -> (entry.position - readPosition).toInt,
@@ -61,20 +61,20 @@ class DataStore(dataDir: String, tempPath: String, readOnly: Boolean) extends Au
 
   // FIXME test
   def truncate(id: Long, dataId: Long, startStop: StartStop, newSize: Long): Unit = {
-    def zeros(position: Long, size: Long): Seq[Entry] =
+    def zeros(offset: Long, size: Long): Seq[Entry] =
       if (size == 0) Seq()
-      else if (size <= 524288) Seq(entries.newEntry(id, dataId, position, new Array[Byte](size.toInt)))
-      else zeros(position + 524288, size - 524288) :+ entries.newEntry(id, dataId, position, new Array[Byte](size.toInt))
+      else if (size <= 524288) Seq(entries.newEntry(id, dataId, offset, new Array[Byte](size.toInt)))
+      else zeros(offset + 524288, size - 524288) :+ entries.newEntry(id, dataId, offset, new Array[Byte](size.toInt))
     entries.getEntry(id, dataId) match {
       case None =>
-        entries.setOrReplace(id, dataId, newSize, zeros(startStop.stop, math.max(newSize - startStop.size, 0)))
+        entries.setOrReplace(id, dataId, newSize, zeros(0, math.max(newSize - startStop.size, 0)))
       case Some(oldSize -> chunks) =>
         val newStop = startStop.start + newSize
         val newChunks = chunks.collect { case entry if entry.position < newStop =>
           val dropRight = math.max(0, entry.position + entry.length - newStop).toInt
           entry.drop(0, dropRight)
         }
-        val zeroPadding = zeros(startStop.stop, math.max(newSize - oldSize, 0))
+        val zeroPadding = zeros(oldSize, math.max(newSize - oldSize, 0))
         entries.setOrReplace(id, dataId, newSize, newChunks ++ zeroPadding)
     }
   }
@@ -82,26 +82,26 @@ class DataStore(dataDir: String, tempPath: String, readOnly: Boolean) extends Au
   def delete(id: Long, dataId: Long): Unit =
     entries.delete(id, dataId)
 
-  def write(id: Long, dataId: Long, longTermSize: Long)(position: Long, data: Array[Byte]): Unit =
+  def write(id: Long, dataId: Long, longTermSize: => Long)(offset: Long, data: Array[Byte]): Unit =
     // https://stackoverflow.com/questions/58506337/java-byte-array-of-1-mb-or-more-takes-up-twice-the-ram
     // TODO document resolution of this issue
-    data.grouped(524288).foldLeft(0) { case (offset, bytes) =>
-      internalWrite(id, dataId, longTermSize)(position + offset, bytes)
-      offset + 524288
+    data.grouped(524288).foldLeft(0) { case (chunkOffset, bytes) =>
+      internalWrite(id, dataId, longTermSize)(offset + chunkOffset, bytes)
+      chunkOffset + 524288
     }
 
-  private def internalWrite(id: Long, dataId: Long, longTermSize: Long)(position: Long, data: Array[Byte]): Unit = {
+  private def internalWrite(id: Long, dataId: Long, longTermSize: => Long)(offset: Long, data: Array[Byte]): Unit = {
     val (fileSize, chunks) = entries.getEntry(id, dataId).getOrElse(longTermSize -> Seq())
-    val newSize = math.max(fileSize, position + data.length)
-    val combinedChunks = chunks :+ (chunks.find(_.position == position) match {
-      case None => entries.newEntry(id, dataId, position, data)
+    val newSize = math.max(fileSize, offset + data.length)
+    val combinedChunks = chunks :+ (chunks.find(_.position == offset) match {
+      case None => entries.newEntry(id, dataId, offset, data)
       case Some(previousData) =>
-        if (data.length >= previousData.length) entries.newEntry(id, dataId, position, data)
+        if (data.length >= previousData.length) entries.newEntry(id, dataId, offset, data)
         else previousData.tap(_.write(data))
     })
     val (toMerge, others) = combinedChunks.partition { entry =>
-      (entry.position < position && entry.position + entry.length > position) ||
-        (entry.position >= position && entry.position < position + data.length)
+      (entry.position < offset && entry.position + entry.length > offset) ||
+        (entry.position >= offset && entry.position < offset + data.length)
     }
     val reduced = toMerge.sortBy(_.position).reduceLeft[Entry] { case (dataA, dataB) =>
       if (dataA.position + dataA.length >= dataB.position + dataB.length) dataA
