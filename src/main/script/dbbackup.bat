43f89d1ef0e0810782f03ee3a264a7c3a12fd90c
Georg <gdiet@users.noreply.github.com>
2019-11-18 19:07:09 +0100

    fixed timestamp for database backup

--- src/main/script/dbbackup.bat
+++ src/main/script/dbbackup.bat
@@ -1,8 +1,8 @@
 @echo off
+if "%time:~0,1%" == " " (set hour=0%time:~1,1%) else (set hour=%time:~0,2%)
+set timestamp=%date:~6,4%-%date:~3,2%-%date:~0,2%_%hour%-%time:~3,2%
 if exist "fsdb\dedupfs.mv.db" (
   echo "Starting database backup."
-  if "%time:~0,1%" == " " (set timestamp=0%time:~1,1%) else (set timestamp=%time:~0,2%)
-  set timestamp=%date:~6,4%-%date:~3,2%-%date:~0,2%_%timestamp%-%time:~3,2%
   echo Creating sql script database backup...
   echo fsdb\dedupfs.mv.db -^> fsdb\dedupfs_%timestamp%.zip
   jre\bin\java.exe -cp lib\* org.h2.tools.Script -url "jdbc:h2:%~dp0fsdb\dedupfs" -script fsdb\dedupfs_%timestamp%.zip -user sa -options compression zip
