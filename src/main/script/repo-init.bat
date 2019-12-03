fbd4683e990f1f04a0af162b50f22a89069689ad
Georg <gdiet@users.noreply.github.com>
2019-12-03 19:26:29 +0100

    repo-init.sh for linux

--- src/main/script/repo-init.bat
+++ src/main/script/repo-init.bat
@@ -1,3 +1,2 @@
-@rem Options: init write copyWhenMoving repo=repositoryDirectory mount=mountPoint
 @%~dp0jre\bin\java -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.Server init
 @pause
--- src/main/script/repo-init.sh
+++ src/main/script/repo-init.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Options: init write copyWhenMoving repo=repositoryDirectory mount=mountPoint
-# "$(dirname "$0")"/openjdk-11u-11.0.4_11-jre/bin/java
-java -cp "$(dirname "$0")"/lib/* dedup.Server init
+cd "$(dirname "$0")" || exit 1
+jre-linux/bin/java -cp "lib/*" dedup.Server init
