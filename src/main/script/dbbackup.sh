9bbac01da55ed92c1184801b118474a81e01f246
Georg <gdiet@users.noreply.github.com>
2019-12-03 22:12:18 +0100

    dbbackup.sh for linux

---
+++ src/main/script/dbbackup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit 1
+DBFILE=fsdb/dedupfs.mv.db
+if [ -f "$DBFILE" ]; then
+  TIMESTAMP=$(date +%Y-%m-%d_%H-%M)
+  TARGET=fsdb/dedupfs_$TIMESTAMP.zip
+  echo "Creating sql script database backup: $DBFILE -> $TARGET"
+  jre-linux/bin/java -cp "lib/*" org.h2.tools.Script -url "jdbc:h2:$(pwd)/fsdb/dedupfs" -script $TARGET -user sa -options compression zip
+  chmod -w $TARGET
+else
+  echo "Database backup: Database file $DBFILE does not exist."
+fi
