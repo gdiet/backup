b3363f7c4a0bedf428dd5a6de3af95a8d9aab7d6
Georg <gdiet@users.noreply.github.com>
2019-12-05 22:23:07 +0100

    linux: check installed java

--- src/main/script/server-write.sh
+++ src/main/script/server-write.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-# Options: init write copyWhenMoving repo=repositoryDirectory mount=mountPoint temp=tempFileDirectory
+cd "$(dirname "$0")" || exit 1
+JAVAVERSION=$(java -version 2>&1 | head -1)
+if [[ $JAVAVERSION != *"\"11.0."* ]]; then
+  echo "This software has been tested on Java 11 only."
+  echo "Detected $JAVAVERSION"
+  echo "Exiting now."
+  exit 1
+fi
+DBERRORFILE=fsdb/dedupfs.trace.db
+if [ -f "$DBERRORFILE" ]; then
+  echo "Database trace file $DBERRORFILE found."
+  echo "Check for database problems first."
+  exit 1
+fi
+# Options: write copyWhenMoving repo=repositoryDirectory mount=mountPoint temp=tempFileDirectory
 # Server needs at least ~400MB of memory.
-# "$(dirname "$0")"/openjdk-11u-11.0.4_11-jre/bin/java
-java -cp "$(dirname "$0")"/lib/* dedup.Server write
+java -cp "lib/*" -Dfile.encoding=UTF-8 dedup.ServerGui write
