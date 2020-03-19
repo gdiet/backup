051c729d7d781a62cd33d2fc9ecdbd18d3301d22
Georg <gdiet@users.noreply.github.com>
2020-03-19 23:42:06 +0100

    next attempt for concurrent long term store in go

--- go/lts/fileOps.go
+++ go/lts/fileOps.go
@@ -7,66 +7,68 @@ import (
 	"path/filepath"
 )
 
+// Fmap todo todo
+type Fmap map[string]chan *os.File
+
 // Fman todo todo
-type Fman chan map[string]chan *os.File
+type Fman chan Fmap
 
 // Read todo todo
-func Read(manChannel Fman, basePath string, position, size uint64) ([]byte, error) {
-	man := <-manChannel
-	var err error
-	var file *os.File
-	relativePath, offsetInFile, bytesToRead := PathOffsetSize(position, size)
-	fileChannel, ok := man[relativePath]
-	if ok {
-		manChannel <- man
-		file = <-fileChannel
-		if file == nil {
-			fileChannel <- nil
-			return Read(manChannel, basePath, position, size)
-		}
-	} else {
-		if len(man) > 4 {
-			if !closeOne(&man) {
-				for _, fileChannel = range man {
-					manChannel <- man
-					file := <-fileChannel
-					fileChannel <- nil
-					if file != nil {
-						file.Close()
-					}
-					return Read(manChannel, basePath, position, size)
-				}
+func Read(fman Fman, basePath, relativePath string, offsetInFile, bytesToRead uint64, result chan []byte) {
+	read(fman, <-fman, basePath, relativePath, offsetInFile, bytesToRead, result)
+}
+
+func read(fman Fman, fmap Fmap, basePath, relativePath string, offsetInFile, bytesToRead uint64, result chan []byte) {
+	if fchan, ok := fmap[relativePath]; ok {
+		fman <- fmap // pushing back means we have to call Read if needed, not read
+		if file := <-fchan; file != nil {
+			if _, err := file.Seek(int64(offsetInFile), io.SeekStart); err != nil {
+				result <- nil
+				return
 			}
-			manChannel <- man
-			return Read(manChannel, basePath, position, size)
+			buffer := make([]byte, bytesToRead)
+			if _, err := io.ReadFull(file, buffer); err != nil {
+				log.Println(err)
+			}
+			result <- buffer
+			return
 		}
-		path := filepath.Join(basePath, relativePath)
-		// os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0777)
-		if file, err = os.Open(path); err != nil {
-			return nil, err
+		Read(fman, basePath, relativePath, offsetInFile, bytesToRead, result) // just try again
+		return
+	}
+	// here: no entry in map for basepath
+	if len(fmap) >= maxOpenFiles {
+		if closeOne(&fmap) {
+			read(fman, fmap, basePath, relativePath, offsetInFile, bytesToRead, result)
+			return
 		}
-		fileChannel := make(chan *os.File)
-		man[relativePath] = fileChannel
-		manChannel <- man
+		// here: could not close an entry in a non-blocking way
+		for path, fileChannel := range fmap {
+			(<-fileChannel).Close() // yes, blocks - but makes sure that maxOpenFiles is not exceeded
+			fileChannel <- nil
+			delete(fmap, path)
+			read(fman, fmap, basePath, relativePath, offsetInFile, bytesToRead, result)
+			return
+		}
 	}
-	buffer := make([]byte, bytesToRead)
-	if _, err = file.Seek(int64(offsetInFile), io.SeekStart); err != nil {
-		fileChannel <- file
-		return nil, err
+	path := filepath.Join(basePath, relativePath)
+	file, err := os.Open(path) // or os.OpenFile(path, os.O_RDWR|os.O_CREATE, 0777)
+	if err != nil {
+		result <- nil
 	}
-	if _, err = io.ReadFull(file, buffer); err != nil {
-		log.Println(err)
-	}
+	fileChannel := make(chan *os.File, 1)
 	fileChannel <- file
-	return buffer, err
+	fmap[relativePath] = fileChannel
+	read(fman, fmap, basePath, relativePath, offsetInFile, bytesToRead, result)
+	return
 }
 
 // closeOne closes one available file, removing it from the map
-func closeOne(man *map[string]chan *os.File) bool {
-	for relativePath, fileChannel := range *man {
+func closeOne(fmap *Fmap) bool {
+	for relativePath, fileChannel := range *fmap {
 		select {
 		case file := <-fileChannel:
-			delete(*man, relativePath)
+			delete(*fmap, relativePath)
 			if file != nil {
 				file.Close()
 			}
--- go/lts/package.go
+++ go/lts/package.go
@@ -1,3 +1,4 @@
 package lts
 
 const fileSize = 100000000
+const maxOpenFiles = 5
