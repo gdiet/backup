1b8bfe968ed7d8a83fc8ff46ccadf6240b16e4b4
Georg <gdiet@users.noreply.github.com>
2020-05-11 21:34:02 +0200

    removed unnecessary try

--- src/test/scala/dedup/store/LongTermStorePerformanceTest.scala
+++ src/test/scala/dedup/store/LongTermStorePerformanceTest.scala
@@ -20,32 +20,30 @@ object LongTermStorePerformanceTest extends App {
     import java.io.File
     val dir = new File(basePath)
     if (dir.exists()) delete(dir)
-    try {
-      dir.mkdirs()
-      def testRun(parallel: Int): Unit = {
-        val store = new LongTermStore(dir.getAbsolutePath, false) {
-          override val parallelOpenResources: Int = parallel
-        }
-        store.write(0, new Array[Byte](310000000))
-        def testLoop(): Unit = {
-          for(_ <- 1 to 20) {
-            for(k <- 0 to 6) {
-              for (b <- 0 to 3) {
-                store.read(b*100000000 + k*1000, 1000)
-              }
+    dir.mkdirs()
+    def testRun(parallel: Int): Unit = {
+      val store = new LongTermStore(dir.getAbsolutePath, false) {
+        override val parallelOpenResources: Int = parallel
+      }
+      store.write(0, new Array[Byte](310000000))
+      def testLoop(): Unit = {
+        for(_ <- 1 to 20) {
+          for(k <- 0 to 6) {
+            for (b <- 0 to 3) {
+              store.read(b*100000000 + k*1000, 1000)
             }
           }
         }
-        testLoop()
-        val start = now
-        testLoop()
-        println(f"parallelOpenResources $parallel - time ${(now - start)/1000} us")
       }
-      println("Multiple times read 1000 bytes from 4 different data files...")
-      println("When data files are kept open between reads:")
-      for (n <- 1 to 10) testRun(5)
-      println("When each read requires to close & open a data file:")
-      for (n <- 1 to 10) testRun(2)
+      testLoop()
+      val start = now
+      testLoop()
+      println(f"parallelOpenResources $parallel - time ${(now - start)/1000} us")
     }
+    println("Multiple times read 1000 bytes from 4 different data files...")
+    println("When data files are kept open between reads:")
+    for (n <- 1 to 10) testRun(5)
+    println("When each read requires to close & open a data file:")
+    for (n <- 1 to 10) testRun(2)
   }
 }
