2017f975cb60dc4ec370bbdc5068879fcf0378d7
Georg <gdiet@users.noreply.github.com>
2020-09-19 22:43:30 +0200

    deleted obsolete code

--- src/test/scala/dedup/TestRead.scala
+++
@@ -1,9 +0,0 @@
-package dedup
-
-object TestRead extends App {
-  val ds = new DataStore("data", "dedupfs-temp", false)
-  ds.write(1, 2, 0)(4, Array[Byte](1,2,3,4))
-  ds.truncate(1, 2, 0, 10)
-  val out = ds.read(1, 2, Parts(Seq()))(0, 20)
-  println(out.mkString(" "))
-}
--- src/test/scala/dedup/TestWrite.scala
+++
@@ -1,9 +0,0 @@
-package dedup
-
-object TestWrite extends App {
-  val ds = new DataStore("data", "dedupfs-temp", false)
-  ds.truncate(1, 2, 0, 8)
-  ds.write(1, 2, 0)(2, Array[Byte](1,2,3,4))
-  val out = ds.read(1, 2, Parts(Seq()))(0, 20)
-  println(out.mkString(" "))
-}
