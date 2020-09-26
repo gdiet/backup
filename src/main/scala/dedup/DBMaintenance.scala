cd9d8d1c3f9d417d37026c0db6a60a5301e53fe1
Georg <gdiet@users.noreply.github.com>
2020-09-26 13:37:52 +0200

    more readable number formatting

--- src/main/scala/dedup/DBMaintenance.scala
+++ src/main/scala/dedup/DBMaintenance.scala
@@ -68,9 +68,9 @@ object DBMaintenance {
       val dataChunks = allDataChunks(stat).to(SortedMap)
       log.info(s"Number of data chunks in storage database: ${dataChunks.size}")
       val (endOfStorage, dataGaps) = endOfStorageAndDataGaps(dataChunks)
-      log.info(f"Current size of data storage: ${readable(endOfStorage)}B")
+      log.info(s"Current size of data storage: ${readable(endOfStorage)}B")
       val compactionPotential = combinedSize(dataGaps)
-      log.info(f"Compaction potential of stage 2: ${compactionPotential/1000000000d}%,.2f GB in ${dataGaps.size} gaps.")
+      log.info(s"Compaction potential of stage 2: ${readable(compactionPotential)}B in ${dataGaps.size} gaps.")
     }
     log.info(s"Finished stage 1 of reclaiming space. Undo by restoring the database from a backup.")
   }
@@ -84,9 +84,9 @@ object DBMaintenance {
     val dataChunks = dataEntries.map(e => e._3 -> e._4).to(SortedMap)
     log.info(s"Number of data chunks in storage database: ${dataChunks.size}")
     val (endOfStorage, dataGaps) = endOfStorageAndDataGaps(dataChunks)
-    log.info(f"Current size of data storage: ${readable(endOfStorage)}B")
+    log.info(s"Current size of data storage: ${readable(endOfStorage)}B")
     val compactionPotentialString = readable(combinedSize(dataGaps))+"B"
-    log.info(f"Compaction potential: $compactionPotentialString in ${dataGaps.size} gaps.")
+    log.info(s"Compaction potential: $compactionPotentialString in ${dataGaps.size} gaps.")
 
     type Entry = (Long, Seq[Chunk])
     /** Seq(dataId -> Seq(chunk)), ordered by maximum chunk position descending */
@@ -103,7 +103,7 @@ object DBMaintenance {
     def reclaim(sortedEntries: Seq[Entry], gaps: Seq[Chunk], reclaimed: Long): Long =
       if (sortedEntries.isEmpty) reclaimed else { // can't fold or foreach because that's not tail recursive in Scala 2.13.3
         if (now > progressLoggedLast + 10000) {
-          log.info(f"In progress... already reclaimed ${readable(reclaimed)}B of $compactionPotentialString.")
+          log.info(s"In progress... already reclaimed ${readable(reclaimed)}B of $compactionPotentialString.")
           progressLoggedLast = now
         }
 
@@ -147,6 +147,6 @@ object DBMaintenance {
       }
 
     val reclaimed = reclaim(sortedEntries, dataGaps, 0)
-    log.info(f"Reclaimed ${reclaimed/1000000000d}%,.2f GB.")
+    log.info(s"Reclaimed ${readable(reclaimed)}B.")
   }
 }
