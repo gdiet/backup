e6d9ee9e288e4ba01515d0c1483ae2cb49c7d05d
Georg <gdiet@users.noreply.github.com>
2020-10-02 09:51:53 +0200

    improved Windows server-write utility

--- src/main/script/server-write.bat
+++ src/main/script/server-write.bat
@@ -6,8 +6,12 @@ rem -DLOG_BASE=<log base directory> | mandatory for sensible logging
 "%~dp0jre\bin\java" "-DLOG_BASE=%~dp0\" -cp "%~dp0lib\*" dedup.Server dbbackup %*
 
 rem Options for write:
+rem copywhenmoving                  | default: disabled
+rem mount=K:\                       | default: Windows J:\ Linux /tmp/mnt
 rem repo=<repository directory>     | default: working directory
+rem temp=<temp file directory>      | default: <system temp dir>/dedupfs-temp
 rem -DLOG_BASE=<log base directory> | mandatory for sensible logging
 rem -Dfile.encoding=UTF-8           | necessary for FUSE operations
 rem write needs at least 128MB RAM.
-start "DedupFS" "%~dp0jre\bin\javaw" "-DLOG_BASE=%~dp0\" -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.ServerGui write
+echo Starting the DedupFS server GUI now - it will run detached from the command line.
+start "DedupFS" "%~dp0jre\bin\javaw" "-DLOG_BASE=%~dp0\" -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.ServerGui write %*
