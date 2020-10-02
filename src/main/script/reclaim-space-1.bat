80782992c212e643f613bd70e450a3f08bede920
Georg <gdiet@users.noreply.github.com>
2020-10-02 23:56:14 +0200

    fixed bugs in reclaim utilities

--- src/main/script/reclaim-space-1.bat
+++ src/main/script/reclaim-space-1.bat
@@ -4,4 +4,5 @@ rem Options for reclaim space 1:
 rem keepdays=<number of days to keep> | default: 0
 rem repo=<repository directory>       | default: working directory
 rem -DLOG_BASE=<log base directory>   | mandatory for sensible logging
-"%~dp0jre\bin\javaw" "-DLOG_BASE=%~dp0\" -cp "%~dp0lib\*" dedup.Server reclaimspace1 %*
+"%~dp0jre\bin\java" "-DLOG_BASE=%~dp0\" -cp "%~dp0lib\*" dedup.Server reclaimspace1 %*
+pause
--- src/main/script/reclaim-space-2.bat
+++ src/main/script/reclaim-space-2.bat
@@ -3,4 +3,5 @@ title Reclaim Space 2 - DedupFS
 rem Options for reclaim space 2:
 rem repo=<repository directory>       | default: working directory
 rem -DLOG_BASE=<log base directory>   | mandatory for sensible logging
-"%~dp0jre\bin\javaw" "-DLOG_BASE=%~dp0\" -cp "%~dp0lib\*" dedup.Server reclaimspace2 %*
+"%~dp0jre\bin\java" "-DLOG_BASE=%~dp0\" -cp "%~dp0lib\*" dedup.Server reclaimspace2 %*
+pause
