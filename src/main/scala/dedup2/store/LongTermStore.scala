05878749e89fe7bb76f1c23da9852394ab77161b
Georg <gdiet@users.noreply.github.com>
2021-01-27 10:28:19 +0100

    mock in memory long term store

---
+++ src/main/scala/dedup2/store/LongTermStore.scala
@@ -0,0 +1,21 @@
+package dedup2.store
+
+class LongTermStore {
+  private val storeSize = 10000000
+  private val store = new Array[Byte](storeSize)
+
+  def write(position: Long, data: Array[Byte]): Unit = synchronized {
+    require(position >= 0)
+    val actualLength = math.max(math.min(data.length, storeSize - position), 0).toInt
+    if (actualLength > 0) System.arraycopy(data, 0, store, position.toInt, actualLength)
+  }
+
+  def read(position: Long, size: Int): Array[Byte] = if (size == 0) Array() else synchronized {
+    require(position >= 0)
+    require(size >= 0)
+    val actualLength = math.max(math.min(size, storeSize - position), 0).toInt
+    val result = new Array[Byte](actualLength)
+    if (actualLength > 0) System.arraycopy(store, position.toInt, result, 0, actualLength)
+    result
+  }
+}
