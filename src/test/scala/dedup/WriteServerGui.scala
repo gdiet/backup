425e421b5c2468bb3e635ebb0c88aa38353f0bac
Georg <gdiet@users.noreply.github.com>
2021-02-20 11:04:05 +0100

    initialize GUI earlier to include temp dir warning log

--- src/test/scala/dedup/WriteServerGui.scala
+++ src/test/scala/dedup/WriteServerGui.scala
@@ -2,5 +2,5 @@ package dedup
 
 object WriteServerGui extends App {
   sys.props.update("LOG_BASE", "./")
-  Main.main(Array("write", "gui=true", "mount=/home/georg/temp/mnt"))
+  Main.main(Array("write", "gui=true", "repo=/media/georg/WD/dedupfs", "mount=/home/georg/temp/mnt"))
 }
