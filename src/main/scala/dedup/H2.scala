b6c8fb82045de40c344d73fb95ff8b68975b4e75
Georg <gdiet@users.noreply.github.com>
2021-02-20 18:03:16 +0100

    don't auto-close the database on exit, it is still needed in the shutdown process

--- src/main/scala/dedup/H2.scala
+++ src/main/scala/dedup/H2.scala
@@ -7,14 +7,11 @@ object H2 {
 
   // For SQL debugging, add to the DB URL "...;TRACE_LEVEL_SYSTEM_OUT=2"
   private def jdbcUrl(directory: java.io.File, readonly: Boolean) =
-    if (readonly) s"jdbc:h2:$directory/dedupfs;ACCESS_MODE_DATA=r" else s"jdbc:h2:$directory/dedupfs"
+    if (readonly) s"jdbc:h2:$directory/dedupfs;ACCESS_MODE_DATA=r" else s"jdbc:h2:$directory/dedupfs;DB_CLOSE_ON_EXIT=FALSE"
 
   def file(directory: java.io.File, readonly: Boolean, settings: String = ""): Connection = {
     if (!readonly) new java.io.File(directory, "fsdb/dedupfs.trace.db")
       .tap(file => require(!file.exists(), s"Database trace file $file found. Check for database problems."))
     DriverManager.getConnection(jdbcUrl(directory, readonly) + settings, "sa", "").tap(_.setAutoCommit(true))
   }
-
-  def mem(): Connection =
-    DriverManager.getConnection("jdbc:h2:mem:", "sa", "").tap(_.setAutoCommit(true))
 }
