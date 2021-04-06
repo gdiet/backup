bbadc38dd4966311b45a84d835f9d487afad3607
georg <gdiet@users.noreply.github.com>
2021-04-06 09:32:59 +0200

    more mem cache tests

--- src/test/scala/dedup/cache/MemCacheFunctionality.scala
+++ src/test/scala/dedup/cache/MemCacheFunctionality.scala
@@ -9,7 +9,6 @@ object MemCacheFunctionality extends App {
     def data: Array[Byte] = {
       var result = Array[Byte]()
       entries.forEach((position: Long, data: Array[Byte]) => {
-        println(s"${position} ${data.length}")
         result ++= new Array[Byte]((position - result.length).asInt) ++ data
       })
       result
@@ -17,19 +16,45 @@ object MemCacheFunctionality extends App {
   }
 
   println("Inserting spaced data works.")
-  cache.write(3, Array[Byte](1,2))
-  cache.write(9, Array[Byte](3,4))
-  cache.write(6, Array[Byte](5,6))
+  assert(cache.write(3, Array[Byte](1,2)))
+  assert(cache.write(9, Array[Byte](3,4)))
+  assert(cache.write(6, Array[Byte](5,6)))
   assert(cache.data.sameElements(Array[Byte](0, 0, 0, 1, 2, 0, 5, 6, 0, 3, 4)))
   assert(100000 - available.get() == 6)
 
   println("Replacing an entry works.")
-  cache.write(6, Array[Byte](7,8))
+  assert(cache.write(6, Array[Byte](7,8)))
   assert(cache.data.sameElements(Array[Byte](0, 0, 0, 1, 2, 0, 7, 8, 0, 3, 4)))
   assert(100000 - available.get() == 6)
 
   println("Overwriting multiple entries works.")
-  cache.write(4, Array[Byte](1,2,3,4,5,6))
+  assert(cache.write(4, Array[Byte](1,2,3,4,5,6)))
   assert(cache.data.sameElements(Array[Byte](0, 0, 0, 1, 1, 2, 3, 4, 5, 6, 4)))
   assert(100000 - available.get() == 8)
+
+  println("Inserting in front works.")
+  assert(cache.write(0, Array[Byte](1,2,3)))
+  assert(cache.data.sameElements(Array[Byte](1, 2, 3, 1, 1, 2, 3, 4, 5, 6, 4)))
+  assert(100000 - available.get() == 11)
+
+  println("Partially replacing at the start works.")
+  assert(cache.write(4, Array[Byte](9)))
+  assert(cache.data.sameElements(Array[Byte](1, 2, 3, 1, 9, 2, 3, 4, 5, 6, 4)))
+  assert(100000 - available.get() == 11)
+
+  println("Partially replacing in the middle works.")
+  assert(cache.write(6, Array[Byte](9)))
+  assert(cache.data.sameElements(Array[Byte](1, 2, 3, 1, 9, 2, 9, 4, 5, 6, 4)))
+  assert(100000 - available.get() == 11)
+
+  println("Partially replacing in the end works.")
+  assert(cache.write(9, Array[Byte](9)))
+  assert(cache.data.sameElements(Array[Byte](1, 2, 3, 1, 9, 2, 9, 4, 5, 9, 4)))
+  assert(100000 - available.get() == 11)
+
+  println("Memory check works.")
+  assert(!cache.write(0, new Array[Byte](100000 - 10)))
+  val data = new Array[Byte](100000 - 11)
+  assert(cache.write(0, data))
+  assert(available.get() == 11)
 }
