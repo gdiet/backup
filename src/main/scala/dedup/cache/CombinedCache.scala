ded23d955c01bd9014e602e03a1406c6a27bc791
georg <gdiet@users.noreply.github.com>
2021-04-15 21:05:41 +0200

    fixed CombinedCache.read for size = 0

--- src/main/scala/dedup/cache/CombinedCache.scala
+++ src/main/scala/dedup/cache/CombinedCache.scala
@@ -1,13 +1,13 @@
 package dedup.cache
 
-import dedup.memChunk
+import dedup.{ClassLogging, memChunk}
 
 import java.nio.file.Path
 import java.util.concurrent.atomic.AtomicLong
 
 /** Caches byte arrays with positions, where the byte arrays are not necessarily contiguous. Useful for representing
   * a singe file's contents in a virtual file system. */
-class CombinedCache(availableMem: AtomicLong, tempFilePath: Path, initialSize: Long) extends AutoCloseable {
+class CombinedCache(availableMem: AtomicLong, tempFilePath: Path, initialSize: Long) extends AutoCloseable with ClassLogging {
 
   private var _size: Long = initialSize
   def size: Long = _size
@@ -53,7 +53,8 @@ class CombinedCache(availableMem: AtomicLong, tempFilePath: Path, initialSize: L
   }
 
   /** @return LazyList((holePosition, holeSize) | (dataPosition, bytes)) where positions start at `position`. */
-  def read(position: Long, size: Long): LazyList[Either[(Long, Long), (Long, Array[Byte])]] =
+  def read(position: Long, size: Long): LazyList[Either[(Long, Long), (Long, Array[Byte])]] = if (size == 0) LazyList() else {
+    log.trace(s"read(position = $position, size: $size)")
     // Read from memory cache.
     memCache.read(position, size).flatMap {
       case Right(data) => LazyList(Right(data))
@@ -71,6 +72,7 @@ class CombinedCache(availableMem: AtomicLong, tempFilePath: Path, initialSize: L
               .map(off => Right(localPos + off -> new Array[Byte](math.min(memChunk, localSize - off).asInt)))
         }
     }
+  }
 
   override def close(): Unit = maybeChannelCache.foreach(_.close())
 }
