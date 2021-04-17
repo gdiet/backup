a6ac82695bd3684e585adb7833d7f02f9ed70bfa
georg <gdiet@users.noreply.github.com>
2021-04-17 07:32:03 +0200

    Fixed read offset require check

--- src/main/scala/dedup/Level2.scala
+++ src/main/scala/dedup/Level2.scala
@@ -95,7 +95,7 @@ class Level2(settings: Settings) extends AutoCloseable with ClassLogging {
     * @param readSize Number of bytes to read, must be >= 0.
     * @return A contiguous LazyList(position, bytes) where data chunk size is limited to [[dedup.memChunk]]. */
   private def readFromLts(parts: Seq[(Long, Long)], readFrom: Long, readSize: Long, resultOffset: Long): LazyList[(Long, Array[Byte])] = {
-    require(readFrom > 0, s"Read offset $readFrom must be > 0.")
+    require(readFrom >= 0, s"Read offset $readFrom must be >= 0.")
     require(readSize > 0, s"Read size $readSize must be > 0.")
     val (lengthOfParts, partsToReadFrom) = parts.foldLeft(0L -> Vector[(Long, Long)]()) {
       case ((currentOffset, result), part @ (partPosition, partSize)) =>
