8a3250a8b886241195dd7c89bc7077ca8b41ff37
georg <gdiet@users.noreply.github.com>
2021-04-23 21:22:40 +0200

    new runtime instantiation time estimation for ClassLogging

--- src/main/scala/dedup/logging.scala
+++ src/main/scala/dedup/logging.scala
@@ -2,7 +2,7 @@ package dedup
 
 /** Provides logging methods. For optimum performance uses by-name string parameters.
   *
-  * Instantiation needs a couple of ns. When used in a class that might be instantiated million times per second,
+  * Instantiation needs e.g. 30 ns. When used in a class that might be instantiated million times per second,
   * consider using the companion object for class logging. In this case see also
   *
   * @see https://stackoverflow.com/questions/13713557/scala-accessing-protected-field-of-companion-objects-trait */
