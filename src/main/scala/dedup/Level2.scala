1f7b86619e790b8feb607c585707f5e1c5e227b8
georg <gdiet@users.noreply.github.com>
2021-04-23 22:38:15 +0200

    fixed a log level

--- src/main/scala/dedup/Level2.scala
+++ src/main/scala/dedup/Level2.scala
@@ -129,7 +129,7 @@ class Level2(settings: Settings) extends AutoCloseable with ClassLogging {
     * @return A contiguous LazyList(position, bytes) where data chunk size is limited to [[dedup.memChunk]].
     */
   private def readFromLts(parts: Seq[(Long, Long)], readFrom: Long, readSize: Long): LazyList[(Long, Array[Byte])] = {
-    log.info(s"readFromLts $parts $readFrom $readSize")
+    log.trace(s"readFromLts(parts: $parts, readFrom: $readFrom, readSize: $readSize)")
     require(readFrom >= 0, s"Read offset $readFrom must be >= 0.")
     require(readSize > 0, s"Read size $readSize must be > 0.")
     val (lengthOfParts, partsToReadFrom) = parts.foldLeft(0L -> Vector[(Long, Long)]()) {
