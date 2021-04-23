ba6726893de36b67a8e860f52623f820e7443c68
georg <gdiet@users.noreply.github.com>
2021-04-23 09:43:00 +0200

    store integration test fails

--- src/test/scala/dedup/XTestLevel1.scala
+++ src/test/scala/dedup/XTestLevel1.scala
@@ -3,8 +3,13 @@ package dedup
 import java.io.File
 import java.util.concurrent.atomic.AtomicBoolean
 import scala.util.Using.resource
+import cache.LongDecorator
 
 object XTestLevel1 extends App with ClassLogging {
+  implicit object BytesSink extends DataSink[Array[Byte]] {
+    override def write(dataSink: Array[Byte], offset: Long, data: Array[Byte]): Unit =
+      System.arraycopy(data, 0, dataSink, offset.asInt, data.length)
+  }
 
   val baseFolder = new File("dedupfs-temp")
 
@@ -19,22 +24,44 @@ object XTestLevel1 extends App with ClassLogging {
     log.info(s"Create store")
     val temp = new File(baseFolder + "temp")
     val settings = Settings(repo, dbDir, temp, readonly = false, new AtomicBoolean(false))
-    val store = new Level1(settings)
+    resource(new Level1(settings)) { store =>
 
-    log.info(s"Create a file.")
-    store.createAndOpen(parentId = 0, name = "file", time = 0).get
+      log.info(s"Create a file.")
+      store.createAndOpen(parentId = 0, name = "file", time = 0).get
 
-    log.info(s"Access the file.")
-    val file = store.child(0, "file").get
+      def file = {
+        store.child(0, "file").get.asInstanceOf[FileEntry].tap { f =>
+          log.info(s"Access " + f)
+        }
+      }
+      val fileId = file.id
 
-    log.info(s"Write 1000 bytes.")
-    store.write(file.id, LazyList(0L -> Array.fill(1000)(65)))
+      log.info(s"Write 1000 bytes.")
+      store.write(fileId, LazyList(0L -> Array.fill(1000)(Array[Byte](70, 71, 72, 73, 74)).flatten))
 
-    log.info(s"Close the file.")
-    store.release(file.id)
+      log.info(s"Close the file.")
+      store.release(fileId)
+      Thread.sleep(100) // Wait until persisted
 
-    log.info(s"Close file system.")
-    store.close()
+      log.info(s"Open the file.")
+      store.open(file)
+
+      log.info(s"Write 6 bytes at position 2.")
+      store.write(fileId, LazyList(2L -> Array.fill(6)(80)))
+
+      log.info(s"Read 10 bytes.")
+      val bytesRead = new Array[Byte](10)
+      store.read(fileId, 0, 10, bytesRead)
+
+      log.info(s"Check read result " + bytesRead.toList)
+      require(bytesRead.sameElements(Array[Byte](70, 71, 80, 80, 80, 80, 80, 80, 73, 74)))
+
+      log.info(s"Close the file.")
+      store.release(fileId)
+      Thread.sleep(100) // Wait until persisted
+
+      log.info(s"Close file system.")
+    }
   } finally {
     log.info(s"Clean up")
     CleanInit.delete(baseFolder)
