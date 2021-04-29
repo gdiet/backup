df1a8d182564e0544fbdc0279e485d14029c5d4f
georg <gdiet@users.noreply.github.com>
2021-04-29 16:58:13 +0200

    log mem cache size

--- src/main/scala/dedup/cache/MemCache.scala
+++ src/main/scala/dedup/cache/MemCache.scala
@@ -1,11 +1,12 @@
 package dedup.cache
 
-import dedup.scalaUtilChainingOps
+import dedup.{ClassLogging, readableBytes, scalaUtilChainingOps}
 
 import java.util.concurrent.atomic.AtomicLong
 
-object MemCache {
+object MemCache extends ClassLogging {
   private val cacheLimit: Long = math.max(0, (Runtime.getRuntime.maxMemory - 64000000) * 7 / 10)
+  log.info(s"Memory cache size: ${readableBytes(cacheLimit)}")
   val availableMem = new AtomicLong(cacheLimit)
 }
 
