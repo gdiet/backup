c2d8912e36285417f613d2208ad827fae7d64fbb
georg <gdiet@users.noreply.github.com>
2021-05-17 19:23:11 +0200

    use FromString type class for file main parameter

--- scala3/src/main/scala/main.scala
+++ scala3/src/main/scala/main.scala
@@ -14,11 +14,16 @@ object main extends util.ClassLogging {
     sys.exit(0)
 }
 
-// TODO for param file = File(repoDir).getAbsoluteFile(), use FromString type class
-@main def init(repoDir: String) =
-  val repo  = File(repoDir).getAbsoluteFile()
+given scala.util.CommandLineParser.FromString[File] with
+  def fromString(file: String) = File(file).getAbsoluteFile()
+
+@main def init(repo: File) =
   val dbDir = db.dbDir(repo)
   if dbDir.exists() then
     main.failureExit(s"Database directory $dbDir exists - repository is probably already initialized.");
   resource(db.H2.connection(dbDir, readonly = false))(db.initialize)
   main.exit(s"Database initialized for repository $repo.")
+
+// TODO for param file = File(repoDir).getAbsoluteFile(), use FromString type class
+@main def mount(repo: File) =
+  ???
