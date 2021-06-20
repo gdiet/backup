7df1ade780780acd06c95032e684c0a267bb3fa8
gdiet <gdiet@users.noreply.github.com>
2021-06-20 11:26:15 +0200

    Test scenario for FileCache

--- scala3/src/test/scala/dedup/TestHelpers.scala
+++ scala3/src/test/scala/dedup/TestHelpers.scala
@@ -10,8 +10,8 @@ def delete(file: File): Unit =
     if file.isDirectory then file.listFiles.foreach(delete)
     file.delete()
 
-trait TestDir:
-  lazy val testDir: File =
+trait TestFile:
+  lazy val testFile: File =
     File(sys.props("java.io.tmpdir") + s"/dedupfs-test/${getClass.getTypeName}")
       .tap(delete)
       .tap(_.getParentFile.mkdirs)
--- scala3/src/test/scala/dedup/cache/FileCacheSpec.scala
+++ scala3/src/test/scala/dedup/cache/FileCacheSpec.scala
@@ -8,10 +8,11 @@ import java.util.concurrent.atomic.AtomicLong
 import scala.util.Random
 
 // No IDEA support for scalatest with scala 3? https://youtrack.jetbrains.com/issue/SCL-18644
-class FileCacheSpec extends AnyFreeSpec with TestDir:
+class FileCacheSpec extends AnyFreeSpec with TestFile:
 
   s"Important parts of ${°[FileCache]} are tested by ${°[CacheBaseSpec]}, see there..." - {
-    val cache = FileCache(testDir.toPath)
+    val cache = FileCache(testFile.toPath)
+
     "Scenario 1 for basic functionality" in {
       def chunk1 = new Array[Byte](10000).tap(Random(1).nextBytes)
       def chunk2 = new Array[Byte](memChunk).tap(Random(2).nextBytes)
@@ -19,15 +20,29 @@ class FileCacheSpec extends AnyFreeSpec with TestDir:
       cache.write(MaxInt - 1000, chunk1)
       cache.write(MaxInt + 9000, chunk2)
       cache.write(MaxInt + 8000 + memChunk, chunk3)
-      val i = cache.readData(0, MaxInt + 20000 + memChunk)
-      i.foreach {
-        case pos -> Left(size) => println(s"LEFT $size")
-        case pos -> Right(data) => println(s"RIGHT ${data.length}")
-      }
-//      assert(i.next == 0 -> Left(MaxInt - 1000))
-      // FIXME check result
+      val i = cache.readData(0, MaxInt + 20000 + memChunk).iterator
+      assert(i.next == 0 -> Left(MaxInt - 1000))
+      val pos1 -> Right(dat1) = i.next
+      assert(pos1 == MaxInt - 1000)
+      assert(dat1.length == memChunk)
+      assert(dat1.take(10000).toSeq == chunk1.toSeq)
+      assert(dat1.drop(10000).toSeq == chunk2.dropRight(10000).toSeq)
+      val pos2 -> Right(dat2) = i.next
+      assert(pos2 == MaxInt - 1000 + memChunk)
+      assert(dat2.length == 19000)
+      assert(dat2.take(9000).toSeq == chunk2.takeRight(10000).take(9000).toSeq)
+      assert(dat2.drop(9000).toSeq == chunk3.toSeq)
+      assert(i.next == MaxInt + 18000 + memChunk -> Left(2000))
+      assert(i.hasNext == false)
     }
+
     "Scenario 2 for memory handling" in {
       // FIXME test with Runtime.getRuntime.maxMemory zeros
+    }
+
+    s"When the ${°[FileCache]} is closed, the cache file is deleted" in {
+      assert(testFile.exists() == true)
+      cache.close()
+      assert(testFile.exists() == false)
     }
   }
