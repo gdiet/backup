5a6cf6b5fa3b8ac3f25f0deb9de6de9f5779cbab
gdiet <gdiet@users.noreply.github.com>
2021-07-11 15:26:10 +0200

    improved start scripts

--- scala3/src/main/script/repo-init.bat
+++ scala3/src/main/script/repo-init.bat
@@ -1,7 +1,17 @@
 @echo off
 title Repo Init DedupFS
-rem Init options:
-rem First parameter: <repository directory>
-rem Log directory: -DLOG_BASE=<log base directory>
-"%~dp0jre\bin\java" "-DLOG_BASE=%~dp0\log" -cp "%~dp0lib\*" dedup.init %*
+set BUNDLEDJAVA="%~dp0jre\bin\java"
+if exist %BUNDLEDJAVA% ( set JAVA=BUNDLEDJAVA ) else ( set JAVA=java )
+for /f tokens^=2-3^ delims^=.^" %%j in ('%JAVA% -fullversion 2^>^&1') do set JAVAVERSION=%%j.%%k
+if not "%JAVAVERSION%"=="11.1" (
+  echo "This software has been tested with Java 11.0.* only."
+  echo "Detected Java version: %JAVAVERSION%.*"
+  pause
+  exit /B 1
+)
+
+rem ### Init options ###
+rem # Log directory: -DLOG_BASE=<log base directory>
+rem # Parameter    : <repository directory>
+%JAVA% "-DLOG_BASE=%~dp0log" -cp "%~dp0lib\*" dedup.init %*
 pause
