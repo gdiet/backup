972c1dd48862ae6ed50a4d988c5aae1ce7cbd94a
gdiet <gdiet@users.noreply.github.com>
2021-07-11 15:49:56 +0200

    fixed Windows start script

--- scala3/src/main/script/repo-init.bat
+++ scala3/src/main/script/repo-init.bat
@@ -3,7 +3,7 @@ title Repo Init DedupFS
 set BUNDLEDJAVA="%~dp0jre\bin\java"
 if exist %BUNDLEDJAVA% ( set JAVA=BUNDLEDJAVA ) else ( set JAVA=java )
 for /f tokens^=2-3^ delims^=.^" %%j in ('%JAVA% -fullversion 2^>^&1') do set JAVAVERSION=%%j.%%k
-if not "%JAVAVERSION%"=="11.1" (
+if not "%JAVAVERSION%"=="11.0" (
   echo "This software has been tested with Java 11.0.* only."
   echo "Detected Java version: %JAVAVERSION%.*"
   pause
@@ -13,5 +13,5 @@ if not "%JAVAVERSION%"=="11.1" (
 rem ### Init options ###
 rem # Log directory: -DLOG_BASE=<log base directory>
 rem # Parameter    : <repository directory>
-%JAVA% "-DLOG_BASE=%~dp0log" -cp "%~dp0lib\*" dedup.init %*
+%JAVA% "-DLOG_BASE=%~dp0log" -Xmx512m -cp "%~dp0lib\*" dedup.init %*
 pause
