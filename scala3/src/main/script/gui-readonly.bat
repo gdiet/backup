8d4c9dfeab1f33c635b1185b55f735d1cac3991b
georg <gdiet@users.noreply.github.com>
2021-07-20 17:34:09 +0200

    fixed batch scripts

--- scala3/src/main/script/gui-readonly.bat
+++ scala3/src/main/script/gui-readonly.bat
@@ -1,7 +1,7 @@
 @echo off
 title DedupFS
-set BUNDLEDJAVA="%~dp0jre\bin\java"
-if exist %BUNDLEDJAVA% ( set JAVA=BUNDLEDJAVA ) else ( set JAVA=java )
+set BUNDLEDJAVA="%~dp0jre\bin\javaw.exe"
+if exist %BUNDLEDJAVA% ( set JAVA=%BUNDLEDJAVA% ) else ( set JAVA=javaw )
 for /f tokens^=2-3^ delims^=.^" %%j in ('%JAVA% -fullversion 2^>^&1') do set JAVAVERSION=%%j.%%k
 if not "%JAVAVERSION%"=="11.0" (
   echo "This software has been tested with Java 11.0.* only."
@@ -19,5 +19,4 @@ rem # copyWhenMoving=true         | default: false
 rem # gui=true                    | default: false, true in script - true to show server GUI
 rem # temp=<temp dir>             | default: 'dedupfs-temp' in the user's temp dir
 echo Starting the DedupFS server GUI now - it will run detached from the command line.
-%JAVA%w "-DLOG_BASE=%~dp0log" -Xmx80m -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.mount gui=true readOnly=true %*
-pause
+start "DedupFS" %JAVA% "-DLOG_BASE=%~dp0log" -Xmx80m -Dfile.encoding=UTF-8 -cp "%~dp0lib\*" dedup.mount gui=true readOnly=true %*
--- scala3/src/main/script/stats.bat
+++ scala3/src/main/script/stats.bat
@@ -1,7 +1,7 @@
 @echo off
 title Statistics - DedupFS
-set BUNDLEDJAVA="%~dp0jre\bin\java"
-if exist %BUNDLEDJAVA% ( set JAVA=BUNDLEDJAVA ) else ( set JAVA=java )
+set BUNDLEDJAVA="%~dp0jre\bin\java.exe"
+if exist %BUNDLEDJAVA% ( set JAVA=%BUNDLEDJAVA% ) else ( set JAVA=java )
 for /f tokens^=2-3^ delims^=.^" %%j in ('%JAVA% -fullversion 2^>^&1') do set JAVAVERSION=%%j.%%k
 if not "%JAVAVERSION%"=="11.0" (
   echo "This software has been tested with Java 11.0.* only."
