cfb7f71b1c923c6c1d67fc08ff1f31473c138ad4
georg <gdiet@users.noreply.github.com>
2021-12-20 16:25:04 +0100

    update scripts to expect Java 17

--- src/main/script/helpers/set-java.bat
+++ src/main/script/helpers/set-java.bat
@@ -1,8 +1,8 @@
 set BUNDLEDJAVA="%~dp0..\jre\bin\java.exe"
 if exist %BUNDLEDJAVA% ( set JAVA=%BUNDLEDJAVA% ) else ( set JAVA=java )
 for /f tokens^=2-3^ delims^=.^" %%j in ('%JAVA% -fullversion 2^>^&1') do set JAVAVERSION=%%j.%%k
-if not "%JAVAVERSION%"=="11.0" (
-  echo "This software has been tested with Java 11.0.* only."
+if not "%JAVAVERSION%"=="17.0" (
+  echo "This software has been tested with Java 17.0.* only."
   echo "Detected Java version: %JAVAVERSION%.*"
   pause
   exit /B 1
--- src/main/script/helpers/set-javaw.bat
+++ src/main/script/helpers/set-javaw.bat
@@ -1,8 +1,8 @@
 set BUNDLEDJAVAW="%~dp0..\jre\bin\javaw.exe"
 if exist %BUNDLEDJAVAW% ( set JAVAW=%BUNDLEDJAVAW% ) else ( set JAVAW=javaw )
 for /f tokens^=2-3^ delims^=.^" %%j in ('%JAVAW% -fullversion 2^>^&1') do set JAVAVERSION=%%j.%%k
-if not "%JAVAVERSION%"=="11.0" (
-  echo "This software has been tested with Java 11.0.* only."
+if not "%JAVAVERSION%"=="17.0" (
+  echo "This software has been tested with Java 17.0.* only."
   echo "Detected Java version: %JAVAVERSION%.*"
   pause
   exit /B 1
