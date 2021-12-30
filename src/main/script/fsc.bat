9aa0c8e64bd40c5af2ddf11aca071665ae3794bb
georg <gdiet@users.noreply.github.com>
2021-12-30 12:14:10 +0100

    don't set a console title for fsc

--- src/main/script/fsc.bat
+++ src/main/script/fsc.bat
@@ -1,7 +1,7 @@
 @echo off
-title Statistics - DedupFS
 call %~dp0helpers\set-java.bat
 if errorlevel 1 exit /B %errorlevel%
+
 
 rem ### Parameters ###
 rem # repo=<repository directory>     | default: '..' (parent of working directory)
