b4da5c02f635713836bd40105a55e117a1743c5f
georg <gdiet@users.noreply.github.com>
2022-01-26 19:48:54 +0100

    more blacklisting specs

--- src/test/scala/dedup/db/BlacklistSpec.scala
+++ src/test/scala/dedup/db/BlacklistSpec.scala
@@ -11,6 +11,7 @@ class BlacklistSpec extends org.scalatest.freespec.AnyFreeSpec:
       val black1 = db.mkFile(blacklist, "black1", now, DataId(-1)).get
       val black1d = db.newDataIdFor(black1)
       db.insertDataEntry(black1d, 1, 10, 10, 20, Array())
+      val black2 = db.mkFile(blacklist, "black2", now, black1d).get
 //
 //      val data1 = db.newDataIdFor(file1)
 //      val dir1 = db.mkDir(root.id, "dir1").get
@@ -20,7 +21,8 @@ class BlacklistSpec extends org.scalatest.freespec.AnyFreeSpec:
       f(db, Map(
         "blacklist" -> blacklist,
         "black1" -> black1,
-        "black1d" -> black1d.toLong
+        "black1d" -> black1d.toLong,
+        "black2" -> black2,
       ))
     }
 
@@ -29,5 +31,13 @@ class BlacklistSpec extends org.scalatest.freespec.AnyFreeSpec:
       assert(db.parts(DataId(ids("black1d"))) == Vector((10, 10)))
       blacklist.processInternalBlacklist(db, "blacklist", "/blacklist", ids("blacklist"), false)
       assert(db.parts(DataId(ids("black1d"))) == Vector())
+    }
+  }
+
+  "processInternalBlacklist doesn't delete copies in the blacklist folder" in {
+    dbWithContents { case (db, ids) =>
+      blacklist.processInternalBlacklist(db, "blacklist", "/blacklist", ids("blacklist"), true)
+      assert(db.entry(ids("black1")).get.getClass == classOf[FileEntry])
+      assert(db.entry(ids("black2")).get.getClass == classOf[FileEntry])
     }
   }
