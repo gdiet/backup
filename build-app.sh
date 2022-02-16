d7321d8271697429f255af22ea4a5618ec8ebd76
georg <gdiet@users.noreply.github.com>
2022-02-16 20:05:07 +0100

    Java 17 update

--- build-app.sh
+++ build-app.sh
@@ -18,12 +18,12 @@ if [ "$1" ]; then
 fi
 
 # Fetch JREs if necessary. Find newer releases here: https://adoptium.net/releases.html
-jreFolder=jdk-17.0.1%2B12
-jre=OpenJDK17U-jre_x64_windows_hotspot_17.0.1_12.zip
+jreFolder=jdk-17.0.2%2B8
+jre=OpenJDK17U-jre_x64_windows_hotspot_17.0.2_8.zip
 if [ ! -f "$jre" ]; then
   wget https://github.com/adoptium/temurin17-binaries/releases/download/$jreFolder/$jre || exit 1
 fi
-jreLinux=OpenJDK17U-jre_x64_linux_hotspot_17.0.1_12.tar.gz
+jreLinux=OpenJDK17U-jre_x64_linux_hotspot_17.0.2_8.tar.gz
 if [ ! -f "$jreLinux" ]; then
   wget https://github.com/adoptium/temurin17-binaries/releases/download/$jreFolder/$jreLinux || exit 1
 fi
