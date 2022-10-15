gdiet/backup

commit a79e61d38930148a210ff81aa0a653bad0d3979e

cleanup: child check when deleting an entry

src/main/scala/dedup/db/maintenance.scala
@@ -83,14 +83,16 @@ object maintenance extends util.ClassLogging:
       case None =>
         println(s"The path '$path' does not exist.")
       case Some(file: FileEntry) =>
-        db.delete(file.id)
-        log.info(s"Marked deleted file '$path' .. ${readableBytes(db.dataSize(file.dataId))}")
+        if db.deleteChildless(file.id)
+        then log.info(s"Marked deleted file '$path' .. ${readableBytes(db.dataSize(file.dataId))}")
+        else log.warn(s"Could not delete file with children: '$path'")
       case Some(dir: DirEntry) =>
         log.info(s"Marking deleted directory '$path' ...")
         def delete(treeEntry: TreeEntry): Long =
           val childCount = db.children(treeEntry.id).map(delete).sum
-          db.delete(treeEntry.id)
-          log.debug(s"Marked deleted: $treeEntry")
+          if db.deleteChildless(treeEntry.id)
+          then log.debug(s"Marked deleted: $treeEntry")
+          else log.warn(s"Could not delete file with children: '$path'")
           childCount + 1
         log.info(s"Marked deleted ${delete(dir)} files/directories.")
   }
@@ -221,6 +223,8 @@ object blacklist extends util.ClassLogging:
             .filterNot(_._2.startsWith(s"/$dfsBlacklist/"))
           filteredCopies.foreach { (id, path) =>
             log.info(s"Deleting copy of entry: $path")
-            db.delete(id)
+            if db.deleteChildless(file.id)
+            then log.info(s"Marked deleted file '$path' .. ${readableBytes(db.dataSize(file.dataId))}")
+            else log.warn(s"Could not delete file with children: '$path'")
           }
     }

src/main/scala/dedup/db/Database.scala
@@ -169,9 +169,13 @@ class Database(connection: Connection) extends util.ClassLogging:
   private val dTreeEntry = prepare(
     "UPDATE TreeEntries SET deleted = ? WHERE id = ?"
   )
-  def delete(id: Long): Unit = synchronized {
-    val count = dTreeEntry.set(now.nonZero, id).executeUpdate()
-    ensure("db.delete", count == 1, s"For id $id, delete count is $count instead of 1.")
+  /** Deletes a tree entry unless it has children.
+    * @return [[false]] if the tree entry has children. */
+  def deleteChildless(id: Long): Boolean = synchronized {
+    if children(id).nonEmpty then false else
+      val count = dTreeEntry.set(now.nonZero, id).executeUpdate()
+      ensure("db.delete", count == 1, s"For id $id, delete count is $count instead of 1.")
+      true
   }
 
   private val iDir = prepare(

src/main/scala/dedup/server/Level1.scala
@@ -15,13 +15,15 @@ class Level1(settings: Settings) extends AutoCloseable with util.ClassLogging:
   /** id -> (handle count, dataEntry). Remember to synchronize. */
   private var files = Map[Long, (Int, DataEntry)]()
 
-  def delete(entry: TreeEntry): Unit =
+  /** Deletes a tree entry unless it has children.
+    * @return [[false]] if the tree entry has children. */
+  def deleteChildless(entry: TreeEntry): Boolean =
     watch(s"delete($entry)") {
       // From man unlink(2)
       // If the name was the last link to a file but any processes still have the file open,
       // the file will remain in existence until the last file descriptor referring to it is closed.
       // ... This means that we keep the file open in level 1 if it currently is open ...
-      backend.delete(entry.id)
+      backend.deleteChildless(entry.id)
     }
 
   def createAndOpen(parentId: Long, name: String, time: Time): Option[Long] =

src/main/scala/dedup/server/Level2.scala
@@ -37,7 +37,7 @@ class Level2(settings: Settings) extends AutoCloseable with util.ClassLogging:
   private val con = db.H2.connection(settings.dbDir, settings.readonly)
   private val database = db.Database(con)
   private val freeAreas = if settings.readonly then FreeAreas() else FreeAreas(database.freeAreas())
-  export database.{child, children, delete, entry, mkDir, mkFile, setTime, split, update}
+  export database.{child, children, deleteChildless, entry, mkDir, mkFile, setTime, split, update}
 
   /** id -> DataEntry. Remember to synchronize. */
   private var files = Map[Long, DataEntry]()

src/main/scala/dedup/server/Server.scala
@@ -108,15 +108,14 @@ class Server(settings: Settings) extends FuseStubFS with util.ClassLogging:
                 case (_           , previous         ) =>
                   // Other than the contract of rename (see https://linux.die.net/man/2/rename), the
                   // replace operation is not atomic. This is tolerated in order to simplify the code.
-                  // TODO rename here replaces non-empty directories where the contract requires to return ENOTEMPTY or EEXIST
-                  previous.foreach(backend.delete)
-                  if origin.parentId != targetDir.id && settings.copyWhenMoving.get() then
+                  if !previous.forall(backend.deleteChildless) then ENOTEMPTY
+                  else if origin.parentId != targetDir.id && settings.copyWhenMoving.get() then
                     def copy(source: TreeEntry, newName: String, newParentId: Long): Boolean = source match
                       case file: FileEntry =>
                         backend.copyFile(file, newParentId, newName)
                       case dir : DirEntry =>
                         backend.mkDir(newParentId, newName)
-                          .exists(dirId => backend.children(source.id).forall(child => copy(child, child.name, dirId)))
+                          .exists(dirId => backend.children(dir.id).forall(child => copy(child, child.name, dirId)))
                     if (copy(origin, newName, targetDir.id)) OK else EEXIST
                   else
                     backend.update(origin.id, targetDir.id, newName)
@@ -126,7 +125,7 @@ class Server(settings: Settings) extends FuseStubFS with util.ClassLogging:
   override def rmdir(path: String): Int =
     if settings.readonly then EROFS else fs("rmdir $path") {
       backend.entry(path) match
-        case Some(dir: DirEntry) => if backend.children(dir.id).nonEmpty then ENOTEMPTY else { backend.delete(dir); OK }
+        case Some(dir: DirEntry) => if backend.deleteChildless(dir) then OK else ENOTEMPTY
         case Some(_)             => ENOTDIR
         case None                => ENOENT
     }
@@ -242,5 +241,7 @@ class Server(settings: Settings) extends FuseStubFS with util.ClassLogging:
       backend.entry(path) match
         case None => ENOENT
         case Some(_: DirEntry) => EISDIR
-        case Some(file: FileEntry) => backend.delete(file); OK
+        case Some(file: FileEntry) =>
+          if backend.deleteChildless(file) then OK
+          else { log.warn(s"Can't delete regular file with children: $path"); ENOTEMPTY }
     }

commit 65c1f668b56791b5b85d3208d1f45f10fde4d9f3

fixed a blacklisting bug introduced in a79e61d3

src/main/scala/dedup/db/maintenance.scala
@@ -223,7 +223,7 @@ object blacklist extends util.ClassLogging:
             .filterNot(_._2.startsWith(s"/$dfsBlacklist/"))
           filteredCopies.foreach { (id, path) =>
             log.info(s"Deleting copy of entry: $path")
-            if db.deleteChildless(file.id)
+            if db.deleteChildless(id)
             then log.info(s"Marked deleted file '$path' .. ${readableBytes(db.dataSize(file.dataId))}")
             else log.warn(s"Could not delete file with children: '$path'")
           }