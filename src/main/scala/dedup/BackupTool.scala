d0fb2f8dfbe79955a56a25b96ea5bdaea15b72da
georg <gdiet@users.noreply.github.com>
2022-10-29 22:23:05 +0200

    continuing backup logic

--- src/main/scala/dedup/BackupTool.scala
+++ src/main/scala/dedup/BackupTool.scala
@@ -21,21 +21,21 @@ object BackupTool extends ClassLogging:
       case from :: to              :: Nil => (File(from), insertDate(to), None)
       case other => main.failureExit(s"Expected parameters '[from] [to] [optional: reference]', got '${other.mkString(" ")}'")
 
-//    // TODO lots of copy-paste from '@main def mount(opts: (String, String)*): Unit'
-//    val repo = opts.repo
-//    val backup = opts.defaultFalse("dbBackup")
-//    val temp = File(opts.getOrElse("temp", sys.props("java.io.tmpdir") + s"/dedupfs-temp/$now"))
-//    val dbDir = db.dbDir(repo)
-//    if !dbDir.exists() then main.failureExit(s"It seems the repository is not initialized - can't find the database directory: $dbDir")
-//    db.H2.checkForTraceFile(dbDir)
-//    val settings = server.Settings(repo, dbDir, temp, false, AtomicBoolean(false))
-//    temp.mkdirs()
-//    if !temp.isDirectory then main.failureExit(s"Temp dir is not a directory: $temp")
-//    if !temp.canWrite then main.failureExit(s"Temp dir is not writable: $temp")
-//    if temp.list.nonEmpty then log.warn(s"Note that temp dir is not empty: $temp")
-//    cache.MemCache.startupCheck()
-//    if backup then db.maintenance.backup(settings.dbDir)
-//    // TODO end of copy-paste
+    // TODO lots of copy-paste from '@main def mount(opts: (String, String)*): Unit'
+    val repo = opts.repo
+    val backup = opts.defaultFalse("dbBackup")
+    val temp = File(opts.getOrElse("temp", sys.props("java.io.tmpdir") + s"/dedupfs-temp/$now"))
+    val dbDir = db.dbDir(repo)
+    if !dbDir.exists() then main.failureExit(s"It seems the repository is not initialized - can't find the database directory: $dbDir")
+    db.H2.checkForTraceFile(dbDir)
+    val settings = server.Settings(repo, dbDir, temp, false, AtomicBoolean(false))
+    temp.mkdirs()
+    if !temp.isDirectory then main.failureExit(s"Temp dir is not a directory: $temp")
+    if !temp.canWrite then main.failureExit(s"Temp dir is not writable: $temp")
+    if temp.list.nonEmpty then log.warn(s"Note that temp dir is not empty: $temp")
+    cache.MemCache.startupCheck()
+    if backup then db.maintenance.backup(settings.dbDir)
+    // TODO end of copy-paste
 
     if !from.canRead then main.failureExit(s"The backup source $from can't be read.")
 
@@ -44,29 +44,32 @@ object BackupTool extends ClassLogging:
     log.info (s"Backup source:    $from")
     log.info (s"Backup target:    $to")
     log.info (s"Backup reference: $reference")
-//    log.debug(s"Temp dir:         $temp")
-//
-//    resource(server.Level1(settings)) { fs =>
-//      val (targetPath, targetName) = fs.split(to).pipe(target =>
-//          target.dropRight(1) -> target.lastOption.getOrElse(main.failureExit(s"Invalid target: No file name in '$to'."))
-//      )
-//      def targetPathForLog = s"The target's parent DedupFS:/${targetPath.mkString("/")}"
-//      val targetParent = fs.entry(targetPath) match
-//        case Some(dir: DirEntry) => dir
-//        case Some(_: FileEntry) => main.failureExit(s"Invalid target: $targetPathForLog points to a file.")
-//        case None               => main.failureExit(s"Invalid target: $targetPathForLog does not exist.")
-//      val targetId = fs.mkDir(targetParent.id, targetName).getOrElse(
-//        main.failureExit(s"Invalid target: DedupFS:$to already exists.")
-//      )
-//      log.info(s"Created 'DedupFS:$to'.")
-//      // TODO continue
-//
-//      // .backupignore
-//      // if empty, ignore the directory
-//      // otherwise, interpret in a .gitignore like fashion
-//    }
+    log.debug(s"Temp dir:         $temp")
 
-    processRecurse(Seq((0, Seq(), "/", from)), (_, _) => 0, (_, _) => ())
+    resource(server.Level1(settings)) { fs =>
+      val (targetPath, targetName) = fs.split(to).pipe(target =>
+          target.dropRight(1) -> target.lastOption.getOrElse(main.failureExit(s"Invalid target: No file name in '$to'."))
+      )
+      def targetPathForLog = s"The target's parent DedupFS:/${targetPath.mkString("/")}"
+      val targetParent = fs.entry(targetPath) match
+        case Some(dir: DirEntry) => dir
+        case Some(_: FileEntry) => main.failureExit(s"Invalid target: $targetPathForLog points to a file.")
+        case None               => main.failureExit(s"Invalid target: $targetPathForLog does not exist.")
+      val targetId = fs.mkDir(targetParent.id, targetName).getOrElse(
+        main.failureExit(s"Invalid target: DedupFS:$to already exists.")
+      )
+      log.info(s"Created 'DedupFS:$to'.")
+
+      def mkDir(parent: Long, name: String): Long =
+        fs.mkDir(parent, name).getOrElse(main.failureExit(s"Unexpected name conflict creating folder $name."))
+
+      def store(parent: Long, file: File): Unit =
+        // TODO implement
+        log.info(s"Stored $file")
+
+      processRecurse(Seq((0, Seq(), "/", from)), mkDir, store)
+    }
+
 
   @annotation.tailrec
   private def processRecurse(sources: Seq[(Long, Seq[String], String, File)], mkDir: (Long, String) => Long, store: (Long, File) => Unit): Unit =
