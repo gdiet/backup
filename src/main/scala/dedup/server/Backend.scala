ec8c6ca466a4a10cbce23bf4baf4952efa41f2c8
georg <gdiet@users.noreply.github.com>
2022-12-06 16:24:42 +0100

    more handles code

--- src/main/scala/dedup/server/Backend.scala
+++ src/main/scala/dedup/server/Backend.scala
@@ -1,10 +1,14 @@
 package dedup
 package server
 
-class Backend(settings: Settings) extends util.ClassLogging:
+class Backend(settings: Settings) extends AutoCloseable with util.ClassLogging:
   private val lts = store.LongTermStore(settings.dataDir, settings.readonly)
   private val db = dedup.db.DB(dedup.db.H2.connection(settings.dbDir, settings.readonly))
   private val handles = Handles()
+
+  override def close(): Unit =
+    handles.shutdown() // FIXME handle the result
+    lts.close()
 
   /** @return The size of the file. If the cached size if any or the logical size of the file's data entry. */
   def size(fileEntry: FileEntry): Long =
--- src/main/scala/dedup/server/Handles.scala
+++ src/main/scala/dedup/server/Handles.scala
@@ -6,12 +6,51 @@ class Handles extends util.ClassLogging:
   private var files = Map[Long, (Int, DataId, Option[DataEntry], Seq[DataEntry])]()
   private var closing = false
 
-  def shutdown(): Unit = synchronized {
+  /** Decrement the open count for a file. On reaching zero, move the current data entry to the storing queue.
+    *
+    * @return The data entry moved to the storing queue if the storing queue was empty before.
+    *         In this case, the returned data entry needs to be enqueued yet. */
+  def release(fileId: Long): Option[DataEntry] = synchronized {
+    files.get(fileId) match
+      case None =>
+        log.warn(s"release called for file $fileId that is currently not open.")
+        None
+      case Some((count, dataId, current, storing)) =>
+        if count < 1 then
+          log.error(s"Handle count $count for file $fileId.")
+          files += fileId -> (1, dataId, current, storing)
+          release(fileId)
+        else if count > 1 then
+          files += fileId -> (count - 1, dataId, current, storing)
+          None
+        else (dataId, current, storing) match
+          case (_, None, Seq()) =>
+            files -= fileId
+            None
+          case (dataId, Some(current), Seq()) =>
+            files += fileId -> (0, dataId, None, Seq(current))
+            Some(current)
+          case (dataId, current, storing) =>
+            files += fileId -> (0, dataId, None, current ++: storing)
+            None
+  }
+
+  /** @return The data entries that still need to be enqueued. */
+  def shutdown(): Map[Long, Option[DataEntry]] = synchronized {
     closing = true
-    // TODO close write handles
+    log.debug(s"Shutting down - ${files.size} handles present.")
+    val openWriteHandles = files.collect {
+      case fileId -> (count, dataId, Some(current), storing) =>
+        if count < 1 then log.error(s"Handle count $count for write handle of file $fileId at shutdown.")
+        files += fileId -> (1, dataId, Some(current), storing)
+        fileId -> release(fileId)
+    }
+    if openWriteHandles.nonEmpty then
+      log.warn(s"Still ${openWriteHandles.size} open write handles when unmounting the file system.")
     // On Windows, it's sort of normal to still have read file handles open when shutting down the file system.
     val openReadHandles = files.values.count(_._1 > 0)
     if openReadHandles > 0 then log.debug(s"Still $openReadHandles open read handles when unmounting the file system.")
+    openWriteHandles
   }
 
   /** Increments the count of the handle, creating it if necessary.
