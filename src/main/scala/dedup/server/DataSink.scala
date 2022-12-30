3319d078b5834e4d962e970e993f9aaad3842c40
georg <gdiet@users.noreply.github.com>
2022-12-30 17:57:52 +0100

    removed obsolete DataSink

--- src/main/scala/dedup/server/DataSink.scala
+++
@@ -1,9 +0,0 @@
-package dedup
-package server
-
-trait DataSink[D]:
-  extension(d: D) def write(offset: Long, data: Array[Byte]): Unit
-
-given DataSink[jnr.ffi.Pointer] with
-  extension(d: jnr.ffi.Pointer) def write(offset: Long, data: Array[Byte]): Unit =
-    d.put(offset, data, 0, data.length)
