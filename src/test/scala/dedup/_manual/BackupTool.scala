cd2324c0e331f14b60c8ae500e7d4f678a845db2
georg <gdiet@users.noreply.github.com>
2023-02-07 21:26:17 +0100

    updated manual test run for the backup tool

--- src/test/scala/dedup/_manual/BackupTool.scala
+++ src/test/scala/dedup/_manual/BackupTool.scala
@@ -3,7 +3,7 @@ package dedup._manual
 object BackupTool extends App:
   val repo = java.io.File("./manual")
   dedup.fsc("repo=./manual", "dbBackup=false", "backup",
-    """c:\Users\Georg.Dietrich\Downloads\decryption\target""",
-    "/target/backup/?[yyyy]/![yyyy.MM.dd_HH.mm_ss]",
-    "reference=/tar?et/backup/*/*"
+    """c:\georg\temp\dedupfs-demo\Beispieldaten""",
+    "/backup/?[yyyy]/![yyyy.MM.dd_HH.mm_ss]"
+//    , "reference=/target/backup/*/*"
   )
