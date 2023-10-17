2c47230eda3b3a1d3be17047d84ee119300d32a1
georg <gdiet@users.noreply.github.com>
2023-10-17 16:09:07 +0200

    updated set-java*.bat for Java 21

--- src/main/script/helpers/set-java.bat
+++ src/main/script/helpers/set-java.bat
@@ -1,7 +1,7 @@
 set BUNDLEDJAVA="%~dp0..\jre\bin\java.exe"
 if exist %BUNDLEDJAVA% ( set JAVA=%BUNDLEDJAVA% ) else ( set JAVA=java )
-for /f tokens^=2-3^ delims^=.^" %%j in ('%JAVA% -fullversion 2^>^&1') do set JAVAVERSION=%%j.%%k
-if not "%JAVAVERSION%"=="17.0" (
+for /f tokens^=2^ delims^=.+^" %%j in ('%JAVA% -fullversion 2^>^&1') do set JAVAVERSION=%%j
+if not "%JAVAVERSION%"=="21" (
   echo "This software has been tested with Java 17.0.* only."
   echo "Detected Java version: %JAVAVERSION%.*"
   pause
--- src/main/script/helpers/set-javaw.bat
+++ src/main/script/helpers/set-javaw.bat
@@ -1,8 +1,8 @@
 set BUNDLEDJAVAW="%~dp0..\jre\bin\javaw.exe"
 if exist %BUNDLEDJAVAW% ( set JAVAW=%BUNDLEDJAVAW% ) else ( set JAVAW=javaw )
-for /f tokens^=2-3^ delims^=.^" %%j in ('%JAVAW% -fullversion 2^>^&1') do set JAVAVERSION=%%j.%%k
-if not "%JAVAVERSION%"=="17.0" (
-  echo "This software has been tested with Java 17.0.* only."
+for /f tokens^=2^ delims^=.+^" %%j in ('%JAVAW% -fullversion 2^>^&1') do set JAVAVERSION=%%j
+if not "%JAVAVERSION%"=="21" (
+  echo "This software has been tested with Java 21 only."
   echo "Detected Java version: %JAVAVERSION%.*"
   pause
   exit /B 1
