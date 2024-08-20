b363e9346f9be54c993d24bb69b3530ba6483f31
georg <gdiet@users.noreply.github.com>
2024-08-20 19:26:05 +0200

    fix explaining comments in fsc scripts

--- src/main/script/fsc.bat
+++ src/main/script/fsc.bat
@@ -4,10 +4,13 @@ if errorlevel 1 exit /B %errorlevel%
 
 
 rem ### Parameters ###
-rem # repo=<repository directory>     | default: '..' (parent of working directory)
+rem # repo=<repository directory> | default: '..' (parent of working directory)
 rem ### Commands ###
-rem # backup            | create a database backup
-rem # find <matcher>    | find files with matching file names, e.g. '%.scala'
-rem # list <path>       | list directory or show file info
-rem # del  <path>       | mark a file or recursively mark a directory as deleted
+rem # backup <source> <target> [options] | store files in the backup repository
+rem # db-backup                          | create a database backup
+rem # db-restore [backup-zip]            | restore a database backup
+rem # db-compact                         | compact the database file
+rem # find <pattern>   | find files with matching file names, e.g. '*.scala'
+rem # list <path>      | list directory or show file info
+rem # del <path>       | mark a file or recursively mark a directory as deleted
 %JAVA% "-DLOG_BASE=%~dp0log" -Xmx256m -cp "%~dp0lib\*" dedup.fsc %*
