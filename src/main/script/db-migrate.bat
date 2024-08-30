3eaa865a81dc8dd87019142b2e952d1a08b076e3
georg <gdiet@users.noreply.github.com>
2024-08-30 07:40:13 +0200

    simplify batch error handling

--- src/main/script/db-migrate.bat
+++ src/main/script/db-migrate.bat
@@ -3,12 +3,8 @@ call "%~dp0helpers\set-java.bat"
 if errorlevel 1 exit /B %errorlevel%
 
 %JAVA% "-DLOG_BASE=%~dp0log" -Xmx256m -cp "%~dp0lib\*;%~dp0lib-h2-previous\*" dedup.fsc db-migrate1
-rem clean up errorlevel handling here and everywhere else
-if errorlevel 1 (
-    echo Database migration step 1 finished with error code %errorlevel%, exiting...
-    pause
-    exit /b %errorlevel%
-) else if errorlevel 0 (
+rem clean up errorlevel handling everywhere else
+if errorlevel 0 if not errorlevel 1 (
     echo Database migration step 1 finished.
 ) else (
     echo Database migration step 1 finished with error code %errorlevel%, exiting...
@@ -16,11 +12,7 @@ if errorlevel 1 (
     exit /b %errorlevel%
 )
 %JAVA% "-DLOG_BASE=%~dp0log" -Xmx256m -cp "%~dp0lib\*;%~dp0lib-h2\*"          dedup.fsc db-migrate2
-if errorlevel 1 (
-    echo Database migration step 2 finished with error code %errorlevel%, exiting...
-    pause
-    exit /b %errorlevel%
-) else if errorlevel 0 (
+if errorlevel 0 if not errorlevel 1 (
     echo Database migration step 2 finished.
     pause
 ) else (
