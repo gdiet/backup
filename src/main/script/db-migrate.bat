4c2c947c271346def405e4c018b535e8a1eeee5d
georg <gdiet@users.noreply.github.com>
2024-08-30 16:05:11 +0200

    add intro echo to db-migrate.bat

--- src/main/script/db-migrate.bat
+++ src/main/script/db-migrate.bat
@@ -3,6 +3,15 @@ title DB Migrate - DedupFS
 call "%~dp0helpers\set-java.bat"
 if errorlevel 1 exit /B %errorlevel%
 
+echo.
+echo This utility will update a DedupFS database created by the previous
+echo major release to the current major release.  If you do not want
+echo that, simply close the console window where this utility is running.
+echo.
+pause
+echo.
+echo Starting database migration...
+
 %JAVA% "-DLOG_BASE=%~dp0log" -Xmx256m -cp "%~dp0lib\*;%~dp0lib-h2-previous\*" dedup.fsc db-migrate1
 if errorlevel 0 if not errorlevel 1 (
     echo Database migration step 1 finished.
