64178709c194149806424b6e34fd754f8d64ab2b
georg <gdiet@users.noreply.github.com>
2024-09-02 07:57:06 +0200

    use a different log configuration for the readonly fsc commands

--- src/main/script/fsc.bat
+++ src/main/script/fsc.bat
@@ -13,9 +13,9 @@ rem # db-compact                         | compact the database file
 rem # find <pattern>   | find files with matching file names, e.g. '*.scala'
 rem # list <path>      | list directory or show file info
 rem # del <path>       | mark a file or recursively mark a directory as deleted
-%JAVA% "-DLOG_BASE=%~dp0log" -Xmx256m -cp "%~dp0lib\*;%~dp0lib-h2\*" dedup.fsc %*
-if errorlevel 0 if not errorlevel 1 (
-) else (
-    echo fsc finished with error code %errorlevel%, exiting...
-    exit /b %errorlevel%
+IF NOT "%1" == "list" IF NOT "%1" == "fsc" (
+  %JAVA% "-DLOG_BASE=%~dp0log" -Xmx256m -cp "%~dp0lib\*;%~dp0lib-h2\*" dedup.fsc %*
+) ELSE (
+  %JAVA% "-DLOG_BASE=%~dp0log" -Xmx256m -Dlogback.configurationFile=logback-readonly.xml -cp "%~dp0lib\*;%~dp0lib-h2\*" dedup.fsc %*
 )
+rem # No pause, no additional output, fsc is a plain command line tool.
--- src/test/manual/README.md
+++ src/test/manual/README.md
@@ -11,3 +11,7 @@
   - [x] 2024-09-01 6.0.0-M3 c919ca34
 - Linux: Trying to create a new repository when repository is already initialized & good failure information is displayed
   - [x] ???
+
+## Plans for Logging and Command Line Output
+
+Write operations should write to logfile. Read-only operations should only write to console. They should be silent except for the actual command output unless an error occurs.
