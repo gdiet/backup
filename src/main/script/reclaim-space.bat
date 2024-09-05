f56549cc8833b324cfcdaa7b6b38ff5e90a4f029
georg <gdiet@users.noreply.github.com>
2024-09-05 18:23:23 +0200

    add confirmation message to reclaim space scripts

--- src/main/script/reclaim-space.bat
+++ src/main/script/reclaim-space.bat
@@ -3,6 +3,16 @@ title Reclaim Space - DedupFS
 call "%~dp0helpers\set-java.bat"
 if errorlevel 1 exit /B %errorlevel%
 
+echo.
+echo This utility will clean deleted file entries and orphan data entries from the
+echo DedupFS database, so their disk space is released and can be used when storing
+echo new files. If you do not want that, simply close the console window where this
+echo utility is running.
+echo.
+pause
+echo.
+echo Starting database migration...
+
 rem ### Parameters ###
 rem # repo=<repository directory>       | default: '..' (parent of working directory)
 rem # keepDays=<number of days to keep> | default: 0 - the 'keepDays=' prefix can be omitted
