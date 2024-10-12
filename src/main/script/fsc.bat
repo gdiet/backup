44de037497d1c3e7a8942d31f5ead0beac40501d
Georg Dietrich <georg.dietrich@thieme-compliance.de>
2024-10-12 23:41:12 +0200

    introduce logback-readonly.xml

--- src/main/script/fsc.bat
+++ src/main/script/fsc.bat
@@ -14,4 +14,12 @@ rem # db-compact                         | compact the database file
 rem # find <pattern>   | find files with matching file names, e.g. '*.scala'
 rem # list <path>      | list directory or show file info
 rem # del <path>       | mark a file or recursively mark a directory as deleted
-%JAVA% "-DLOG_BASE=%~dp0log" -Xmx256m -cp "%~dp0lib\*" dedup.fsc %*
+set readonly=false
+IF "%1" == "find" set readonly=true
+IF "%1" == "list" set readonly=true
+IF "%readonly%" == "false" (
+  %JAVA% "-DLOG_BASE=%~dp0log" -Xmx256m -cp "%~dp0lib\*" dedup.fsc %*
+) ELSE (
+  %JAVA% "-DLOG_BASE=%~dp0log" -Xmx256m -Dlogback.configurationFile=logback-readonly.xml -cp "%~dp0lib\*" dedup.fsc %*
+)
+rem # No pause, no additional output, fsc is a plain command line tool.
