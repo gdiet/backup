0d8e2d1f9cca1d2490d0e503203452fa56748ea3
Georg Dietrich <georg.dietrich@thieme-compliance.de>
2024-10-12 23:26:22 +0200

    improve Java 21 version check

--- src/main/script/helpers/set-java.bat
+++ src/main/script/helpers/set-java.bat
@@ -2,7 +2,7 @@ set BUNDLEDJAVA="%~dp0..\jre\bin\java.exe"
 if exist %BUNDLEDJAVA% ( set JAVA=%BUNDLEDJAVA% ) else ( set JAVA=java )
 for /f tokens^=2^ delims^=.+^" %%j in ('%JAVA% -fullversion 2^>^&1') do set JAVAVERSION=%%j
 if not "%JAVAVERSION%"=="21" (
-  echo "This software has been tested with Java 17.0.* only."
+  echo "This software has been tested with Java 21 only."
   echo "Detected Java version: %JAVAVERSION%.*"
   pause
   exit /B 1
