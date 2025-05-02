e00aef0b0fdda555f001d388703b1053eeb69627
gdiet <gdiet@users.noreply.github.com>
2025-05-02 15:35:12 +0200

    add helpful scaladoc comment

--- src/main/scala/dedup/cache/Helpers.scala
+++ src/main/scala/dedup/cache/Helpers.scala
@@ -4,6 +4,8 @@ package cache
 object JEntry:
   def unapply[K,V](e: java.util.Map.Entry[K,V]): Option[(K,V)] = Some(e.getKey -> e.getValue)
 
-extension(long: Long) def asInt: Int =
-  ensure("cache.int", long >= 0 && long <= Int.MaxValue, s"Illegal int+ value: $long")
-  long.toInt
+extension(long: Long)
+  /** Ensures the Long value is within the range of 0 / positive Int before conversion. */
+  def asInt: Int =
+    ensure("cache.int", long >= 0 && long <= Int.MaxValue, s"Illegal int+ value: $long")
+    long.toInt
