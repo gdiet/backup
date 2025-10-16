d9d4e18cd5d47b2f02d8fa277cb27f463fe849ed
Georg Dietrich <georg.dietrich@thieme-compliance.de>
2025-10-16 20:36:25 +0200

    more idiomatic test

--- src/cache/memory_trimOverlappingAreas_test.go
+++ src/cache/memory_trimOverlappingAreas_test.go
@@ -6,88 +6,79 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestPartialOverlapTopBeforeBottom1(t *testing.T) {
-	top := dataArea{position: 0, data: bytes("hello")}
-	bottom := dataArea{position: 3, data: bytes("world")}
-	expected := dataAreas{dataArea{position: 0, data: bytes("hello")}, dataArea{position: 5, data: bytes("rld")}}
-	result := removeOverlaps(top, bottom)
-	assertDataAreasEqual(t, expected, result)
-}
-
-func TestPartialOverlapTopBeforeBottom2(t *testing.T) {
-	top := dataArea{position: 0, data: bytes("1234")}
-	bottom := dataArea{position: 2, data: bytes("abcdefg")}
-	expected := dataAreas{dataArea{position: 0, data: bytes("1234")}, dataArea{position: 4, data: bytes("cdefg")}}
-	result := removeOverlaps(top, bottom)
-	assertDataAreasEqual(t, expected, result)
-}
-
-func TestTopFullyCoversBottom1(t *testing.T) {
-	top := dataArea{position: 0, data: bytes("1234")}
-	bottom := dataArea{position: 0, data: bytes("abcd")}
-	expected := dataAreas{dataArea{position: 0, data: bytes("1234")}}
-	result := removeOverlaps(top, bottom)
-	assertDataAreasEqual(t, expected, result)
-}
-
-func TestTopFullyCoversBottom2(t *testing.T) {
-	top := dataArea{position: 0, data: bytes("1234")}
-	bottom := dataArea{position: 1, data: bytes("ab")}
-	expected := dataAreas{dataArea{position: 0, data: bytes("1234")}}
-	result := removeOverlaps(top, bottom)
-	assertDataAreasEqual(t, expected, result)
-}
-
-func TestPartialOverlapBottomBeforeStart1(t *testing.T) {
-	top := dataArea{position: 3, data: bytes("hello")}
-	bottom := dataArea{position: 0, data: bytes("world")}
-	expected := dataAreas{dataArea{position: 0, data: bytes("wor")}, dataArea{position: 3, data: bytes("hello")}}
-	result := removeOverlaps(top, bottom)
-	assertDataAreasEqual(t, expected, result)
-}
-
-func TestPartialOverlapBottomBeforeStart2(t *testing.T) {
-	top := dataArea{position: 2, data: bytes("1234")}
-	bottom := dataArea{position: 0, data: bytes("abcdef")}
-	expected := dataAreas{dataArea{position: 0, data: bytes("ab")}, dataArea{position: 2, data: bytes("1234")}}
-	result := removeOverlaps(top, bottom)
-	assertDataAreasEqual(t, expected, result)
-}
-
-func TestTopIsFullyContainedInBottom(t *testing.T) {
-	top := dataArea{position: 1, data: bytes("12")}
-	bottom := dataArea{position: 0, data: bytes("abcd")}
-	expected := dataAreas{
-		dataArea{position: 0, data: bytes("a")},
-		dataArea{position: 1, data: bytes("12")},
-		dataArea{position: 3, data: bytes("d")}}
-	result := removeOverlaps(top, bottom)
-	assertDataAreasEqual(t, expected, result)
-}
-
-func TestNoOverlapPanicTopBeforeBottom(t *testing.T) {
-	top := dataArea{position: 1, data: bytes("12")}
-	bottom := dataArea{position: 10, data: bytes("abcd")}
-	require.Panics(t, func() { removeOverlaps(top, bottom) })
-}
-
-func TestNoOverlapPanicTopAfterBottom(t *testing.T) {
-	top := dataArea{position: 10, data: bytes("12")}
-	bottom := dataArea{position: 1, data: bytes("abcd")}
-	require.Panics(t, func() { removeOverlaps(top, bottom) })
-}
-
-func assertDataAreasEqual(t *testing.T, expected, actual dataAreas) {
-	if len(expected) != len(actual) {
-		t.Errorf("Expected %d areas, got %d", len(expected), len(actual))
-		return
+func TestTrimOverlappingAreas(t *testing.T) {
+	tests := []struct {
+		name        string
+		top, bottom dataArea
+		expected    dataAreas
+		expectPanic bool
+	}{
+		{
+			name:     "Top overlaps start of bottom #1",
+			top:      dataArea{position: 0, data: bytes("hello")},
+			bottom:   dataArea{position: 3, data: bytes("world")},
+			expected: dataAreas{dataArea{position: 0, data: bytes("hello")}, dataArea{position: 5, data: bytes("rld")}},
+		},
+		{
+			name:     "Top overlaps start of bottom #2",
+			top:      dataArea{position: 0, data: bytes("1234")},
+			bottom:   dataArea{position: 2, data: bytes("abcdefg")},
+			expected: dataAreas{dataArea{position: 0, data: bytes("1234")}, dataArea{position: 4, data: bytes("cdefg")}},
+		},
+		{
+			name:     "Top fully covers bottom #1",
+			top:      dataArea{position: 0, data: bytes("1234")},
+			bottom:   dataArea{position: 0, data: bytes("abcd")},
+			expected: dataAreas{dataArea{position: 0, data: bytes("1234")}},
+		},
+		{
+			name:     "Top fully covers bottom #2",
+			top:      dataArea{position: 0, data: bytes("1234")},
+			bottom:   dataArea{position: 1, data: bytes("ab")},
+			expected: dataAreas{dataArea{position: 0, data: bytes("1234")}},
+		},
+		{
+			name:     "Top overlaps end of bottom #1",
+			top:      dataArea{position: 3, data: bytes("hello")},
+			bottom:   dataArea{position: 0, data: bytes("world")},
+			expected: dataAreas{dataArea{position: 0, data: bytes("wor")}, dataArea{position: 3, data: bytes("hello")}},
+		},
+		{
+			name:     "Top overlaps end of bottom #2",
+			top:      dataArea{position: 2, data: bytes("1234")},
+			bottom:   dataArea{position: 0, data: bytes("abcdef")},
+			expected: dataAreas{dataArea{position: 0, data: bytes("ab")}, dataArea{position: 2, data: bytes("1234")}},
+		},
+		{
+			name:   "Top is fully contained in bottom",
+			top:    dataArea{position: 1, data: bytes("12")},
+			bottom: dataArea{position: 0, data: bytes("abcd")},
+			expected: dataAreas{
+				dataArea{position: 0, data: bytes("a")},
+				dataArea{position: 1, data: bytes("12")},
+				dataArea{position: 3, data: bytes("d")}},
+		},
+		{
+			name:        "NoOverlapPanicTopBeforeBottom",
+			top:         dataArea{position: 1, data: bytes("12")},
+			bottom:      dataArea{position: 10, data: bytes("abcd")},
+			expectPanic: true,
+		},
+		{
+			name:        "NoOverlapPanicTopAfterBottom",
+			top:         dataArea{position: 10, data: bytes("12")},
+			bottom:      dataArea{position: 1, data: bytes("abcd")},
+			expectPanic: true,
+		},
 	}
-	for i := range expected {
-		if expected[i].position != actual[i].position {
-			t.Errorf("Expected position %d, got %d", expected[i].position, actual[i].position)
-		}
-		if string(expected[i].data) != string(actual[i].data) {
-			t.Errorf("Expected data %q, got %q", expected[i].data, actual[i].data)
-		}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.expectPanic {
+				require.Panics(t, func() { removeOverlaps(tt.top, tt.bottom) })
+			} else {
+				result := removeOverlaps(tt.top, tt.bottom)
+				require.Equal(t, tt.expected, result)
+			}
+		})
 	}
 }
