953f3433b973fb430c471d72377ce9b085926c3a
Georg Dietrich <georg.dietrich@thieme-compliance.de>
2025-10-17 22:02:31 +0200

    improve test

--- src/aaa/memory_write_test.go
+++ src/aaa/memory_write_test.go
@@ -9,16 +9,20 @@ func TestMemoryWrite(t *testing.T) {
 
 	t.Run("write to empty memory", func(t *testing.T) {
 		mem := &memory{}
-		pos := 0
-		data := bytesOf(1, 2, 3)
-		mem.write(pos, data, 1024)
+		area := dataArea{0, bytesOf(1, 2, 3)}
+		mem.write(area.position, area.data, 1024)
+
+		// create a deep copy of area for comparison
+		areaCopy := area.copy()
+		// overwrite data in place to prove write has used a deep copy
+		copy(area.data, make(bytes, area.len()))
+
 		if len(mem.areas) != 1 {
 			t.Fatalf("expected 1 area, got %d", len(mem.areas))
 		}
-		if mem.areas[0].position != pos || !reflect.DeepEqual(mem.areas[0].data, data) {
+		if mem.areas[0].position != areaCopy.position || !reflect.DeepEqual(mem.areas[0].data, areaCopy.data) {
 			t.Errorf("unexpected area: got %+v", mem.areas[0])
 		}
-		// FIXME ensure we have a copy of data, not the same slice
 	})
 
 	t.Run("overwrite existing area", func(t *testing.T) {
@@ -26,6 +30,10 @@ func TestMemoryWrite(t *testing.T) {
 		pos := 1
 		data := bytesOf(9, 9, 9)
 		mem.write(pos, data, 1024)
+
+		// overwrite data in place to prove write has used a deep copy
+		copy(data, make(bytes, len(data)))
+
 		// Expect: [1 9 9 9 5] - merged into one area
 		want := dataAreas{
 			{position: 0, data: bytesOf(1, 9, 9, 9, 5)},
@@ -33,8 +41,6 @@ func TestMemoryWrite(t *testing.T) {
 		if !reflect.DeepEqual(mem.areas, want) {
 			t.Errorf("areas after overwrite: got %+v, want %+v", mem.areas, want)
 		}
-		// FIXME ensure we have a copy of data, not the same slice
-		// FIXME add test for no merge due to size limit
 	})
 
 	t.Run("merge adjacent areas", func(t *testing.T) {
@@ -50,6 +56,7 @@ func TestMemoryWrite(t *testing.T) {
 		}
 	})
 
+	// FIXME add test for no merge due to size limit
 	// FIXME more tests...
 }
 
