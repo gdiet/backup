1d82521241ca47620e2b2f2b30a389cb866b61c8
Georg Dietrich <georg.dietrich@thieme-compliance.de>
2025-10-17 22:05:38 +0200

    improve test

--- src/aaa/memory_write_test.go
+++ src/aaa/memory_write_test.go
@@ -10,7 +10,7 @@ func TestMemoryWrite(t *testing.T) {
 	t.Run("write to empty memory", func(t *testing.T) {
 		mem := &memory{}
 		area := dataArea{0, bytesOf(1, 2, 3)}
-		mem.write(area.position, area.data, 1024)
+		memoryDelta := mem.write(area.position, area.data, 1024)
 
 		// create a deep copy of area for comparison
 		areaCopy := area.copy()
@@ -23,13 +23,16 @@ func TestMemoryWrite(t *testing.T) {
 		if mem.areas[0].position != areaCopy.position || !reflect.DeepEqual(mem.areas[0].data, areaCopy.data) {
 			t.Errorf("unexpected area: got %+v", mem.areas[0])
 		}
+		if memoryDelta != areaCopy.len() {
+			t.Errorf("unexpected memoryDelta: got %d, want %d", memoryDelta, areaCopy.len())
+		}
 	})
 
 	t.Run("overwrite existing area", func(t *testing.T) {
 		mem := &memory{areas: dataAreas{{position: 0, data: bytesOf(1, 2, 3, 4, 5)}}}
 		pos := 1
 		data := bytesOf(9, 9, 9)
-		mem.write(pos, data, 1024)
+		memoryDelta := mem.write(pos, data, 1024)
 
 		// overwrite data in place to prove write has used a deep copy
 		copy(data, make(bytes, len(data)))
@@ -41,6 +44,9 @@ func TestMemoryWrite(t *testing.T) {
 		if !reflect.DeepEqual(mem.areas, want) {
 			t.Errorf("areas after overwrite: got %+v, want %+v", mem.areas, want)
 		}
+		if memoryDelta != 0 {
+			t.Errorf("unexpected memoryDelta: got %d, want %d", memoryDelta, 0)
+		}
 	})
 
 	t.Run("merge adjacent areas", func(t *testing.T) {
@@ -48,11 +54,14 @@ func TestMemoryWrite(t *testing.T) {
 			{position: 0, data: bytesOf(1, 2)},
 			{position: 2, data: bytesOf(3, 4)},
 		}}
-		mem.write(4, bytesOf(5, 6), 1024)
+		memoryDelta := mem.write(4, bytesOf(5, 6), 1024)
 		// Expect: [1 2] [3 4 5 6]
 		want := dataAreas{{position: 0, data: bytesOf(1, 2)}, {position: 2, data: bytesOf(3, 4, 5, 6)}}
 		if !reflect.DeepEqual(mem.areas, want) {
 			t.Errorf("areas after merge: got %+v, want %+v", mem.areas, want)
+		}
+		if memoryDelta != 2 {
+			t.Errorf("unexpected memoryDelta: got %d, want %d", memoryDelta, 2)
 		}
 	})
 
