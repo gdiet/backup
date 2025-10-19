ec0ecd8411b14af7ce52e065f4963a6661b5a672
Georg Dietrich <georg.dietrich@thieme-compliance.de>
2025-10-19 11:01:39 +0200

    cleaner test

--- src/aaa/dataAreas_invariants_test.go
+++ src/aaa/dataAreas_invariants_test.go
@@ -7,38 +7,48 @@ import (
 )
 
 func TestValidateDataAreasInvariants(t *testing.T) {
-	require.Panics(t, func() {
-		validateDataAreasInvariants(dataAreas{
-			dataArea{position: 0, data: bytes{}},
+	t.Run("empty area should cause panic", func(t *testing.T) {
+		require.Panics(t, func() {
+			validateDataAreasInvariants(dataAreas{
+				dataArea{position: 0, data: bytes{}},
+			})
 		})
-	}, "empty area should cause panic")
+	})
 
-	require.Panics(t, func() {
-		validateDataAreasInvariants(dataAreas{
-			dataArea{position: 18, data: bytes("data")},
-			dataArea{position: 10, data: bytes("1234")},
+	t.Run("areas not ordered by position should cause panic", func(t *testing.T) {
+		require.Panics(t, func() {
+			validateDataAreasInvariants(dataAreas{
+				dataArea{position: 18, data: bytes("data")},
+				dataArea{position: 10, data: bytes("1234")},
+			})
 		})
-	}, "areas not ordered by position should cause panic")
+	})
 
-	require.Panics(t, func() {
-		validateDataAreasInvariants(dataAreas{
-			dataArea{position: 8, data: bytes("data")},
-			dataArea{position: 10, data: bytes("1234")},
+	t.Run("overlapping areas should cause panic", func(t *testing.T) {
+		require.Panics(t, func() {
+			validateDataAreasInvariants(dataAreas{
+				dataArea{position: 8, data: bytes("data")},
+				dataArea{position: 10, data: bytes("1234")},
+			})
 		})
-	}, "overlapping areas should cause panic")
+	})
 
-	require.Panics(t, func() {
-		data := make(bytes, 10, 20)
-		validateDataAreasInvariants(dataAreas{
-			dataArea{position: 8, data: data},
+	t.Run("non-compact areas should cause panic", func(t *testing.T) {
+		require.Panics(t, func() {
+			data := make(bytes, 10, 20)
+			validateDataAreasInvariants(dataAreas{
+				dataArea{position: 8, data: data},
+			})
 		})
-	}, "non-compact areas should cause panic")
+	})
 
-	require.NotPanics(t, func() {
-		validateDataAreasInvariants(dataAreas{
-			dataArea{position: 8, data: bytes("data")},
-			dataArea{position: 18, data: bytes("1234")},
-			dataArea{position: 28, data: bytes("data")},
+	t.Run("well-formed areas should not cause panic", func(t *testing.T) {
+		require.NotPanics(t, func() {
+			validateDataAreasInvariants(dataAreas{
+				dataArea{position: 8, data: bytes("data")},
+				dataArea{position: 18, data: bytes("1234")},
+				dataArea{position: 28, data: bytes("data")},
+			})
 		})
-	}, "well-formed areas should not cause panic")
+	})
 }
