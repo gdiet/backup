2f0b0fbe596d484dc28b4ccc19ccc40dff28004a
Georg Dietrich <georg.dietrich@thieme-compliance.de>
2025-11-10 21:21:02 +0100

    message fix

--- src/metadata/internal/treeentry.go
+++ src/metadata/internal/treeentry.go
@@ -9,7 +9,7 @@ import (
 func treeEntry(tree *bbolt.Bucket, id []byte) (TreeEntry, error) {
 	bytes := tree.Get(id)
 	if bytes == nil {
-		return nil, fmt.Errorf("orphaned tree entry for child ID %x", id)
+		return nil, fmt.Errorf("orphaned tree entry for ID %x", id)
 	}
 	return TreeEntryFromBytes(bytes)
 }
