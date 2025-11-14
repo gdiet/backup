427ddcbd7bc33caffb48071fcc67d1be91e03a1d
Georg Dietrich <georg.dietrich@thieme-compliance.de>
2025-11-14 16:43:25 +0100

    starting to connect fs with repository

--- src/fs.go
+++ src/fs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"backup/src/metadata"
 	"log"
 	"os"
 
@@ -9,11 +10,11 @@ import (
 
 type Fs struct {
 	fuse.FileSystemBase
+	r *metadata.Repository
 }
 
-func NewFs() *Fs {
-	self := Fs{}
-	return &self
+func NewFs(r *metadata.Repository) *Fs {
+	return &Fs{r: r}
 }
 
 func (f *Fs) Readdir(path string, fill func(name string, stat *fuse.Stat_t, ofst int64) bool, ofst int64, fh uint64) int {
@@ -54,9 +55,54 @@ func (f *Fs) Getattr(path string, stat *fuse.Stat_t, fh uint64) int {
 }
 
 func main() {
-	fs := NewFs()
+	var err error
+	var tempFile *os.File
+	var r *metadata.Repository
+
+	if tempFile, err = os.CreateTemp("", "fs-*.db"); err != nil {
+		log.Printf("Failed to create temp file: %v", err)
+		os.Exit(1)
+	}
+	defer func() {
+		if e := os.Remove(tempFile.Name()); e == nil {
+			log.Printf("Removed temp database file: %s", tempFile.Name())
+		} else {
+			log.Printf("Failed to remove temp database file: %v", e)
+		}
+		if err != nil {
+			log.Printf("Process finished with error: %v", err)
+			os.Exit(1)
+		}
+	}()
+	if err = tempFile.Close(); err != nil {
+		log.Printf("Failed to close temp database file: %v", err)
+		return
+	}
+	log.Printf("Using temp database file: %s", tempFile.Name())
+
+	if r, err = metadata.NewRepository(tempFile.Name()); err != nil {
+		log.Printf("Failed to create repository: %v", err)
+		return
+	}
+	if id, err := r.Mkdir(0, "testing"); err != nil {
+		log.Printf("Failed to create directory 'testing': %v", err)
+		return
+	} else {
+		if _, err := r.Mkdir(id, "hello"); err != nil {
+			log.Printf("Failed to create directory 'hello': %v", err)
+			return
+		}
+		if _, err := r.Mkdir(id, "world"); err != nil {
+			log.Printf("Failed to create directory 'world': %v", err)
+			return
+		}
+	}
+
+	fs := NewFs(r)
 	host := fuse.NewFileSystemHost(fs)
 	// Using host.SetCapReaddirPlus(true) could save some Getattr calls, but it's not easy to get it right.
 	// On FUSE3, we could set host.SetUseIno(true), but I don't see a real benefit yet.
+	log.Printf("Mounting file system...")
 	host.Mount("", os.Args[1:])
+	log.Printf("Finished main execution")
 }
