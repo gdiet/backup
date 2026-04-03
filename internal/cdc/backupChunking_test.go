6d1b44964225e6f6a4bbb13d2d3d5a317a6badee
Georg Dietrich <georg.dietrich@thieme-compliance.de>
2026-04-03 23:46:26 +0200

    fix: ensure chunkers flush correctly and add TODO for outer loop

--- internal/cdc/backupChunking_test.go
+++ internal/cdc/backupChunking_test.go
@@ -11,6 +11,8 @@ import (
 )
 
 func TestBackupChunking(t *testing.T) {
+	// TODO add outer loop for the id pages
+
 	resp, err := http.Get("http://localhost:8080/data/ids?startAfter=-1&size=100")
 	require.NoError(t, err)
 	require.Equal(t, resp.StatusCode, 200)
@@ -37,10 +39,11 @@ func TestBackupChunking(t *testing.T) {
 		require.ErrorIs(t, err, io.EOF)
 		require.NoError(t, resp.Body.Close())
 		sChunks = append(sChunks, sChunker.Flush()...)
+		cChunks = append(cChunks, cChunker.Flush()...)
+		fChunks = append(fChunks, fChunker.Flush()...)
 		for i, chunk := range sChunks {
 			t.Logf("s:%d:%d:%v", id, i+1, &chunk)
 		}
-		// FIXME cChunks and fChunks are empty...?
 		for i, chunk := range cChunks {
 			t.Logf("c:%d:%d:%v", id, i+1, &chunk)
 		}
